e85b19eadd8fce652e119441193fd9d6197cbfe7
duncan <duncan>
2004-10-27 17:39:02 +0000

    removed message about licence

--- glue/setup.py
+++ glue/setup.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python
 #
-# ldbd setup file
-#
 # $Id$
-#
-# Copyright (C) 2003 Duncan Brown
 # 
-# This file is part of the lightweight datapase dumper (ldbd)
-#
-# ldbd is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# ldbd is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# ldbd; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA  02111-1307  USA
-#
+# setup for glue
 
 from distutils.core import setup
 
