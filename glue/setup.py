0ef448ff382b7fe40decbb207db3a8d07c1c1e04
duncan <duncan>
2004-11-17 19:33:30 +0000

    changed email address

--- glue/setup.py
+++ glue/setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 setup( name = "glue",
   version = "0.1",
   author = "Duncan Brown",
-  author_email = "duncan@gravity.phys.uwm.edu",
+  author_email = "dbrown@ligo.caltech.edu",
   description = "Grid LSC User Engine",
   url = "http://www.lsc-group.phys.uwm.edu/daswg/",
   license = 'See file LICENSE',
