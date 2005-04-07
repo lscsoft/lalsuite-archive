ecf5dd37c01a27441496b3b7f2671e945d31aa8b
volodya <volodya>
2005-04-07 16:34:04 +0000

    Add more options to dat2skymap, print out first four data points.

--- lalapps/src/pulsar/PowerFlux/dat2skymap.c
+++ lalapps/src/pulsar/PowerFlux/dat2skymap.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "rastermagic.h"
 #include "grid.h"
@@ -39,16 +40,20 @@ return 1;
 int main(int argc, char *argv[])
 {
 FILE *fin;
+int i;
+float a;
 
 LOG=stderr;
 
-if(argc<4){
-	fprintf(stderr, "Usage: %s resolution file.dat skymap.png\n", argv[0]);
+if(argc<7){
+	fprintf(stderr, "\nUsage: %s mode resolution lower_cut upper_cut file.dat skymap.png\n", argv[0]);
+	fprintf(stderr, "\tmodes: plain, log10\n");
+	fprintf(stderr, "\tcutoff: number or \"auto\"\n\n");
 	return -1;
 	}
-resolution=atof(argv[1]);
+resolution=atof(argv[2]);
 
-fin=fopen(argv[2], "r");
+fin=fopen(argv[5], "r");
 if(fin==NULL){
 	perror("Cannot read file:");
 	return -1;
@@ -58,10 +63,27 @@ grid=make_sin_theta_grid(resolution);
 
 data=do_alloc(grid->npoints, sizeof(*data));
 fread(data, sizeof(*data), grid->npoints, fin);
+fprintf(stderr, "%g %g %g %g\n", data[0], data[1], data[2], data[3]);
+
+if(!strcasecmp(argv[1], "log10")){
+	for(i=0;i<grid->npoints;i++)data[i]=log10(data[i]);
+	}
+
+if(strcasecmp(argv[3], "auto")){
+	a=atof(argv[3]);
+	for(i=0;i<grid->npoints;i++)
+		if(data[i]<=a)data[i]=a;
+	}
+
+if(strcasecmp(argv[4], "auto")){
+	a=atof(argv[4]);
+	for(i=0;i<grid->npoints;i++)
+		if(data[i]>=a)data[i]=a;
+	}
 
 pic=make_RGBPic(grid->max_n_ra+140, grid->max_n_dec);
 plot_grid_f(pic, grid, data, 1);
 
-RGBPic_dump_png(argv[3], pic);
+RGBPic_dump_png(argv[6], pic);
 return 0;
 }
