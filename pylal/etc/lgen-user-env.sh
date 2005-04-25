185534be9031678e186d6b4a9cc1abea7d0007c3
patrick <patrick>
2005-04-25 03:38:57 +0000

    corrected path name

--- pylal/etc/lgen-user-env.sh
+++ pylal/etc/lgen-user-env.sh
@@ -4,31 +4,31 @@
 #
 # source this file to set up your environment for glue
 
-if [ -z "${LGT_LOCATION}" ]; then
-    echo "ERROR: environment variable LGT_LOCATION not defined"  1>&2
+if [ -z "${LGEN_LOCATION}" ]; then
+    echo "ERROR: environment variable LGEN_LOCATION not defined"  1>&2
     return 1
 fi
 
-PATH=`echo "${PATH}" | sed -e "s%:${LGT_LOCATION}[^:]*%%g" -e "s%^${LGT_LOCATION}[^:]*:\{0,1\}%%"`
-PYTHONPATH=`echo "${PYTHONPATH}" | sed -e "s%:${LGT_LOCATION}[^:]*%%g" -e "s%^${LGT_LOCATION}[^:]*:\{0,1\}%%"`
-LD_LIBRARY_PATH=`echo "${LD_LIBRARY_PATH}" | sed -e "s%:${LGT_LOCATION}[^:]*%%g" -e "s%^${LGT_LOCATION}[^:]*:\{0,1\}%%"`
+PATH=`echo "${PATH}" | sed -e "s%:${LGEN_LOCATION}[^:]*%%g" -e "s%^${LGEN_LOCATION}[^:]*:\{0,1\}%%"`
+PYTHONPATH=`echo "${PYTHONPATH}" | sed -e "s%:${LGEN_LOCATION}[^:]*%%g" -e "s%^${LGEN_LOCATION}[^:]*:\{0,1\}%%"`
+LD_LIBRARY_PATH=`echo "${LD_LIBRARY_PATH}" | sed -e "s%:${LGEN_LOCATION}[^:]*%%g" -e "s%^${LGEN_LOCATION}[^:]*:\{0,1\}%%"`
 
 if [ -n "${MANPATH}" ]; then
-    MANPATH=`echo "${MANPATH}" | sed -e "s%:${LGT_LOCATION}[^:]*%%g" -e "s%^${LGT_LOCATION}[^:]*:\{0,1\}%%"`
+    MANPATH=`echo "${MANPATH}" | sed -e "s%:${LGEN_LOCATION}[^:]*%%g" -e "s%^${LGEN_LOCATION}[^:]*:\{0,1\}%%"`
 fi
 
-LGT_PATH=${LGT_LOCATION}
-PATH="${LGT_LOCATION}/bin:${LGT_LOCATION}/sbin:${PATH}";
-PYTHONPATH="${LGT_LOCATION}/lib/python:${PYTHONPATH}"
+LGT_PATH=${LGEN_LOCATION}
+PATH="${LGEN_LOCATION}/bin:${LGEN_LOCATION}/sbin:${PATH}";
+PYTHONPATH="${LGEN_LOCATION}/lib/python:${PYTHONPATH}"
 
 if [ -n "${MANPATH}" ]; then
-    MANPATH="${LGT_LOCATION}/man:${MANPATH}"
+    MANPATH="${LGEN_LOCATION}/man:${MANPATH}"
 fi
 
 DELIM=
 if [ -n "${LD_LIBRARY_PATH}" ]; then
     DELIM=:
 fi
-LD_LIBRARY_PATH="${LGT_LOCATION}/lib${DELIM}${LD_LIBRARY_PATH}"
+LD_LIBRARY_PATH="${LGEN_LOCATION}/lib${DELIM}${LD_LIBRARY_PATH}"
 
 export LGT_PATH PATH MANPATH LD_LIBRARY_PATH PYTHONPATH
