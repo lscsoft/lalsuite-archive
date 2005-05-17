875579bbee7a4835de2c78391fa824a0c67aa4a2
kipp <kipp>
2005-05-17 19:31:59 +0000

    Clean up setup.py: 	- Don't spread across two files. 	- Only include code that is actually used. 	- Get LAL cflags and libs options from pkg-config.

--- pylal/setup.py
+++ pylal/setup.py
@@ -1,31 +1,41 @@
 # $Id$
 # 
-# setup for glue
+# setup for lgen
 
 import os
-from distutils.core import setup
-from setupext import build_metaio
+from distutils.core import setup, Extension
 
-ext_modules = []
+def stripfirsttwo(string):
+	return string[2:]
 
-packages = [ 'lgen' ]
+lallibs = map(stripfirsttwo, os.popen("pkg-config lal --libs-only-l").read().split())
+lallibdirs = map(stripfirsttwo, os.popen("pkg-config lal --libs-only-L").read().split())
+lalincdirs = map(stripfirsttwo, os.popen("pkg-config lal --cflags-only-I").read().split())
 
-build_metaio(ext_modules, packages)
-
-setup( name = "lgen",
-  version = "0.1",
-  author = "Patrick Brady",
-  author_email = "patrick@gravity.phys.uwm.edu",
-  description = "LSC Graphics Toolkit",
-  url = "http://www.lsc-group.phys.uwm.edu/daswg/",
-  license = 'See file LICENSE',
-  packages = packages,
-  ext_modules = ext_modules,
-  scripts = [ os.path.join('bin','plotburst'),
-              os.path.join('bin','plotsiminspiral'),
-              os.path.join('bin','plotinspiral') ],
-  data_files = [ ('etc',[
-    os.path.join('etc','lgen-user-env.sh'),
-    os.path.join('etc','lgen-user-env.csh')
-    ] ) ]
-  )
+setup(
+	name = "lgen",
+	version = "0.1",
+	author = "Patrick Brady",
+	author_email = "patrick@gravity.phys.uwm.edu",
+	description = "LSC Graphics Toolkit",
+	url = "http://www.lsc-group.phys.uwm.edu/daswg/",
+	license = "See file LICENSE",
+	packages = ["lgen"],
+	ext_modules = [
+		Extension("lgen.metaio", ["src/metaio.c"],
+			include_dirs = lalincdirs,
+			libraries = lallibs,
+			library_dirs = lallibdirs)
+	],
+	scripts = [
+		os.path.join("bin", "plotburst"),
+		os.path.join("bin", "plotsiminspiral"),
+		os.path.join("bin", "plotinspiral")
+	],
+	data_files = [
+		("etc", [
+			os.path.join("etc", "lgen-user-env.sh"),
+			os.path.join("etc", "lgen-user-env.csh")
+		])
+	]
+)
