1023ed63cf0d7f6446fc44d556e1ad610c36f569
volodya <volodya>
2005-06-25 20:34:25 +0000

    Properly delete statistics files in STATS_DIR instead of local directory.

--- lalapps/src/pulsar/PowerFlux/scripts/all_stats.sh
+++ lalapps/src/pulsar/PowerFlux/scripts/all_stats.sh
@@ -3,7 +3,8 @@
 UTILS=~/PowerFlux/scripts/
 
 ROOT=`$UTILS/get_param.tcl ROOT_DIR`
+STATS_DIR=`$UTILS/get_param.tcl STATS_DIR`
 
-rm -f *.dat
+rm -f $STATS_DIR/*.dat
 find $ROOT/output -name powerflux.log -exec $UTILS/split.tcl {} \;
 
