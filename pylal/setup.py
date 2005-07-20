042d66fb9f3057c629cd0e69db44bc8f4621762d
darren <darren>
2005-07-20 16:06:18 +0000

    Added plotinspmissed

--- pylal/setup.py
+++ pylal/setup.py
@@ -37,7 +37,8 @@ setup(
 		os.path.join("bin", "plotsiminspiral"),
 		os.path.join("bin", "plotgbb"),
 		os.path.join("bin", "plotinspiral"),
-		os.path.join("bin", "plotinspinj")
+		os.path.join("bin", "plotinspinj"),
+                os.path.join("bin", "plotinspmissed")
 	],
 	data_files = [
 		("etc", [
