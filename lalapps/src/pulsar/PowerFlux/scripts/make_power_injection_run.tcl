d0274c01e0da8e1694671cbe695bcd51f184246b
volodya <volodya>
2005-07-22 02:46:48 +0000

    Add scripts for making power injection Monte-Carlo runs.

---
+++ lalapps/src/pulsar/PowerFlux/scripts/make_power_injection_run.tcl
@@ -0,0 +1,68 @@
+#!/usr/bin/env tclsh
+
+source "power_injection_params.tcl"
+
+foreach $PARAMS_FORMAT $PARAMS {
+	set $var [subst -nocommands -nobackslashes $value]
+	}
+
+file mkdir $ROOT_DIR
+file mkdir $CONF_DIR $OUTPUT_DIR $ERR_DIR
+	
+proc sample { limits } {
+set a [lindex $limits 0]
+set b [lindex $limits 1]
+return [expr $a+rand()*($b-$a)]
+}
+
+set first_bin [expr round($FREQ_START*1800)]	
+set i 0
+set k 0
+while { 1 }  {
+	set dec [sample $DEC_RANGE]
+	set ra [sample $RA_RANGE]
+	set orientation [sample $ORIENT_RANGE]
+	set freq [sample [list [expr $first_bin/1800.0] [expr ($first_bin+450)/1800.0]]]
+	set power [expr exp([sample $POWER_LOG10_RANGE] * log(10.0)) * $POWER_MAX]
+        set spindown [expr exp([sample $SPINDOWN_LOG10_RANGE] * log(10.0)) * $SPINDOWN_MAX]
+
+	set FILE [open "$CONF_DIR/$i" "w"]
+	puts $FILE [subst -nocommands -nobackslashes $POWERFLUX_CONF_FILE]
+	close $FILE
+
+	incr i
+	incr k
+
+	if { $k >= $INJECTIONS_PER_BAND } {
+		puts stderr "Band [expr $first_bin/1800.0] complete"
+		set first_bin [expr round($first_bin+$FREQ_STEP*1800)]
+		if { $first_bin >= $FREQ_STOP*1800 } { break }
+		set k 0
+		}
+	}
+
+set CONDOR_FILE {
+universe=standard
+executable=$ANALYSIS_PROGRAM
+input=/dev/null
+output=$ERR_DIR/out.\$(PID)
+error=$ERR_DIR/err.\$(PID)
+arguments=--config=$CONF_DIR/\$(PID)
+log=$LOG_FILE
+queue
+}
+
+	
+set FILE [open "$ROOT_DIR/condor" "w"]
+puts $FILE [subst -nocommands $CONDOR_FILE]
+close $FILE
+
+set FILE [open "$ROOT_DIR/dag" "w"]
+for { set k 0 } { $k < $i } { incr k } {
+	puts $FILE "JOB A$k $ROOT_DIR/condor"
+	puts $FILE "VARS A$k PID=\"$k\""
+	}
+close $FILE
+
+exec cp power_injection_params.tcl $ROOT_DIR
+puts stderr "Prepared input for $i jobs"
---
+++ lalapps/src/pulsar/PowerFlux/scripts/power_injection_params.tcl
@@ -0,0 +1,57 @@
+#!/usr/bin/env tclsh
+
+set PARAMS {
+	IFO "H1"
+	DETECTOR "H"
+	RUN_NAME "fake_random.1"
+	ROOT_DIR "/archive/home/volodya/runs/$RUN_NAME/"
+	CONF_DIR "$ROOT_DIR/in/"
+	OUTPUT_DIR "$ROOT_DIR/output/"
+	ERR_DIR "$ROOT_DIR/err/"
+	LOG_FILE "/usr1/volodya/$RUN_NAME.log"
+	FREQ_START  150.0
+	FREQ_STOP   250.0
+	FREQ_STEP    0.25
+	INJECTIONS_PER_BAND  20
+	DEC_RANGE  { -1.57 1.57 }
+	RA_RANGE { 0 6.283 }
+	ORIENT_RANGE { 0 0.785 }
+	ORIENT_RANGE { 0 3.1415 }
+	SPINDOWN_LOG10_RANGE { -1.5 0}
+	SPINDOWN_MAX 2e-9
+	POWER_LOG10_RANGE { -1.5 0}
+	POWER_MAX 4e-23
+	ANALYSIS_PROGRAM "/archive/home/volodya/PowerFlux/powerflux"
+	}
+
+set PARAMS_FORMAT { var value }	
+
+set POWERFLUX_CONF_FILE {
+input /archive/home/volodya/SFT-3/S4.${IFO}.ht.geo/sft.${IFO}.
+input-format GEO
+detector L${DETECTOR}O
+earth-ephemeris /archive/home/volodya/detresponse/earth05.dat
+sun-ephemeris /archive/home/volodya/detresponse/sun05.dat
+output $OUTPUT_DIR/$i
+first-bin $first_bin
+nbins 501
+do-cutoff 1
+three-bins 0
+filter-lines 1
+fake-linear
+fake-ra	$ra
+fake-dec $dec
+fake-strain $power 
+fake-freq $freq
+fake-orientation $orientation
+fake-spindown $spindown
+write-dat NONE
+write-png NONE
+focus-ra $ra
+focus-dec $dec
+focus-radius 0.1
+subtract-background 1
+ks-test 1
+	}
+	
+	
