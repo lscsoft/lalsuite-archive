7be3274ad3ad5311cc048b76e2b5397f076cc233
sfairhur <sfairhur>
2005-07-29 22:54:18 +0000

    added clustering method to coincInspiralTable

--- pylal/pylal/readMeta.py
+++ pylal/pylal/readMeta.py
@@ -76,12 +76,13 @@ class metaDataTable:
     provided column name.
     @param colname: string name of the column to be sliced out
     """
-    myarray = asarray( [ self.table[i][colname] for i in range(self.nevents())] )
+    myarray = asarray([ self.table[i][colname] for i in range(self.nevents())])
     return myarray
 
   def ifocut(self, ifo):
     """
     Return a subset of the table, containing only events from the specified ifo
+    @param ifo: ifo from which to keep events
     """
     ifocuttable = metaDataTable(None, self.tabletype )
 
@@ -91,7 +92,18 @@ class metaDataTable:
     return ifocuttable
 
 class coincInspiralTable:
+  """
+  Table to hold coincident inspiral triggers.  Coincidences are reconstructed 
+  by making use of the event_id contained in the sngl_inspiral table.
+  The coinc is a dictionary with entries: H1, H2, L1, event_id, numifos, snrsq.
+  The snrsq is the sum of the squares of the snrs of the individual triggers.
+  """
   def __init__(self, inspTriggers = None):
+    """
+    @param inspTriggese: a metaDataTable containing inspiral triggers 
+                         from which to construct coincidences
+    """
+
     if not inspTriggers:
       self.table = []
     else:
@@ -109,13 +121,14 @@ class coincInspiralTable:
       eventidlist = asarray(uniq(inspTriggers.mkarray("event_id")))
       self.table = []
       for event_id in eventidlist: 
-        self.table.append({ "event_id":event_id, "numifos":0 })
+        self.table.append({ "event_id":event_id, "numifos":0, "snrsq":0 })
   
       for coinc in self.table:
         for h1_trig in h1triggers.table:
           if coinc["event_id"] == h1_trig["event_id"]:
             coinc["H1"] = h1_trig
             coinc["numifos"] += 1
+            coinc["snrsq"] += h1_trig["snr"] * h1_trig["snr"] 
             break
   
       for coinc in self.table:
@@ -123,6 +136,7 @@ class coincInspiralTable:
           if coinc["event_id"] == h2_trig["event_id"]:
             coinc["H2"] = h2_trig
             coinc["numifos"] += 1
+            coinc["snrsq"] += h2_trig["snr"] * h2_trig["snr"] 
             break
     
       for coinc in self.table:
@@ -130,6 +144,7 @@ class coincInspiralTable:
           if coinc["event_id"] == l1_trig["event_id"]:
             coinc["L1"] = l1_trig
             coinc["numifos"] += 1
+            coinc["snrsq"] += l1_trig["snr"] * l1_trig["snr"] 
             break
 
 
@@ -151,7 +166,13 @@ class coincInspiralTable:
     return asarray( mylist )
 
   def coinctype(self, ifo1, ifo2, ifo3 = None):
-    
+    """
+    Return the coincs which are from ifo1-ifo2(-ifo3 if specified).
+    @param ifo1: first ifo in coincidence
+    @param ifo2: second ifo in coincidence
+    @param ifo3: third (optional) ifo in coincidence
+    """
+ 
     selected_coincs = coincInspiralTable()
     for coinc in self.table:
       if coinc.has_key(ifo1) and coinc.has_key(ifo2):
@@ -161,8 +182,26 @@ class coincInspiralTable:
         
     return selected_coincs
 
+  def getsngls(self, ifo):
+    """
+    Return the sngls for a specific ifo.
+    @param ifo: ifo for which to retrieve the single inspirals.
+    """
+
+    ifo_sngl = metaDataTable(None, 'sngl_inspiral')
+    for coinc in self.table:
+      if coinc.has_key(ifo):
+        ifo_sngl.table.append(coinc[ifo])
+        
+    return ifo_sngl
+
+
 
   def getslide(self, slide_num):
+    """
+    Return the triggers with a specific slide number.
+    @param slide_num: the slide number to recover (contained in the event_id)
+    """
 
     slide_coincs = coincInspiralTable()
     for coinc in self.table:
@@ -171,7 +210,49 @@ class coincInspiralTable:
      
     return slide_coincs 
 
+  
+  def cluster(self, cluster_window):
+    """
+    Return the clustered triggers, returning the one with the largest snrsq in 
+    each fixed cluster_window
     
+    @param cluster_window: length of time over which to cluster (seconds)
+    """
+    
+    # find times when there is a trigger
+    cluster_times = []
+    for coinc in self.table:
+      for ifo in ['H1','H2','L1']:
+        if coinc.has_key(ifo):
+          end_time = coinc[ifo]['end_time']
+          break
+      
+      cluster_times.append(cluster_window * (end_time/cluster_window) )
+    cluster_times = uniq(cluster_times)
+    
+    cluster_triggers = coincInspiralTable()
+    for cluster_time in cluster_times:
+      # find all triggers at that time
+      cluster = coincInspiralTable()
+      for coinc in self.table:
+        for ifo in ['H1','H2','L1']:
+          if coinc.has_key(ifo):
+            end_time = coinc[ifo]['end_time']
+            break
+        if ((end_time - cluster_time) / cluster_window == 0):   
+          cluster.table.append(coinc)
+
+      # find loudest trigger in time and append
+      loudest_snr = 0
+      for trigger in cluster.table:
+        if trigger["snrsq"] > loudest_snr:
+          loudest_trig = trigger
+
+      cluster_triggers.table.append(loudest_trig)
+      
+    return cluster_triggers 
+
+
 def usage():
         print "readMeta.py -x xml file"
         sys.exit(0)
