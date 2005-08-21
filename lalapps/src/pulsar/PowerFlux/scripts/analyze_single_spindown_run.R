dfb936af324af986ad41e99d7f6e9617b909a056
volodya <volodya>
2005-08-21 01:47:51 +0000

    Improve memory usage. Improve plots. Output BluePoints.txt.

--- lalapps/src/pulsar/PowerFlux/scripts/analyze_single_spindown_run.R
+++ lalapps/src/pulsar/PowerFlux/scripts/analyze_single_spindown_run.R
@@ -9,24 +9,57 @@ ofn<-function(name) {
 	return(file(paste(output_dir, "/", name, sep=""), open="wt"))
 	}
 
+#
+# Load large data sets first
+#
+ksVeto<- NULL
+for(i in Bands) {
+	#
+	# Read ks_hist here as it takes too much RAM otherwise
+	#
+	fn<-paste(prefix, "ks_hist", ".", i, suffix, sep="")
+	cat("Loading data from ", fn, "\n", sep="")
+	data<-read.table(pipe(paste("cut -f 1,2,3,4 -d \\  < ", fn, sep="")), header=FALSE)
+	if(is.null(ksVeto)) {
+		ksVeto <- data[,4]<ksLarge
+		} else {
+		ksVeto <- ksVeto + (data[,4]<ksLarge)
+		}
+	#
+	# Free RAM
+	#
+	rm(data)
+	#gc(verbose=FALSE, reset=TRUE)
+	}
+
+
+fn<-paste(prefix, "hist_residuals", suffix, sep="")
+cat("Loading data from ", fn, "\n", sep="")
+data<-read.table(pipe(paste("cut -f 1,2,3,4 -d \\  < ", fn, sep="")), header=FALSE)
+highRes<-data[,4]
+highResMax<-highRes>ResLarge
+#
+# Free RAM
+#
+rm(data)
+#gc(verbose=FALSE, reset=TRUE)
+
 for(field in c("band", "cputime", "max_dx", "masked", "npoints", 
 		"max_high_ul", "max_circ_ul", "median", "TMedian",
-		"hist_residuals")) {
+		"spindown")) {
 	fn<-paste(prefix, field, suffix, sep="")
 	cat("Loading data from ", fn, "\n", sep="")
 	assign(paste(var_prefix, field, sep=""), read.table(fn))
 	}
 
 for(field in c("max_band", "masked_max_band", "max_high_ul_band",
-		"max_circ_ul_band", "ks_hist")) {
+		"max_circ_ul_band")) {
 	for(i in Bands) {
 		fn<-paste(prefix, field, ".", i, suffix, sep="")
 		cat("Loading data from ", fn, "\n", sep="")
 		assign(paste(var_prefix,field,i,sep=""), read.table(fn))
 		}
 	}
-
-ks_hist2Count <- apply(ks_hist2[,5:204], 1, sum)
 
 plus<-max_dx[,4]=="plus"
 cross<-max_dx[,4]=="cross"
@@ -37,6 +70,8 @@ circ<-max_dx[,4]=="circular"
 nomasked<-masked[,3]==0
 # Assume 1 Hz within 60 Hz is 
 no60hz <- (((band[,4]+1)%% 60)>2) & (((band[,4]+1.25)%% 60)>2)
+
+ksVetoP <- pmax(ksVeto[plus], ksVeto[cross], ksVeto[pol1], ksVeto[pol2], ksVeto[circ])==6
 
 highulfactor <- 1.0/cos(pi/8)
 circulfactor <- 1.0
@@ -80,21 +115,16 @@ dev.off()
 
 CPUDays<-sum(cputime[,3]/(3600.0*5.0*24.0))
 #
-plot(band[,4],masked[,3], xlab="Frequency", ylab="Count", main="Number of masked points")
+#plot(band[,4],masked[,3], xlab="Frequency", ylab="Count", main="Number of masked points")
 
 
-ksVeto<- NULL
-for(i in Bands) {
-	if(is.null(ksVeto)) {
-		ksVeto <- vn("ks_hist", i)[,4]<ksLarge
-		} else {
-		ksVeto <- ksVeto + (vn("ks_hist", i)[,4]<ksLarge)
-		}
-	}
-
-ksVetoP <- pmax(ksVeto[plus], ksVeto[cross], ksVeto[pol1], ksVeto[pol2], ksVeto[circ])==6
-
-highResMax<-hist_residuals[,4]>ResLarge
+#
+# Histogram of detected strength
+#
+Factor<-no60hz[plus] & ksVetoP & !highResMax[plus]
+start_plot("detection_strength_hist")
+plot(hist(pmin(DxMaxBandPCapped, 15)[Factor], 200), main="Histogram of capped detection strength", xlab="Detection strength capped at 15", ylab="Count")
+dev.off()
 
 
 FancyPlot <- function(UL, f0=NULL, f1=NULL, ylab="y", title=ylab, median_shift=2, median_scale=1) {
@@ -106,6 +136,7 @@ FancyPlot <- function(UL, f0=NULL, f1=NULL, ylab="y", title=ylab, median_shift=2
 		}
 		
 	plot(band[plus,4], UL, type="n", main=title, xlim=c(f0, f1), xlab="Frequency", ylab=ylab)
+	grid()
 
 	points(band[plus,4], median_scale*(log10(2.0*median[plus,3])*0.5-log10(1800*16384))+median_shift, col="pink", type="s")
 
@@ -139,46 +170,83 @@ FancyPlot(DxMaxBandPCapped, title=DXtitle, ylab="Capped detection strength", med
 dev.off()
 
 #
-# Red points data
+# Write out special points data
 #
 
-factor<-no60hz[plus] & ksVetoP
-RedDiamonds<-data.frame(Band=band[plus,4][factor & highResMax[plus]],
-	pulsarUL=pulsarUL[plus][factor & highResMax[plus]],
-	circularUL=circularUL[factor & highResMax[plus]],
-	maxDx=pmax(max_dx[cross,5], 
+p_max_dx<-pmax(max_dx[cross,5], 
 		max_dx[plus,5], 
 		max_dx[pol1,5], 
 		max_dx[pol2,5], 
-		max_dx[circ,5])[factor & highResMax[plus]],
-	resMax=hist_residuals[plus,4][factor & highResMax[plus]],
-	spindown=circularUL[factor & highResMax[plus]]*0.0
+		max_dx[circ,5])
+		
+p_max_dx_pol<-max_dx[cross,]
+p_max_dx_pol[p_max_dx==max_dx[plus,5],]<-max_dx[plus,][p_max_dx==max_dx[plus,5],]
+p_max_dx_pol[p_max_dx==max_dx[pol1,5],]<-max_dx[pol1,][p_max_dx==max_dx[pol1,5],]
+p_max_dx_pol[p_max_dx==max_dx[pol2,5],]<-max_dx[pol2,][p_max_dx==max_dx[pol2,5],]
+p_max_dx_pol[p_max_dx==max_dx[circ,5],]<-max_dx[circ,][p_max_dx==max_dx[circ,5],]
+
+signalX<-cos(p_max_dx_pol[,2])*cos(p_max_dx_pol[,3])
+signalY<-sin(p_max_dx_pol[,2])*cos(p_max_dx_pol[,3])
+signalZ<-sin(p_max_dx_pol[,3])
+
+signalCos<-BandAxis[1]*signalX+BandAxis[2]*signalY+BandAxis[3]*signalZ
+
+Factor <- no60hz[plus] & !ksVetoP
+BluePoints<-data.frame(Band=band[plus,4][Factor],
+	pulsarUL=pulsarUL[plus][Factor],
+	circularUL=circularUL[Factor],
+	resMax=highRes[plus][Factor],
+	fdot=spindown[plus,3][Factor],
+	maxDx=p_max_dx[Factor],
+	RA=p_max_dx_pol[Factor, 2],
+	DEC=p_max_dx_pol[Factor, 3],
+	f0=p_max_dx_pol[Factor, 8],
+	ul=p_max_dx_pol[Factor, 6],
+	pol=p_max_dx_pol[Factor,4],
+	thetaCos=signalCos[Factor]
+	)
+
+file<-ofn("BluePoints.txt")
+write.table(BluePoints, file)
+close(file)
+
+
+
+Factor<-no60hz[plus] & ksVetoP & highResMax[plus]
+RedDiamonds<-data.frame(Band=band[plus,4][Factor],
+	pulsarUL=pulsarUL[plus][Factor],
+	circularUL=circularUL[Factor],
+	resMax=highRes[plus][Factor],
+	fdot=spindown[plus,3][Factor],
+	maxDx=p_max_dx[Factor],
+	RA=p_max_dx_pol[Factor, 2],
+	DEC=p_max_dx_pol[Factor, 3],
+	f0=p_max_dx_pol[Factor, 8],
+	ul=p_max_dx_pol[Factor, 6],
+	pol=p_max_dx_pol[Factor,4],
+	thetaCos=signalCos[Factor]
 	)
 
 file<-ofn("RedDiamonds.txt")
 write.table(RedDiamonds, file)
 close(file)
 
-factor <- factor & !highResMax[plus]
+Factor<-no60hz[plus] & ksVetoP & !highResMax[plus] & highDxAny
 
-RedPoints<-data.frame(Band=band[plus,4][factor & highDxAny],
-	pulsarUL=pulsarUL[plus][factor & highDxAny],
-	circularUL=circularUL[factor & highDxAny],
-	maxDx=pmax(max_dx[cross,5], 
-		max_dx[plus,5], 
-		max_dx[pol1,5], 
-		max_dx[pol2,5], 
-		max_dx[circ,5])[factor & highDxAny],
-	resMax=hist_residuals[plus,4][factor & highDxAny],
-	spindown=circularUL[factor & highDxAny]*0.0
+RedPoints<-data.frame(Band=band[plus,4][Factor],
+	pulsarUL=pulsarUL[plus][Factor],
+	circularUL=circularUL[Factor],
+	resMax=highRes[plus][Factor],
+	fdot=spindown[plus,3][Factor],
+	maxDx=p_max_dx[Factor],
+	RA=p_max_dx_pol[Factor, 2],
+	DEC=p_max_dx_pol[Factor, 3],
+	f0=p_max_dx_pol[Factor, 8],
+	ul=p_max_dx_pol[Factor, 6],
+	pol=p_max_dx_pol[Factor,4],
+	thetaCos=signalCos[Factor]
 	)
 
 file<-ofn("RedPoints.txt")
 write.table(RedPoints, file)
 close(file)
-#
-# TMedians based veto
-#
-#plot(band[,4], TMedian[,5])
-#points(band[,4], -32.95+(band[,4]-200.0)*0.85/500-0.45*(band[,4]-200)*(band[,4]-700)/500^2, type="l", col="red")
-
