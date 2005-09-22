ae6c1da464c4330a452c46555e94150e6ebab636
duncan <duncan>
2005-09-22 00:00:11 +0000

    added lfn map table

--- glue/src/conf/db2/lfn.sql
+++ glue/src/conf/db2/lfn.sql
@@ -10,18 +10,28 @@ CREATE TABLE lfn
 -- Database which created this entry
       creator_db         INTEGER NOT NULL WITH DEFAULT 1,
 
--- Unique process ID of the process which wrote this lfn
+-- Unique process ID of the process which wrote this lfn entry
       process_id         CHAR(13) FOR BIT DATA NOT NULL,
+
+-- Unique identifier for this LFN
+      lfn_id             CHAR(13) FOR BIT DATA NOT NULL,
 
 -- logical file name
       lfn                VARCHAR(255) NOT NULL,
+
+-- optional comment string
+      comment            VARCHAR(240),
 
 -- Insertion time (automatically assigned by the database)
       insertion_time     TIMESTAMP WITH DEFAULT CURRENT TIMESTAMP,
 
 -- LFNs in this table must be unique
       CONSTRAINT lfn_pk
-      PRIMARY KEY (lfn)
+      PRIMARY KEY (lfn),
+
+-- Secondary key to ensure that the lfn ID is unique
+      CONSTRAINT lfn_sk
+      UNIQUE (lfn_id)
 )
 -- The following line is needed for this table to be replicated to other sites
 DATA CAPTURE CHANGES
---
+++ glue/src/conf/db2/segment_lfn_map.sql
@@ -0,0 +1,46 @@
+CREATE TABLE segment_lfn_map
+(
+-- Create a map between segments and logical file names (LFNs)
+
+-- Database which created this entry
+      creator_db         INTEGER NOT NULL WITH DEFAULT 1,
+
+-- Process ID of the program which created this map
+      process_id         CHAR(13) FOR BIT DATA NOT NULL,
+
+-- ID of the segment
+      segment_id         CHAR(13) FOR BIT DATA NOT NULL,
+
+-- ID of the logiacal file name
+      segment_lfn_id     CHAR(13) FOR BIT DATA NOT NULL,
+
+-- Insertion time (automatically assigned by the database)
+      insertion_time     TIMESTAMP WITH DEFAULT CURRENT TIMESTAMP,
+
+      CONSTRAINT seglfnmap_pk
+      PRIMARY KEY (process_id, segment_id, lfn_id),
+
+      CONSTRAINT seglfnmap_fk_pid
+      FOREIGN KEY (process_id, creator_db)
+          REFERENCES process(process_id, creator_db)
+
+      CONSTRAINT seglfnmap_fk_sid
+      FOREIGN KEY (segment_id)
+          REFERENCES segment(segment_id),
+
+      CONSTRAINT seglfnmap_fk_sdid
+      FOREIGN KEY (lfn_id)
+          REFERENCES lfn(lfn_id)
+)
+-- The following line is needed for this table to be replicated to other sites
+DATA CAPTURE CHANGES
+;
+-- Create an index based on process ID
+CREATE INDEX seglfnmap_pid on segment_lfn_map(process_id);
+;
+-- Create an index based on segment ID
+CREATE INDEX seglfnmap_sid on segment_lfn_map(segment_id);
+;
+-- Create an index based on segment lfninintion ID
+CREATE INDEX seglfnmap_lfnid on segment_lfn_map(lfn_id);
+;
