357e45f8e698b772c807cebefdcde966fc7bf9ba
kipp <kipp>
2005-09-25 22:15:15 +0000

    Move the function calls around a bit.

--- glue/test/segmentsUtils_verify.py
+++ glue/test/segmentsUtils_verify.py
@@ -11,16 +11,16 @@ from glue import segmentsUtils
 
 class test_fromsegwizard(unittest.TestCase):
 	def test(self):
-		data = """# This is a comment
+		data = StringIO.StringIO("""# This is a comment
  # This is another comment
 	# Again a comment
 1  10 100 90
 2 110 120 10# Here's a comment
 3 125 130 5 # Another one
 
-4   0 200 200"""
+4   0 200 200""")
 		correct = segmentlist([segment(10, 100), segment(110, 120), segment(125, 130), segment(0, 200)])
-		self.assertEqual(correct, segmentsUtils.fromsegwizard(StringIO.StringIO(data), strict=True))
+		self.assertEqual(correct, segmentsUtils.fromsegwizard(data, strict=True))
 
 
 #
