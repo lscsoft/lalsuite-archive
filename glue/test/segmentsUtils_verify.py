aeb7ac10458c5c22823593973fb40ef15e0f604e
kipp <kipp>
2005-09-25 22:50:06 +0000

    Add a test of the segwizard writing routine.

--- glue/test/segmentsUtils_verify.py
+++ glue/test/segmentsUtils_verify.py
@@ -9,8 +9,11 @@ from glue import segmentsUtils
 # Define the components of the test suite.
 #
 
-class test_fromsegwizard(unittest.TestCase):
-	def test(self):
+class test_segwizard(unittest.TestCase):
+	def test_fromsegwizard(self):
+		"""
+		Test segwizard parsing.
+		"""
 		data = StringIO.StringIO("""# This is a comment
  # This is another comment
 	# Again a comment
@@ -22,12 +25,23 @@ class test_fromsegwizard(unittest.TestCase):
 		correct = segmentlist([segment(10, 100), segment(110, 120), segment(125, 130), segment(0, 200)])
 		self.assertEqual(correct, segmentsUtils.fromsegwizard(data, strict=True))
 
+	def test_tofromseqwizard(self):
+		"""
+		Check that the segwizard writing routine's output is parsed
+		correctly.
+		"""
+		data = StringIO.StringIO()
+		correct = segmentlist([segment(10, 100), segment(110, 120), segment(125, 130), segment(0, 200)])
+		segmentsUtils.tosegwizard(data, correct)
+		data.seek(0)
+		self.assertEqual(correct, segmentsUtils.fromsegwizard(data, strict=True))
+
 
 #
 # Construct and run the test suite.
 #
 
 suite = unittest.TestSuite()
-suite.addTest(unittest.makeSuite(test_fromsegwizard))
+suite.addTest(unittest.makeSuite(test_segwizard))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
