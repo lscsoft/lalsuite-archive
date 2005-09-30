681b3801ac078947058bfba617ae1b8267f6ebfa
kipp <kipp>
2005-09-30 21:53:39 +0000

    Make the string representation of a segment prettier.

--- glue/glue/segments.py
+++ glue/glue/segments.py
@@ -124,7 +124,7 @@ class segment(tuple):
 		return "segment(" + str(self[0]) + ", " + str(self[1]) + ")"
 
 	def __str__(self):
-		return str(self[0]) + "_" + str(self[1])
+		return "[" + str(self[0]) + " ... " + str(self[1]) + "]"
 
 	# accessors
 
