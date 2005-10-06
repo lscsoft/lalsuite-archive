2e927cbc7555f9841b02ad5703bcb2bb6ab1d05e
duncan <duncan>
2005-10-06 02:43:24 +0000

    removed init file

--- glue/glue/segfindserver/segments_1_7/__init__.py
+++
@@ -1,9 +0,0 @@
-# Initialization module for glue segments version 1.7
-#
-# $Id$
-#
-__author__ = 'Duncan Brown <dbrown@ligo.caltech.edu>'
-__date__ = '$Date$'
-__version__ = '$Revision$'[11:-2]
-# $Source$
-
