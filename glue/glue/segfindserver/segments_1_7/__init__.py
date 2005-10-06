c20990332117964dad2a9ec3dbc49fcd2f9f0a54
duncan <duncan>
2005-10-06 02:18:58 +0000

    added an __init__ file so we can use __import__ with this module

---
+++ glue/glue/segfindserver/segments_1_7/__init__.py
@@ -0,0 +1,9 @@
+# Initialization module for glue segments version 1.7
+#
+# $Id$
+#
+__author__ = 'Duncan Brown <dbrown@ligo.caltech.edu>'
+__date__ = '$Date$'
+__version__ = '$Revision$'[11:-2]
+# $Source$
+
