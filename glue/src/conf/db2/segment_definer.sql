4055de163116f6a1e7a07fe790ca542b4b2aa959
duncan <duncan>
2005-10-22 01:22:07 +0000

    added a run column to hold the science run name

--- glue/src/conf/db2/segment_definer.sql
+++ glue/src/conf/db2/segment_definer.sql
@@ -14,6 +14,8 @@ CREATE TABLE segment_definer
 -- Unique identification string for this segment definition
       segment_def_id     CHAR(13) FOR BIT DATA NOT NULL,
 
+-- The name of the run which this segment corresponds to (S2, A4, M7, etc.)
+      run                CHAR(4) NOT NULL,
 -- Interferometer(s) for which these segments are meaningful
       ifos               CHAR(12) NOT NULL,
 -- Descriptive name for this group of segments (e.g. 'Science', 'Dust')
@@ -32,7 +34,7 @@ CREATE TABLE segment_definer
       insertion_time     TIMESTAMP WITH DEFAULT CURRENT TIMESTAMP,
 
       CONSTRAINT segdef_pk
-      PRIMARY KEY (ifos, name, version),
+      PRIMARY KEY (run, ifos, name, version),
 
       CONSTRAINT segdef_sk
       UNIQUE (segment_def_id),
@@ -46,6 +48,9 @@ DATA CAPTURE CHANGES
 ;
 -- Create a clustering index for quicker scanning of a given segment type
 CREATE INDEX segdef_cind ON segment_definer(name, version) CLUSTER
+;
+-- Create an index based on the run name
+CREATE INDEX segdef_run ON segment_definer(run)
 ;
 -- Create an index based on the keys that state_segment needs
 CREATE INDEX segdef_ind_value ON segment_definer(ifos,state_vec_major,state_vec_minor)
