d6e6393aa05c2e40b99535b48b1884dcd250967d
duncan <duncan>
2005-11-01 01:41:21 +0000

    initial entry

---
+++ glue/src/conf/q_replication/start_1.sql
@@ -0,0 +1,83 @@
+--Beginning of script 1--   DatabaseDB2LUOW (SEG_LHO) [WARNING***Please do not alter this line]--
+-- CONNECT TO SEG_LHO USER XXXX using XXXX;
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'PROCESS0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'PROCESS_PARAMS0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'LFN0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'GRIDCERT0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FILTER0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FILTER_PARAMS0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_CHANLIST0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_WRITER0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_LOC0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_DEFINER0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_DEF_MAP0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_LFN_MAP0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_VALUE0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_STATISTICS0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_SPECTRUM0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_CSD0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_COMMENT0002');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_MIME0002');
+-- COMMIT;
---
+++ glue/src/conf/q_replication/start_2.sql
@@ -0,0 +1,83 @@
+--Beginning of script 1--   DatabaseDB2LUOW (SEG_LHO) [WARNING***Please do not alter this line]--
+-- CONNECT TO SEG_LHO USER XXXX using XXXX;
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'PROCESS0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'PROCESS_PARAMS0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'LFN0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'GRIDCERT0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FILTER0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FILTER_PARAMS0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_CHANLIST0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_WRITER0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'FRAMESET_LOC0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_DEFINER0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_DEF_MAP0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SEGMENT_LFN_MAP0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_VALUE0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_STATISTICS0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_SPECTRUM0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_CSD0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_COMMENT0004');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_type, signal_subtype, signal_input_in)
+ VALUES
+ ('CMD', 'CAPSTART', 'SUMM_MIME0004');
+-- COMMIT;
