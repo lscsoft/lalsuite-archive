4fe4e6212a82029e5ac1850e1141f3aede646ad2
duncan <duncan>
2005-11-02 05:54:22 +0000

    initial entry

---
+++ glue/src/conf/q_replication/loaddone_1.sql
@@ -0,0 +1,83 @@
+--Beginning of script 1--   DatabaseDB2LUOW (SEG_LHO) [WARNING***Please do not alter this line]--
+-- CONNECT TO SEG_LHO USER XXXX using XXXX;
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'PROCESS0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'PROCESS_PARAMS0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'LFN0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'GRIDCERT0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FILTER0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FILTER_PARAMS0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_CHANLIST0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_WRITER0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_LOC0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_DEFINER0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_DEF_MAP0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_LFN_MAP0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_VALUE0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_STATISTICS0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_SPECTRUM0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_CSD0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_COMMENT0002', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_MIME0002', 'P');
+-- COMMIT;
---
+++ glue/src/conf/q_replication/loaddone_2.sql
@@ -0,0 +1,83 @@
+--Beginning of script 1--   DatabaseDB2LUOW (SEG_LHO) [WARNING***Please do not alter this line]--
+-- CONNECT TO SEG_LHO USER XXXX using XXXX;
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'PROCESS0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'PROCESS_PARAMS0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'LFN0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'GRIDCERT0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FILTER0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FILTER_PARAMS0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_CHANLIST0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_WRITER0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'FRAMESET_LOC0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_DEFINER0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_DEF_MAP0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SEGMENT_LFN_MAP0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_VALUE0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_STATISTICS0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_SPECTRUM0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_CSD0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_COMMENT0004', 'P');
+INSERT INTO ASN.IBMQREP_SIGNAL
+ (signal_time, signal_type, signal_subtype, signal_input_in, signal_state)
+ VALUES
+ (CURRENT TIMESTAMP, 'CMD', 'LOADDONE', 'SUMM_MIME0004', 'P');
+-- COMMIT;
