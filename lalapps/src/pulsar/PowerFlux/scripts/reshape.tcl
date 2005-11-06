243222c319738ef7260ae309359314ac3aa4389e
volodya <volodya>
2005-11-06 05:07:15 +0000

    Latest round of changes. Check that we got as much data as we expected and put NaNs otherwise. This is mostly useful for checking up on runs still being computed. Construct header in such a way as to mark position of the field in powerflux.log file - this R script immune to adding new fields, etc.

--- lalapps/src/pulsar/PowerFlux/scripts/reshape.tcl
+++ lalapps/src/pulsar/PowerFlux/scripts/reshape.tcl
@@ -12,34 +12,34 @@ foreach $PARAMS_FORMAT $PARAMS {
         }
 
 set FIELDS {
-	"^useful band start"	band 0		{3 3}
-	"^seconds elapsed"	cputime 0  	{2 2}
-	"^fine_grid npoints"	npoints 0 	{3 3}
-	"^max_high_ul:"	max_high_ul 3  		{1 end}
-	"^max_circ_ul:"	max_circ_ul 3  		{1 end}
-	"\\(TMedian\\):"	TMedian 0 	{4 4}
-	"median:"	median 0 		{2 2}
-	"qlines:"	qlines 0 		{2 2}
-	"qmost :"	qmost 0			{3 3}
-	"^side_cut"	side_cut 0		{2 2}
-	"^Maximum bin shift"	max_shift 3	{3 3}
-	"^Minimum bin shift"	min_shift 3	{3 3}
-	"^hist_residuals:"	hist_residuals 0	{1 4}
-        "^max_dx:"       max_dx 1	{1 end}
-        "^largest:"  largest 1	{1 end}
-        "^masked:"   masked 1	{1 end}
-	"^spindown  :" spindown 3	{1 end}
+	"^useful band start"	band 0		{3 3}		1
+	"^seconds elapsed"	cputime 0  	{2 2}		1
+	"^fine_grid npoints"	npoints 0 	{3 3}		1
+	"^max_high_ul:"	max_high_ul 3  		{1 end}		X
+	"^max_circ_ul:"	max_circ_ul 3  		{1 end}		X
+	"\\(TMedian\\):"	TMedian 0 	{4 4}		1
+	"median:"	median 0 		{2 2}		1
+	"qlines:"	qlines 0 		{2 2}		1
+	"qmost :"	qmost 0			{3 3}		1
+	"^side_cut"	side_cut 0		{2 2}		1
+	"^Maximum bin shift"	max_shift 3	{3 3}		1
+	"^Minimum bin shift"	min_shift 3	{3 3}		1
+	"^hist_residuals:"	hist_residuals 0	{1 4}	4
+        "^max_dx:"       max_dx 1	{1 7}			7
+        "^largest:"  largest 1	{1 end}				X
+        "^masked:"   masked 1	{1 end}				X
+	"^spindown  :" spindown 3	{2 2}			1
 	}
 
-set FIELDS_LAYOUT {exp var pol fields}
+set FIELDS_LAYOUT {exp var pol fields EC}
 
 for { set band 0 } { $band < $NBANDS } { incr band } {
-	lappend FIELDS "^max_high_ul_band: $band " "max_high_ul_band.$band" 3	{1 end}
-	lappend FIELDS "^max_circ_ul_band: $band " "max_circ_ul_band.$band" 3	{1 end}
-        lappend FIELDS "^max_band: $band " "max_band.$band" 1	{1 end}
-        lappend FIELDS "^masked_max_band: $band " "masked_max_band.$band" 1	{1 end}
-	lappend FIELDS "^max_ratio: $band " "max_ratio.$band" 1	{1 end}
-	lappend FIELDS "hist_.*_ks_test: $band " "ks_hist.$band" 1		{1 4}
+	lappend FIELDS "^max_high_ul_band: $band " "max_high_ul_band.$band" 3	{1 end}		X
+	lappend FIELDS "^max_circ_ul_band: $band " "max_circ_ul_band.$band" 3	{1 end}		X
+        lappend FIELDS "^max_band: $band " "max_band.$band" 1	{1 end}				X
+        lappend FIELDS "^masked_max_band: $band " "masked_max_band.$band" 1	{1 end}		X
+	lappend FIELDS "^max_ratio: $band " "max_ratio.$band" 1	{1 end}				X
+	lappend FIELDS "hist_.*_ks_test: $band " "ks_hist.$band" 1		{1 4}		4
         }
 
 
@@ -48,12 +48,21 @@ foreach $FIELDS_LAYOUT $FIELDS {
 	}
 
 # Return subsetted list
-proc list_subset { L subset } {
+proc list_subset { L subset EC } {
 set LL {}
 foreach {first last} $subset {
 	lappend LL [lrange $L $first $last]
 	}
-return [join [concat $LL] "\t"]
+set S [join $LL " "]
+if { $EC != "X" } {
+	set L [split $S " "]
+	if { [llength $L ] != $EC } {
+		set L  {}
+		for { set i 0 } { $i < $EC } { incr i } { lappend L NaN }
+		set S [join $L " "]
+		}
+	}
+return $S
 }
 
 proc list_header { root L subset } {
@@ -64,7 +73,7 @@ foreach {first last} $subset {
 		lappend LL "${root}.$i"
 		}
 	}
-return [join $LL "\t"]
+return [join $LL " "]
 }
 
 # Comment this out to check for completed jobs
@@ -108,32 +117,38 @@ foreach $FIELDS_LAYOUT $FIELDS {
 
 file mkdir $STATS_DIR
 
-set FOUT [open $STATS_DIR/stat${STATS_SUFFIX}.dat "a"]
+set OFILENAME "$STATS_DIR/stat${STATS_SUFFIX}.dat"
 
-puts -nonewline $FOUT "LogFile"
-# Write out header
-foreach $FIELDS_LAYOUT $FIELDS {
-	set header [list_header $var [lindex [set $var] end] $fields]
-	puts -nonewline $FOUT "\t$header"
+set appending [file exists $OFILENAME]
+
+set FOUT [open $OFILENAME "a"]
+
+if { !$appending } {
+	puts -nonewline $FOUT "LogFile"
+	# Write out header
+	foreach $FIELDS_LAYOUT $FIELDS {
+		set header [list_header $var [lindex [set $var] end] $fields]
+		puts -nonewline $FOUT " $header"
+		}
+	puts $FOUT ""
 	}
-puts $FOUT ""
 
 for { set i 0 } { $i < $NPOL*$spindown_count } { incr i } {
 	puts -nonewline $FOUT $argv
 	foreach $FIELDS_LAYOUT $FIELDS {
-		puts -nonewline $FOUT "\t"
+		puts -nonewline $FOUT " "
 		switch -exact $pol {
 			1  	{
-				set value [list_subset [lindex [set $var] [expr $i]] $fields]
-				puts -nonewline $FOUT [join $value "\t"]
+				set value [list_subset [lindex [set $var] [expr $i]] $fields $EC]
+				puts -nonewline $FOUT $value
 				} 
 			3 	{
-				set value [list_subset [lindex [set $var] [expr $i/$NPOL]] $fields]
-				puts -nonewline $FOUT [join $value "\t"]
+				set value [list_subset [lindex [set $var] [expr $i/$NPOL]] $fields $EC]
+				puts -nonewline $FOUT $value
 				}
 			0	{
-				set value [list_subset [lindex [set $var] end] $fields]
-				puts -nonewline $FOUT [join $value "\t"]
+				set value [list_subset [lindex [set $var] end] $fields $EC]
+				puts -nonewline $FOUT $value
 				}
 			}
 		}
