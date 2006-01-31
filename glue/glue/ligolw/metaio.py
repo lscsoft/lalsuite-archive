7fcedce8f0c7d6c453f4bd564c279b8bad5edea7
kipp <kipp>
2006-01-31 23:46:38 +0000

    Add more column types.

--- glue/glue/ligolw/metaio.py
+++ glue/glue/ligolw/metaio.py
@@ -10,13 +10,16 @@ import ligolw
 
 
 StringTypes = ["ilwd:char", "ilwd:char_u", "lstring", "string"]
-IntTypes = ["int_2s", "int_4s", "int_8s", "int"]
+IntTypes = ["int_2s", "int_2u", "int_4s", "int_4u", "int_8s", "int_8u", "int"]
 FloatTypes = ["real_4", "real_8", "float", "double"]
 Types = StringTypes + IntTypes + FloatTypes
 ToNumArrayType = {
 	"int_2s": "Int16",
+	"int_2u": "UInt16",
 	"int_4s": "Int32",
+	"int_4u": "UInt32",
 	"int_8s": "Int64",
+	"int_8u": "UInt64",
 	"int": "Int32",
 	"real_4": "Float32",
 	"real_8": "Float64",
