612c6a2c87a92aad9df175b7de72d6fb194fd653
kipp <kipp>
2006-03-09 18:13:02 +0000

    Oops, reversed the slices in the __date__ and __version__ strings.

--- glue/glue/ligolw/docutils.py
+++ glue/glue/ligolw/docutils.py
@@ -3,8 +3,8 @@ High-level document manipulation utilities.
 """
 
 __author__ = "Kipp Cannon <kipp@gravity.phys.uwm.edu>"
-__date__ = "$Date$"[11:-2]
-__version__ = "$Revision$"[7:-2]
+__date__ = "$Date$"[7:-2]
+__version__ = "$Revision$"[11:-2]
 
 import ligolw
 import metaio
--- glue/glue/ligolw/lsctables.py
+++ glue/glue/ligolw/lsctables.py
@@ -7,8 +7,8 @@ interested users.
 """
 
 __author__ = "Kipp Cannon <kipp@gravity.phys.uwm.edu>"
-__date__ = "$Date$"[11:-2]
-__version__ = "$Revision$"[7:-2]
+__date__ = "$Date$"[7:-2]
+__version__ = "$Revision$"[11:-2]
 
 import re
 from xml import sax
