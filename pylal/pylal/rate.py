89f4c21bb05d7b64b4a026b04213e7dd300dcd50
kipp <kipp>
2006-04-30 22:09:41 +0000

    Add comments and author and license information.

--- pylal/pylal/rate.py
+++ pylal/pylal/rate.py
@@ -1,6 +1,45 @@
+# $Id$
+#
+# Copyright (C) 2006  Kipp C. Cannon
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+# =============================================================================
+#
+# Preamble
+#
+# =============================================================================
+#
+
+"""
+This module provides code to convolve impulsive events with a window
+function to produce a smooth data set.  The convolution is
+integral-preserving, and so is particularly-well suited for use in
+eliminating binning artifacts from histograms, and other rate-like data
+sets.
+"""
+
 import math
 import numarray
 from numarray import convolve
+
+__author__ = "Kipp Cannon <kipp@gravity.phys.uwm.edu>"
+__version__ = "$Revision$"[11:-2]
+__date__ = "$Date$"[7:-2]
+
 
 #
 # =============================================================================
@@ -10,20 +49,27 @@ from numarray import convolve
 # =============================================================================
 #
 
-def GaussianWindow(halfwidth):
+def gaussian_window(halfwidth):
 	"""
-	Generate a normalized Gaussian window (integral = 1).
+	Generate a normalized (integral = 1) Gaussian window.
 	"""
 	bins_per_unit = 10.0 / halfwidth
 	return numarray.exp(-numarray.arrayrange(-10.0 * halfwidth, +10.0 * halfwidth, 1.0/bins_per_unit)**2.0 / (2.0 * halfwidth**2.0)) / math.sqrt(2.0 * math.pi) / halfwidth
 
 
+def tophat_window(halfwidth):
+	"""
+	Generate a normalized (integral = 1) top-hat window.
+	"""
+	bins_per_unit = 10.0 / halfwidth
+	return numarray.ones(2.0 * halfwidth / bins_per_unit) / (2.0 * halfwidth)
+
+
 class Rate(object):
 	"""
-	An object for binning and smoothing data to compute a moving average
-	rate.
+	An object for binning and smoothing impulsive data.
 	"""
-	def __init__(self, segment, halfwidth):
+	def __init__(self, segment, halfwidth, windowfunc = gaussian_window):
 		"""
 		Initialize the bins for the given segment and width.
 		"""
@@ -32,6 +78,7 @@ class Rate(object):
 		self.start = segment[0]
 		self.xvals = numarray.arrayrange(0.0, float(segment.duration()), 1.0/self.bins_per_unit) + float(segment[0])
 		self.yvals = numarray.zeros(len(self.xvals), "Float32")
+		self.set_window(windowfunc)
 
 	def bin(self, x):
 		"""
@@ -51,30 +98,53 @@ class Rate(object):
 		"""
 		self.yvals[self.bin(x)] += weight
 
+	def set_window(self, windowfunc):
+		"""
+		Set the window function.
+		"""
+		self.window = windowfunc
+
 	def convolve(self):
 		"""
-		Generate a window, and convolve it with the binned weights
-		to generate rate data.
+		Convolve the binned weights with the window to smooth the data
+		set.
 		"""
-		self.yvals = convolve.convolve(self.yvals, GaussianWindow(self.halfwidth), mode=convolve.SAME)
+		self.yvals = convolve.convolve(self.yvals, self.window(self.halfwidth), mode=convolve.SAME)
 		return self
 
 
 def smooth(impulses, segment, width, weights = None):
+	"""
+	Given a list of the co-ordinates of impulsive events, an optional
+	matching list of weights for those events, a segment and a
+	characteristic length, this function returns two arrays:  an array
+	of x co-ordinates, and an array of corresponding y co-ordinates
+	describing the rate of impulse events.
+
+	The y values returned carry units of "weight per unit of x".  The
+	default weight of each impulse is 1.0 if weights are not provided.
+
+	Example:
+		# generate regular impulses at a rate of 2 Hz from 10 s to
+		# 90 s.
+		impulses = numarray.arrayrange(10, 90, 0.5)
+
+		# using the impulses, generate rate data in the interval
+		# 0 s to 100 s with a 1 s Gaussian window.
+		x, y = smooth(impulses, segment(0, 100), 1)
+
+		# plot
+		plot(x, y)
+	"""
 	rate = Rate(segment, width / 2.0)
-	if True:
-		if weights == None:
-			for x in impulses:
-				if segment[0] <= x < segment[1]:
-					rate[x] = 1.0
-		else:
-			for n, x in enumerate(impulses):
-				if segment[0] <= x < segment[1]:
-					rate[x] = weights[n]
+	if weights != None:
+		for n, x in enumerate(impulses):
+			if segment[0] <= x < segment[1]:
+				rate[x] = weights[n]
 	else:
-		# inject pulses at regular intervals to test normalization
-		for x in numarray.arrayrange(float(segment[0]), float(segment[1]), 0.5):
-			rate[x] = 1.0
+		for x in impulses:
+			if segment[0] <= x < segment[1]:
+				rate[x] = 1.0
 	rate.convolve()
 	return rate.xvals, rate.yvals
 
