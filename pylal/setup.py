dc54311e74e6ba029b43bfd9697b66cb522c18a3
sfairhur <sfairhur>
2006-05-16 17:05:53 +0000

    added hipecoire

--- pylal/setup.py
+++ pylal/setup.py
@@ -67,6 +67,7 @@ setup(
 		os.path.join("bin", "plotdistance"),
 		os.path.join("bin", "plottisi"),
 		os.path.join("bin", "s3_statistic"),
+		os.path.join("bin", "hipecoire"),
 		os.path.join("bin", "lalapps_ll2cache"),
 		os.path.join("bin", "lalapps_path2cache"),
 		os.path.join("bin", "lalapps_pire"),
