cf528081118ef61eb3d02bf39e5d5a5b3d3626b8
duncan <duncan>
2006-09-22 20:50:33 +0000

    addded version handling as requested by Igor in pr-34

--- glue/glue/__init__.py
+++ glue/glue/__init__.py
@@ -4,6 +4,10 @@
 #
 __author__ = 'Duncan Brown <dbrown@ligo.caltech.edu>'
 __date__ = '$Date$'
-__version__ = '$Name$'
+__cvs_tag__ = '$Name$'
+try:
+  __version__ = __cvs_tag__.split('-')[1] + '.' + __cvs_tag__.split('-')[2][0:-2]
+except IndexError:
+  __version__ = ''
 # $Source$
 
