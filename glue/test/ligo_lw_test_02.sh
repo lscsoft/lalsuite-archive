8d488d7b317b031aac672f6910943c24156736dd
kipp <kipp>
2006-10-02 17:23:11 +0000

    Add a pass/fail message.

--- glue/test/ligo_lw_test_02.sh
+++ glue/test/ligo_lw_test_02.sh
@@ -1,1 +1,7 @@
-ligolw_add inspiral_event_id_test_in?.xml | diff -q -s - inspiral_event_id_test_out.xml
+if ligolw_add inspiral_event_id_test_in?.xml | diff -q -s - inspiral_event_id_test_out.xml ; then
+	echo "Pass"
+	true
+else
+	echo "Fail"
+	false
+fi
