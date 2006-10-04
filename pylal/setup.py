22f6c2dd1770954391d8c301251e68452759b32d
sfairhur <sfairhur>
2006-10-04 20:06:54 +0000

    added script to write an inspiral upper limit web page based on the output from upperlimit.py

--- pylal/setup.py
+++ pylal/setup.py
@@ -58,6 +58,7 @@ setup(
 		os.path.join("bin", "plotsiminspiral"),
 		os.path.join("bin", "plotnumgalaxies"),
 		os.path.join("bin", "upperlimit.py"),
+		os.path.join("bin", "write_iul_page"),
 		os.path.join("bin", "lalapps_compute_posterior"),
 		os.path.join("bin", "plotinspiral"),
 		os.path.join("bin", "plotinspinj"),
