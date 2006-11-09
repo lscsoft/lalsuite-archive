7f227555eaa02a85889eabb5ef749fee1974575a
duncan <duncan>
2006-11-09 01:15:15 +0000

    create user env scripts as part of build process

--- glue/setup.py
+++ glue/setup.py
@@ -4,7 +4,55 @@
 
 import os
 from distutils.core import setup, Extension
+from distutils.command import install
+from distutils import log
 from sys import version_info
+
+class glue_install(install.install):
+  def run(self):
+
+    # create the user env scripts
+    if self.install_purelib == self.install_platlib:
+      glue_pythonpath = self.install_purelib
+    else:
+      glue_pythonpath = self.install_platlib + ":" + self.install_purelib
+    
+    log.info("creating glue-user-env.sh script")
+    env_file = open(os.path.join('etc','glue-user-env.sh'),'w')
+    print >> env_file, "# Source this file to access GLUE"
+    print >> env_file, "GLUE_PREFIX=" + self.prefix
+    print >> env_file, "export GLUE_PREFIX"
+    print >> env_file, "PATH=" + self.install_scripts + ":${PATH}"
+    print >> env_file, "PYTHONPATH=" + glue_pythonpath + ":${PYTHONPATH}"
+    print >> env_file, "LD_LIBRARY_PATH=" + self.install_platlib + ":${LD_LIBRARY_PATH}"
+    print >> env_file, "DYLD_LIBRARY_PATH=" + self.install_platlib + ":${DYLD_LIBRARY_PATH}"
+    print >> env_file, "export PATH PYTHONPATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH"
+    env_file.close()
+
+    log.info("creating glue-user-env.csh script")
+    env_file = open(os.path.join('etc','glue-user-env.csh'),'w')
+    print >> env_file, "# Source this file to access GLUE"
+    print >> env_file, "setenv GLUE_PREFIX " + self.prefix
+    print >> env_file, "setenv PATH " + self.install_scripts + ":${PATH}"
+    print >> env_file, "if ( $?PYTHONPATH ) then"
+    print >> env_file, "  setenv PYTHONPATH " + glue_pythonpath + ":${PYTHONPATH}"
+    print >> env_file, "else"
+    print >> env_file, "  setenv PYTHONPATH " + glue_pythonpath
+    print >> env_file, "endif"
+    print >> env_file, "if ( $?LD_LIBRARY_PATH ) then"
+    print >> env_file, "  setenv LD_LIBRARY_PATH " + self.install_platlib + ":${LD_LIBRARY_PATH}"
+    print >> env_file, "else"
+    print >> env_file, "  setenv LD_LIBRARY_PATH " + self.install_platlib
+    print >> env_file, "endif"
+    print >> env_file, "if ( $?DYLD_LIBRARY_PATH ) then"
+    print >> env_file, "  setenv DYLD_LIBRARY_PATH " + self.install_platlib + ":${DYLD_LIBRARY_PATH}"
+    print >> env_file, "else"
+    print >> env_file, "  setenv DYLD_LIBRARY_PATH " + self.install_platlib
+    print >> env_file, "endif"
+    env_file.close()
+
+    # now run the installer
+    install.install.run(self)
 
 # Add py_modules to the argument list for setup() if this is at least Python
 # 2.3.  Duplicating this function call is ugly but it solves the problem.
@@ -19,6 +67,7 @@ if version_info >= (2, 3):
     url = "http://www.lsc-group.phys.uwm.edu/daswg/",
     license = 'See file LICENSE',
     packages = [ 'glue', 'glue.ligolw', 'glue.ligolw.utils' ],
+    cmdclass = { 'install' : glue_install },
     py_modules = [ 'glue.segfindserver.segments_1_7.segments' ],
     ext_modules = [
       Extension("glue.ligolw.tokenizer", ["glue/ligolw/tokenizer.c"]),
@@ -58,6 +107,7 @@ else:
     url = "http://www.lsc-group.phys.uwm.edu/daswg/",
     license = 'See file LICENSE',
     packages = [ 'glue', 'glue.ligolw' ],
+    cmdclass = { 'install' : glue_install },
     ext_modules = [
       Extension("glue.ligolw.tokenizer", ["glue/ligolw/tokenizer.c"]),
       #Extension("glue.__segments", ["src/segments/segments.c", "src/segments/infinity.c", "src/segments/segment.c", "src/segments/segmentlist.c"], include_dirs = ["src/segments"])
