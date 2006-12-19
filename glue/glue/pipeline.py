022bb2b744cc12c62a07af7fbc11b70aa3d23d85
duncan <duncan>
2006-12-19 00:35:07 +0000

    changed LSCdataFind universe from local to scheduler until condor is fixed

--- glue/glue/pipeline.py
+++ glue/glue/pipeline.py
@@ -2069,7 +2069,7 @@ class LSCDataFindJob(CondorDAGJob, AnalysisJob):
     the LSCdataFind options are read.
     """
     self.__executable = config_file.get('condor','datafind')
-    self.__universe = 'local'
+    self.__universe = 'scheduler'
     CondorDAGJob.__init__(self,self.__universe,self.__executable)
     AnalysisJob.__init__(self,config_file)
     self.__cache_dir = cache_dir
