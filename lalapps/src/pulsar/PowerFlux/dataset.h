888b14b6c7b66c391fed4fe35b43498db3bea77b
volodya <volodya>
2007-02-17 16:56:36 +0000

    Remove duplicated declaration.

--- lalapps/src/pulsar/PowerFlux/dataset.h
+++ lalapps/src/pulsar/PowerFlux/dataset.h
@@ -97,7 +97,6 @@ float datasets_normalizing_weight(void);
 INT64 min_gps(void);
 INT64 max_gps(void);
 void post_init_datasets(void);
-void output_datasets_info(void);
 void datasets_average_detector_speed(double *average_det_velocity);
 float effective_weight_ratio(float target_ra, float target_dec, float source_ra, float source_dec, float bin_tolerance, float spindown_tolerance);
 float stationary_effective_weight_ratio(float target_ra, float target_dec, float bin_tolerance);
