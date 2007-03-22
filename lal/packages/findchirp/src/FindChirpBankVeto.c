696fe93e464de3b6f66883885c01b0a8a40d870c
duncan <duncan>
2007-03-22 01:56:46 +0000

    added function that splits a bank into subbanks

---
+++ lal/packages/findchirp/src/FindChirpBankVeto.c
@@ -0,0 +1,133 @@
+/*----------------------------------------------------------------------- 
+ * 
+ * File Name: FindChirpBankVeto.c
+ *
+ * Author: Brown D. A., and Hanna, C.
+ * 
+ * Revision: $Id$
+ * 
+ *-----------------------------------------------------------------------
+ */
+
+#if 0 
+<lalVerbatim file="FindChirpBankVetoCV">
+Author: Brown, D. A., and Hanna, C.
+$Id$
+</lalVerbatim> 
+
+<lalLaTeX>
+\vfill{\footnotesize\input{FindChirpBankVetoCV}}
+</lalLaTeX> 
+#endif
+
+#include <lal/LALStdlib.h>
+#include <lal/LALConstants.h>
+#include <lal/AVFactories.h>
+#include <lal/DataBuffer.h>
+#include <lal/LALInspiral.h>
+#include <lal/FindChirp.h>
+
+NRCSID (FINDCHIRPBANKVETOC, "$Id$");
+
+/* <lalVerbatim file="FindChirpBankVetoCP"> */
+FindChirpSubBank*
+XLALFindChirpCreateSubBanks(
+    UINT4                      *maxSubBankSize,
+    UINT4                       subBankSize,
+    UINT4                       bankSize,
+    InspiralTemplate           *bankHead
+    )
+/* </lalVerbatim> */
+{
+  static const char *func = "XLALFindChirpCreateSubBanks";
+  UINT4                 i;
+  UINT4                 numSubBanks = 0;
+  UINT4                 subBankRemainder = 0;
+  UINT4                *bankSizes = NULL;
+  FindChirpSubBank     *subBankHead = NULL;
+  FindChirpSubBank     *thisSubBank = NULL;
+  InspiralTemplate     *thisTmplt = NULL;
+  InspiralTemplate     *nextTmplt = NULL;
+
+  numSubBanks = bankSize / subBankSize;
+
+  if ( ! numSubBanks )
+  {
+    /* the bank is smaller than the subbank size, so return the entire */
+    /* template bank as the subbank                                    */
+    maxSubBankSize = bankSize;
+    subBankHead = (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    subBankHead->bankHead = bankHead;
+    subBankHead->subBankSize = bankSize;
+    return subBankHead;
+  }
+
+  subBankRemainder = bankSize % subBankSize;
+
+  /* create an array of subbank sizes with the minimum size */
+  bankSizes = (UINT4 *) LALCalloc( numSubBanks, sizeof(UINT4) );
+
+  for ( i = 0; i < numSubBanks; ++i )
+  {
+    bankSizes[i] = subBankSize;
+  }
+
+  /* disperse the remainder through the subbanks */
+  while( subBankRemainder )
+  {
+    for ( i = 0; i < numSubBanks; ++i )
+    {
+      if ( ! subBankRemainder )
+      {
+        break;
+      }
+      ++bankSizes[i];
+      --subBankRemainder;
+    }
+  }
+
+  /* allocate storage for the subbanks */
+  *maxSubBankSize = 0;
+  for ( i = 0; i < numSubBanks; ++i )
+  {
+    if ( ! subBankHead )
+    {
+      thisSubBank = subBankHead = 
+        (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    }
+    else
+    {
+      thisSubBank = thisSubBank->next = 
+        (FindChirpSubBank *) LALCalloc( 1, sizeof(FindChirpSubBank) );
+    }
+
+    if ( ! thisSubBank )
+    {
+      XLAL_ERROR( func, XLAL_ENOMEM );
+    }
+
+    thisSubBank->subBankSize = bankSizes[i];
+
+    /* store the size of the biggest bank */
+    if ( bankSizes[i] > *maxSubBankSize )
+    {
+      *maxSubBankSize = bankSizes[i];
+    }
+  }
+
+  /* chop up the template bank into subbanks */
+  for ( thisSubBank = subBankHead, nextTmplt = bankHead; thisSubBank; 
+      thisSubBank = thisSubBank->next )
+  {
+    thisTmplt = nextTmplt;
+    thisSubBank->bankHead = thisTmplt;
+    for ( i = 0; i < thisSubBank->subBankSize; ++i )
+    {
+      thisTmplt = thisTmplt->next;
+    }
+    nextTmplt = thisTmplt->next;
+    thisTmplt->next = NULL;
+  }
+
+  return subBankHead;
+}
