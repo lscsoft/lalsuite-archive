c2a24389176e71cf711e0829090bed0006062a2e
patrick <patrick>
2007-04-06 04:33:29 +0000

    added ability to mangle the event_id when reading in sngl inspirals.

--- pylal/pylal/SnglInspiralUtils.py
+++ pylal/pylal/SnglInspiralUtils.py
@@ -28,6 +28,7 @@ from pylal.date import LIGOTimeGPS
 from glue.ligolw import table
 from glue.ligolw import lsctables
 from glue.ligolw import utils
+from glue.ligolw import utils
 #
 # =============================================================================
 #
@@ -37,18 +38,30 @@ from glue.ligolw import utils
 #
 
 
-def ReadSnglInspiralFromFiles(fileList):
+def ReadSnglInspiralFromFiles(fileList, mangle_event_id = False):
   """
   Read the snglInspiral tables from a list of files
   @param fileList: list of input files
   """
   snglInspiralTriggers = None
+  ncoincs = 0
   for thisFile in fileList:
     doc = utils.load_filename(thisFile)
     # extract the sngl inspiral table
     try: snglInspiralTable = \
       table.get_table(doc, lsctables.SnglInspiralTable.tableName)
     except: snglInspiralTable = None
+
+    # if there is an inspiral table, update the event_id to make sure
+    # they are unique across all input files
+    if snglInspiralTable and mangle_event_id:
+      row_dict = {}
+      for trig in snglInspiralTable:  # N
+        if trig.event_id not in row_dict: # log_2 N
+          ncoincs += 1
+          row_dict[trig.event_id] = ReassignEventId(trig, ncoincs)
+        trig.event_id = row_dict[trig.event_id]
+
     if snglInspiralTriggers and snglInspiralTable: 
       snglInspiralTriggers.extend(snglInspiralTable)
     elif not snglInspiralTriggers:
@@ -88,3 +101,32 @@ def CompareSnglInspiral(a, b, twindow = LIGOTimeGPS(0)):
     return 0
   else:
     return cmp(a.get_end(), b.get_end())
+
+#
+# =============================================================================
+#
+#                                  Those damn event ids
+#
+# =============================================================================
+#
+
+def DisectEventId(eventid):
+  """
+  return the three pieces of the event id
+  """
+  x = eventid // 1000000000
+  slidenum = (eventid % 1000000000) // 100000
+  y = eventid % 100000
+
+  return x,slidenum,y
+
+def ReassignEventId(a, newid):
+  """
+  Applies a new event id based on the new id, but preserving the time
+  slide information.  Ugh.  This is a hack.  FIXME!  Please.
+  """
+  newx = 100000000 + (newid // 100000)
+  newy = newid % 100000
+  x,slidenum,y = DisectEventId(a.event_id)
+  return (1000000000 * newx + slidenum * 100000 + newy)
+
