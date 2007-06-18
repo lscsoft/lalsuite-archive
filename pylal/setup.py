227724418dc32bdee5cc7f1d129eeb026b2afef7
kipp <kipp>
2007-06-18 19:03:24 +0000

    Add a basic wrapping of LAL's burstsearch package.

--- pylal/setup.py
+++ pylal/setup.py
@@ -152,7 +152,7 @@ setup(
 			libraries = lal_pkg_config.libs,
 			library_dirs = lal_pkg_config.libdirs,
 			runtime_library_dirs = lal_pkg_config.libdirs
-		)#,
+		),
 		#Extension(
 		#	"pylal.xlal.window",
 		#	["src/xlal/window.c"],
@@ -160,7 +160,15 @@ setup(
 		#	libraries = lal_pkg_config.libs,
 		#	library_dirs = lal_pkg_config.libdirs,
 		#	runtime_library_dirs = lal_pkg_config.libdirs
-		#)
+		#),
+		Extension(
+			"pylal.xlal.burstsearch",
+			["src/xlal/burstsearch.c"],
+			include_dirs = lal_pkg_config.incdirs + [numpy_get_include()],
+			libraries = lal_pkg_config.libs,
+			library_dirs = lal_pkg_config.libdirs,
+			runtime_library_dirs = lal_pkg_config.libdirs
+		)
 	],
 	scripts = [
 		os.path.join("bin", "followup.py"),
