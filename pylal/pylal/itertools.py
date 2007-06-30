5e951962eb6b3673d41e8dc30504d9a3613e5c3c
nvf <nvf>
2007-06-30 21:00:40 +0000

    Added myself to copyright notice, at Kipp's suggestion

--- pylal/pylal/itertools.py
+++ pylal/pylal/itertools.py
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Copyright (C) 2006  Kipp C. Cannon
+# Copyright (C) 2007  Kipp C. Cannon, Nickolas Fotopoulos
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
