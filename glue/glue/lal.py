cf16c13b5dd3d4040e3ef27ebb78dd74395a517d
nvf <nvf>
2007-10-10 19:27:27 +0000

    Added set arithmetic to Cache.

--- glue/glue/lal.py
+++ glue/glue/lal.py
@@ -447,6 +447,7 @@ class Cache(list):
 	"""
 	entry_class = CacheEntry
 	
+	# methods to create new Cache objects
 	def fromfile(cls, fileobj, coltype=LIGOTimeGPS):
 		"""
 		Return a Cache object whose entries are read from an open file.
@@ -466,6 +467,82 @@ class Cache(list):
 		return cache
 	fromfilenames = classmethod(fromfilenames)
 
+	def from_url_list(cls, urllist, coltype=LIGOTimeGPS):
+		"""
+		Return a Cache whose entries are inferred from the URLs
+		in urllist, if possible.  PFN lists will also work; for PFNs, the path
+		will be absolutized and "file://" and "localhost" will be assumed
+		for the schemes and hosts.
+		
+		The filenames must be in the format set forth by DASWG in T050017-00.
+		"""
+		def pfn_to_url(url):
+			scheme, host, path, dummy, dummy = urlparse.urlsplit(url)
+			if scheme == "": path = os.path.abspath(path)
+			return urlparse.urlunsplit((scheme or "file", host or "localhost",
+			                            path, "", ""))
+		return cls([cls.entry_class(pfn_to_url(f), coltype=coltype) \
+		            for f in urllist])
+	from_url_list = classmethod(from_url_list)
+
+	# some set arithmetic to make life better
+	def __isub__(self, other):
+		"""
+		Remove elements from self that are in other.
+		"""
+		end = len(self) - 1
+		for i, elem in enumerate(self[::-1]):
+			if elem in other:
+				del self[end - i]
+		return self
+
+	def __sub__(self, other):
+		"""
+		Return a Cache containing the entries of self that are not in other.
+		"""
+		return self.__class__([elem for elem in self if elem not in other])
+
+	def __ior__(self, other):
+		"""
+		Append entries from other onto self without introducing (new) duplicates.
+		"""
+		self.extend(other - self)
+		return self
+	
+	def __or__(self, other):
+		"""
+		Return a Cache containing all entries of self and other.
+		"""
+		return self.__class__(self[:]).__ior__(other)
+
+	def __iand__(self, other):
+		"""
+		Remove elements in self that are not in other.
+		"""
+		end = len(self) - 1
+		for i, elem in enumerate(self[::-1]):
+			if elem not in other:
+				del self[end - i]
+		return self
+	
+	def __and__(self, other):
+		"""
+		Return a Cache containing the entries of self that are also in other.
+		"""
+		return self.__class__([elem for elem in self if elem in other])
+
+	def unique(self):
+		"""
+		Return a Cache which has every element of self, but without
+		duplication.  Preserve order.  Does not hash, so a bit slow.
+		"""
+		new = self.__class__([])
+		for elem in self:
+			if elem not in new:
+				new.append(elem)
+		return new
+
+	# other useful manipulations
 	def tofile(self, fileobj):
 		"""
 		write a cache object to the fileobj as a lal cache file
