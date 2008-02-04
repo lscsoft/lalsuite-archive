d275595f8534d56e10c2577daee784d8b81102d3
kipp <kipp>
2008-02-04 22:36:24 +0000

    Replace warning with an error.

--- pylal/pylal/itertools.py
+++ pylal/pylal/itertools.py
@@ -1,12 +1,1 @@
-"""
-This code has been moved to the iterutils module in Glue.
-"""
-
-
-import warnings
-
-
-warnings.warn("The pylal.itertools module is deprecated, the code has been migrated to the glue.iterutils module.  This is only a warning, your program will continue to work, but please update your code to import the new Glue module instead because the pylal.itertools module will be deleted in the near future.", DeprecationWarning)
-
-
-from glue.iterutils import *
+raise ImportError, "The pylal.itertools module is obsolete, the code has been migrated to the glue.iterutils module."
