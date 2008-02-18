ecbf259161e476fc75cd5840ad0dbf167fd9f247
nvf <nvf>
2008-02-18 23:09:29 +0000

    Add HistogramInverseExpectationPlot.

--- pylal/pylal/plotthinca.py
+++ pylal/pylal/plotthinca.py
@@ -26,13 +26,16 @@ __date__ = "$Date$"
 
 import bisect
 itertools = __import__("itertools")
-import operator
-import warnings
+import sys
+
+from glue import iterutils
+from pylal import rate
+from pylal import viz
 
 import numpy
 import pylab
 
-import scipy.optimize
+epsilon = 1e-16
 
 ##############################################################################
 # Supporting classes
@@ -78,6 +81,9 @@ class FitError(Exception):
 
 class ExtrapolatingBackgroundEstimator(BackgroundEstimator):
   def finalize(self):
+    
+    import scipy.optimize
+    
     if self.finalized:
       raise ValueError, "ExtrapolatingBackgroundEstimator has already been finalized"
     BackgroundEstimator.finalize(self)
@@ -126,6 +132,8 @@ class InverseExpectationPlot(object):
     self.fig = pylab.figure(fig_num)
     self.ax = self.fig.add_subplot(111)
     
+    self.slide_stats_list = []
+    self.bg_estimators = []
     self.label_list = []
     self.expectation_list_list = []
     self.finalized = False
@@ -147,14 +155,17 @@ class InverseExpectationPlot(object):
     if self.finalized:
       raise ValueError, "cannot add data after plot has been finalized"
     
+    # store background stats
+    self.slide_stats_list.append([s.getstat() for s in slide_trigs])
+    
     # prepare background estimate
-    bg_estimator = BackgroundEstimator(bg_fg_ratio=len(slide_trigs))
-    for slides in slide_trigs:
-      bg_estimator.extend(slides.getstat())
-    bg_estimator.finalize()
+    bg_est = BackgroundEstimator(iterutils.flatten(self.slide_stats_list[-1]),
+                                 bg_fg_ratio=len(slide_trigs))
+    bg_est.finalize()
+    self.bg_estimators.append(bg_est)
     
     # evaluate background rate for each foreground trigger
-    self.expectation_list_list.append([bg_estimator(s) for s in trigs.getstat()])
+    self.expectation_list_list.append([bg_est(s) for s in trigs.getstat()])
     self.label_list.append(label)
   
   def finalize(self, min_exp=None, max_exp=None):
@@ -164,12 +175,27 @@ class InverseExpectationPlot(object):
     """
     if self.finalized:
       raise ValueError, "InverseExpectationPlot has already been finalized"
-    colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
-    symbols = itertools.cycle(['^', 'D', 'H', 'o', '1', '+'])
     
-    # plot the points, keeping track of maxes and mins
-    internal_min_exp = numpy.inf
-    internal_max_exp = -numpy.inf
+    if sum([len(l) for l in self.expectation_list_list]) == 0:
+      print >>sys.stderr, "warning: nothing to plot"
+      return
+    
+    # sort
+    for expectations in self.expectation_list_list:
+      expectations.sort()
+
+    # get the max and min over all data sets
+    if (min_exp is None) or (max_exp is None):
+      internal_min_exp, internal_max_exp = self._get_extreme_expectations()
+      if min_exp is None:
+        min_exp = internal_min_exp
+      if max_exp is None:
+        max_exp = internal_max_exp
+    
+    colors = itertools.cycle(('b', 'g', 'r', 'c', 'm', 'y', 'k'))
+    symbols = itertools.cycle(('^', 'D', 'H', 'o', '1', '+'))
+    
+    # plot the points
     for expectations, label, color, symbol in \
         zip(self.expectation_list_list, self.label_list, colors, symbols):
       expectations.sort()
@@ -185,10 +211,6 @@ class InverseExpectationPlot(object):
       self.ax.plot(x, y, color + symbol, label=label + " (N = %d)" % len(y),
                    markeredgecolor=color, alpha=0.85,
                    scalex=False, scaley=False)
-
-      # contribute to plot limits
-      internal_min_exp = min(internal_min_exp, expectations[num_zeros])
-      internal_max_exp = max(internal_max_exp, expectations[-1])
       
       # handle zeros
       if num_zeros > 0:
@@ -197,21 +219,15 @@ class InverseExpectationPlot(object):
                      markerfacecolor='w', markeredgecolor=color,
                      scalex=False, scaley=False)
     
-    # determine plot limits
-    if min_exp is None:
-      min_exp = internal_min_exp
-    if max_exp is None:
-      max_exp = internal_max_exp
-    
     # plot the background
-    x = numpy.logspace(numpy.log10(1 / max_exp), numpy.log10(1 / min_exp), 200)
+    x = numpy.logspace(numpy.log10(1 / max_exp), numpy.log10(1 / min_exp), 3)
     y = 1 / x
     self.ax.plot(x, y, 'k--', label="background",
                  scalex=False, scaley=False)
     
     # set plot limits
     self.ax.set_xlim((0.9 / max_exp, 1.1 / min_exp))
-    self.ax.set_ylim((0.9,
+    self.ax.set_ylim((0.6,
       1.1 * max([len(l) for l in self.expectation_list_list])))
     
     # add legend
@@ -220,6 +236,22 @@ class InverseExpectationPlot(object):
     # mark as finalized
     self.savefig = self.fig.savefig
     self.finalized = True
+
+  def _get_extreme_expectations(self):
+    """
+    Return min and max expectation numbers (across all data sets) as a tuple.
+    Assumes that expectation numbers are sorted!
+    """
+    internal_min_exp = numpy.inf
+    internal_max_exp = -numpy.inf
+    for expectations in self.expectation_list_list:
+      expectations.sort()
+      num_zeros = bisect.bisect_right(expectations, 0)
+      if num_zeros == len(expectations):
+        continue
+      internal_min_exp = min(internal_min_exp, expectations[num_zeros])
+      internal_max_exp = max(internal_max_exp, expectations[-1])
+    return internal_min_exp, internal_max_exp
 
   def savefig(self, *args, **kwargs):
     """
@@ -235,3 +267,124 @@ class InverseExpectationPlot(object):
 
   def __del__(self):
     self.close()
+
+
+class HistogramInverseExpectationPlot(InverseExpectationPlot):
+  """
+  A histogrammed version of the InverseExpectationPlot, designed to be more
+  like the output of viz.cumhiststat.
+  """
+  def __init__(self, *args, **kwargs):
+    InverseExpectationPlot.__init__(self, *args, **kwargs)
+    if "num_bins" in kwargs:
+      self.num_bins = kwargs["num_bins"]
+    else:
+      self.num_bins = 20
+
+  def finalize(self, min_exp=None, max_exp=None, num_bins=20):
+    if self.finalized:
+      raise ValueError, "HistogramInverseExpectationPlot has already been finalized"
+
+    if sum([len(l) for l in self.expectation_list_list]) == 0:
+      print >>sys.stderr, "warning: nothing to plot"
+      return
+
+    # sort
+    for expectations in self.expectation_list_list:
+      expectations.sort()
+    
+    # get the max and min over all data sets
+    if (min_exp is None) or (max_exp is None):
+      internal_min_exp, internal_max_exp = self._get_extreme_expectations()
+      if min_exp is None:
+        min_exp = internal_min_exp
+      if max_exp is None:
+        max_exp = internal_max_exp
+
+    # plot the points
+    histograms = []
+    bins = rate.NDBins((rate.LogarithmicBins(1/max_exp, 1/min_exp, num_bins),))
+    x = bins.centres()[0]
+
+    colors = itertools.cycle(('b', 'g', 'r', 'c', 'm', 'y', 'k'))
+    symbols = itertools.cycle(('^', 'D', 'H', 'o', '1', '+'))
+    
+    for expectations, label, color, symbol in \
+        zip(self.expectation_list_list, self.label_list, colors, symbols):
+      num_zeros = bisect.bisect_right(expectations, 0)
+      if num_zeros == len(expectations):
+        continue
+      
+      hist = rate.BinnedArray(bins, dtype=float)
+      for value in expectations[num_zeros:]:
+        hist[1/value,] += 1
+      y = hist.array[::-1].cumsum()[::-1]
+      y[y == 0] = epsilon
+      assert y[0] == len(expectations)
+      
+      # plot the non-zero rates
+      self.ax.plot(x, y, color + symbol, label=label,
+                   markeredgecolor=color, alpha=0.65,
+                   scalex=False, scaley=False)
+      
+      # handle zeros
+      if num_zeros > 0:
+        self.ax.plot([x[-1]], [num_zeros], color + symbol,
+                     label=label + " larger than all BG",
+                     markerfacecolor='w', markeredgecolor=color, markersize=2,
+                     scalex=False, scaley=False)
+
+    # shade background error bars
+    # for each data set, histogram each slide separately and take stats
+    colors = itertools.cycle(('b', 'g', 'r', 'c', 'm', 'y', 'k'))
+    for slide_stats, bg_estimator, color in \
+      zip(self.slide_stats_list, self.bg_estimators, colors):
+      hist_sum = numpy.zeros(len(x), dtype=float)
+      sq_hist_sum = numpy.zeros(len(x), dtype=float)
+      for slide in slide_stats:
+        hist = rate.BinnedArray(bins, dtype=float)
+        for value in slide:
+          try:
+            hist[1/bg_estimator(value),] += 1
+          except IndexError:
+            # the histogram bins were chosen based on foreground extrema, so
+            # background may exceed limits
+            pass
+        y = hist.array[::-1].cumsum()[::-1]
+        hist_sum += y
+        sq_hist_sum += y*y
+      
+      # get statistics
+      N = len(slide_stats)
+      means = hist_sum / N
+      stds = numpy.sqrt((sq_hist_sum - hist_sum*means) / (N - 1))
+      
+      print means
+      print stds
+      
+      # shade in the area
+      upper = means + stds
+      lower = means - stds
+      upper[upper <= epsilon] = epsilon
+      lower[lower <= epsilon] = epsilon
+      means[means <= epsilon] = epsilon
+      tmp_x, tmp_y = viz.makesteps(bins.lower()[0], upper, lower)
+      self.ax.fill(tmp_x, tmp_y, facecolor=color, alpha=0.2)
+    
+    # plot the background
+    x = numpy.logspace(numpy.log10(1/max_exp), numpy.log10(1/min_exp), 200)
+    y = 1 / x
+    self.ax.plot(x, y, 'k--', label="background",
+                 scalex=False, scaley=False)
+
+    # set plot limits
+    self.ax.set_xlim((0.9 / max_exp, 1.1 / min_exp))
+    self.ax.set_ylim((0.6,
+      1.15 * max([len(l) for l in self.expectation_list_list])))
+
+    # add legend
+    self.ax.legend()
+
+    # mark as finalized
+    self.savefig = self.fig.savefig
+    self.finalized = True
