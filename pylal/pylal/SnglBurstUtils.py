c3167ef8ff038e8e27bddeb18a3de0863fd399ba
kipp <kipp>
2008-02-19 18:26:47 +0000

    The string search doesn't use the multi_burst table, so don't require it along with the other coinc tables.

--- pylal/pylal/SnglBurstUtils.py
+++ pylal/pylal/SnglBurstUtils.py
@@ -89,11 +89,13 @@ class CoincDatabase(object):
 			self.coinc_def_table = table.get_table(self.xmldoc, lsctables.CoincDefTable.tableName)
 			self.coinc_table = table.get_table(self.xmldoc, lsctables.CoincTable.tableName)
 			self.time_slide_table = table.get_table(self.xmldoc, lsctables.TimeSlideTable.tableName)
-			self.multi_burst_table = table.get_table(self.xmldoc, lsctables.MultiBurstTable.tableName)
 		except ValueError:
 			self.coinc_def_table = None
 			self.coinc_table = None
 			self.time_slide_table = None
+		try:
+			self.multi_burst_table = table.get_table(self.xmldoc, lsctables.MultiBurstTable.tableName)
+		except ValueError:
 			self.multi_burst_table = None
 
 		# get the segment lists
