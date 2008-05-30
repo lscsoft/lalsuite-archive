1467cd5016ba9cc19016faf613f440e269a5fd19
kipp <kipp>
2008-05-30 02:43:04 +0000

    Put some flesh on the blob support.

--- glue/glue/ligolw/tokenizer.c
+++ glue/glue/ligolw/tokenizer.c
@@ -29,7 +29,10 @@
 
 
 #include <Python.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 #include <tokenizer.h>
 
 
@@ -105,15 +108,145 @@ PyObject *_build_formats(PyObject *sequence)
 
 static PyObject *parse_blob(PyObject *self, PyObject *args)
 {
-	PyErr_SetString(PyExc_NotImplementedError, "ligo light weight blob types not yet supported, please report problem to Glue maintainer");
-	return NULL;
+	PyObject *unicode;
+	PyObject *buffer;
+	const Py_UNICODE *unicode_data;
+	PyObject *string;
+	char *pos;
+	int i;
+	int length;
+
+	/*
+	 * Get the input unicode object's data address and length.
+	 */
+
+	if(!PyArg_ParseTuple(args, "U", &unicode))
+		return NULL;
+	unicode_data = PyUnicode_AS_UNICODE(unicode);
+	length = PyUnicode_GET_SIZE(unicode);
+
+	/*
+	 * Each character turns into (at most) one byte in the buffer.  We
+	 * need to arrange for the data to be free()'ed when the buffer
+	 * object is garbage collected.  The buffer object has a b_base
+	 * element in its structure which points to something to decref
+	 * when the buffer is garbage collected, so instead of allocating
+	 * our own data we put it inside a string object (hidden from the
+	 * rest of the interpreter) and then point the buffer object at the
+	 * string object.  The string object's machinery should then take
+	 * care of garbage collecting our data.
+	 */
+
+	string = PyString_FromStringAndSize(NULL, length);
+	if(!string)
+		return NULL;
+
+	/*
+	 * Translate unicode string.  Whitespace is skipped, a backslash
+	 * initates a three-digit octal code, everything else goes in as
+	 * itself and must be in the range [0, 128).
+	 */
+
+	pos = PyString_AS_STRING(string);
+	for(i = 0; i < length; i++) {
+		if(Py_UNICODE_ISSPACE(unicode_data[i]))
+			continue;
+		if(unicode_data[i] != '\\') {
+			if(unicode_data[i] < 0 || unicode_data[i] > 127) {
+				Py_DECREF(string);
+				PyErr_SetObject(PyExc_ValueError, unicode);
+				return NULL;
+			}
+			*(pos++) = unicode_data[i];
+		} else if(length - i < 4) {
+			Py_DECREF(string);
+			PyErr_SetObject(PyExc_ValueError, unicode);
+			return NULL;
+		} else {
+			int h = Py_UNICODE_TODECIMAL(unicode_data[++i]) * 64;
+			int m = Py_UNICODE_TODECIMAL(unicode_data[++i]) * 8;
+			int l = Py_UNICODE_TODECIMAL(unicode_data[++i]);
+
+			if(h < 0 || m < 0 || l < 0 || h + m + l > 255) {
+				Py_DECREF(string);
+				PyErr_SetObject(PyExc_ValueError, unicode);
+				return NULL;
+			}
+
+			*(pos++) = h + m + l;
+		}
+	}
+
+	/*
+	 * Adjust the strings's allocated size.
+	 */
+
+	length = pos - PyString_AS_STRING(string);
+	if(_PyString_Resize(&string, length))
+		return NULL;
+
+	/*
+	 * Construct buffer object.  PyBuffer_FromObject() increfs the
+	 * string, so when we decref the string the buffer object is the
+	 * last place holding a reference to it.
+	 */
+
+	buffer = PyBuffer_FromObject(string, 0, length);
+	Py_DECREF(string);
+
+	return buffer;
 }
 
 
 static PyObject *blob_format_func(PyObject *self, PyObject *args)
 {
-	PyErr_SetString(PyExc_NotImplementedError, "ligo light weight blob types not yet supported, please report problem to Glue maintainer");
-	return NULL;
+	PyObject *unicode;
+	wchar_t *string;
+	wchar_t *pos;
+	const char *buffer;
+	int i;
+	int length;
+
+	/*
+	 * Get the input buffer's data address and length.
+	 */
+
+	if(!PyArg_ParseTuple(args, "t#", &buffer, &length))
+		return NULL;
+
+	/*
+	 * Each char turns into (at most) a 4-character escaped octal code.
+	 */
+
+	string = malloc((4 * length + 1) * sizeof(*string));
+	if(!string)
+		return PyErr_NoMemory();
+
+	/*
+	 * Translate buffer.  Spaces, unprintable characters, and things
+	 * with the high bit set go in as octal codes, everything else goes
+	 * in as itself.
+	 */
+
+	pos = string;
+	for(i = 0; i < length; i++) {
+		int count = swprintf(pos, 5, isspace(buffer[i]) || !isprint(buffer[i]) || (buffer[i] < 0) ? L"\\%03hho" : L"%hhc", buffer[i]);
+		if(count < 0) {
+			free(string);
+			PyErr_SetFromErrno(PyExc_ValueError);
+			return NULL;
+		}
+		pos += count;
+	}
+
+	/*
+	 * Build into unicode string.
+	 */
+
+	unicode = PyUnicode_FromWideChar(string, pos - string);
+	free(string);
+
+	return unicode;
 }
 
 
@@ -127,8 +260,8 @@ static PyObject *blob_format_func(PyObject *self, PyObject *args)
 
 
 static struct PyMethodDef module_methods[] = {
-	{"parse_blob", parse_blob, METH_VARARGS, NULL},
-	{"blob_format_func", blob_format_func, METH_VARARGS, NULL},
+	{"parse_blob", parse_blob, METH_VARARGS, "Converts a unicode string of printable ASCII characters and escaped octal codes into a Python array object of unsigned char data."},
+	{"blob_format_func", blob_format_func, METH_VARARGS, "Converts a Python array object of unsigned char data into a unicode string of printable ASCII characters and escaped octal codes."},
 	{NULL,}
 };
 
