e1d4aac47bec0418758f81a7aded8ab23fbb968f
kipp <kipp>
2008-06-03 23:16:46 +0000

    Gzip inspiral_event_id_test_in1.xml to include that branch of the reading code in the unit test.

--- glue/test/ligo_lw_test_02.sh
+++ glue/test/ligo_lw_test_02.sh
@@ -1,4 +1,4 @@
-if ligolw_add --verbose inspiral_event_id_test_in?.xml | diff -q -s - inspiral_event_id_test_out.xml ; then
+if ligolw_add --verbose inspiral_event_id_test_in?.xml* | diff -q -s - inspiral_event_id_test_out.xml ; then
 	echo "Pass"
 	true
 else
