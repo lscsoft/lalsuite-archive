3beed08ce3b43170d7e3816e5907fb9d756e7429
piwei <piwei>
2008-06-11 18:39:33 +0000

    update glue location

--- glue/etc/segpagegen_A5.sh
+++ glue/etc/segpagegen_A5.sh
@@ -4,4 +4,4 @@
 #
 # Script which runs segpagegen on ldas-cit to generate the A5 segment dump
 #
-/usr1/ldbd/glue/bin/segpagegen --config-file /export/ldbd/etc/segpagegen_A5.ini
+/usr1/ldbd/glue-1-17/bin/segpagegen --config-file /export/ldbd/etc/segpagegen_A5.ini
