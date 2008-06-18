6bb60ccaa81b97ad77afd45db514a64a711d0a38
piwei <piwei>
2008-06-18 22:03:30 +0000

    Fixed comment: checked in GEOscript.sh and renamed it as GEOpublishstate.sh

--- glue/etc/GEOpublishstate.sh
+++ glue/etc/GEOpublishstate.sh
@@ -4,3 +4,5 @@ source /usr1/grid/.bash_profile
 source /usr1/grid/chrism/suggested_env.txt
 
 /usr1/grid/chrism/GEOpublishstate.py --run A5 --version 4 --logfile /usr1/grid/chrism/GEOpublishstate --database seg_cit --server ldas-cit.ligo.caltech.edu --frametype RDS_C01_L3 --start 835920013 --finder /usr1/grid/chrism/LSCdataFind --fetcher /usr1/grid/chrism/FrStateFetcherSPARC32
+
+
