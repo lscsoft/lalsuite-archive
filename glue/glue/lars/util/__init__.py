1b749c204a568b5fae1a2965a13208e3b27e2074
bmoe <bmoe>
2008-10-02 15:40:42 +0000

    Misc

--- glue/glue/lars/util/__init__.py
+++ glue/glue/lars/util/__init__.py
@@ -2,6 +2,7 @@
 import os
 from urlparse import urlsplit
 import logging
+from subprocess import Popen
 
 log = logging.getLogger("lars.util")
 
@@ -9,9 +10,11 @@ def mountSearch(searchUrl, targetDir):
     (scheme, netloc, path, query, frag) = urlsplit(searchUrl)
     # netloc is just host -- we didn't put user/port in there right?
     fs = "%s:%s" % (netloc, path)
+    targetDir = os.path.abspath(os.path.expanduser(targetDir))
     try:    os.mkdir(targetDir)
     except: pass
     log.info("Mounting '%s' on '%s'" % (fs, targetDir))
-    p = Popen(["sshfs", fs, os.path.abspath(targetDir)], cwd=basedir)
+    #p = Popen(["sshfs", fs, os.path.abspath(targetDir)], cwd=basedir)
+    p = Popen(["sshfs", fs, os.path.abspath(targetDir)])
     p.wait()
     del p
