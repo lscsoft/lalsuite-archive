1085ebc72ac8d70f0f75efc5877fb6478edef6bf
kipp <kipp>
2008-12-23 00:24:47 +0000

    Make a whole bunch of clean-ups in preparation for moving this stuff into glue.

--- pylal/pylal/ligolw_segments.py
+++ pylal/pylal/ligolw_segments.py
@@ -54,6 +54,9 @@ __date__ = "$Date$"[7:-2]
 
 
 def segment_def(instruments, name, comment, id, process):
+	"""
+	Create and initialize a segment_definer row object.
+	"""
 	row = lsctables.SegmentDef()
 	row.process_id = process.process_id
 	row.segment_def_id = id
@@ -66,6 +69,9 @@ def segment_def(instruments, name, comment, id, process):
 
 
 def seg_def_map(seg, seg_def, tbl, process):
+	"""
+	Create and initialize a segment_def_map row object.
+	"""
 	row = lsctables.SegmentDefMap()
 	row.process_id = process.process_id
 	row.segment_id = seg.segment_id
@@ -78,49 +84,30 @@ class LigolwSegmentList(object):
 	"""
 	A description of a class of segments.
 	"""
-	def __init__(self, active = (), inactive = (), unknown = (), instruments = set(), name = None, comment = None):
+	def __init__(self, active = (), inactive = (), instruments = set(), name = None, comment = None):
 		self.active = segments.segmentlist(active)
 		self.inactive = segments.segmentlist(inactive)
-		self.unknown = segments.segmentlist(unknown)
 		self.instruments = instruments
 		self.name = name
 		self.comment = comment
 
 	def sort(self, *args):
 		"""
-		Sort the three internal segment lists in time order.
+		Sort the internal segment lists.  The optional args are
+		passed to the .sort() method of the segment lists.  This
+		can be used to control the sort order by providing an
+		alternate comparison function.  The default is to sort by
+		start time with ties broken by end time.
 		"""
 		self.active.sort(*args)
 		self.inactive.sort(*args)
-		self.unknown.sort(*args)
 
 	def coalesce(self):
 		"""
-		Coalesce the three internal segment lists.
+		Coalesce the internal segment lists.
 		"""
 		self.active.coalesce()
 		self.inactive.coalesce()
-		self.unknown.coalesce()
-
-	def time_order_rows(self, tbl, process):
-		"""
-		A generator, yielding in time order
-		glue.ligolw.lsctables.Segment objects representing all the
-		segments from the three internal segment lists.
-		"""
-		def seglist_iterator(seglist, activity):
-			for seg in seglist:
-				yield seg, activity
-
-		self.sort()
-
-		for seg, activity in iterutils.inorder(seglist_iterator(self.active, True), seglist_iterator(self.inactive, False), seglist_iterator(self.unknown, None)):
-			row = lsctables.Segment()
-			row.set(seg)
-			row.set_active(activity)
-			row.process_id = process.process_id
-			row.segment_id = tbl.get_next_id()
-			yield row
 
 
 #
@@ -137,14 +124,7 @@ class LigolwSegments(object):
 	A high-level interface to the segments tables in a LIGO Light
 	Weight XML document.
 	"""
-	def __init__(self, xmldoc, process):
-		#
-		# The row in the process table on which we will blame our
-		# work
-		#
-
-		self.process = process
-
+	def __init__(self, xmldoc):
 		#
 		# Find tables, and synchronize ID generators
 		#
@@ -177,7 +157,7 @@ class LigolwSegments(object):
 		self.segment_lists = {}
 		for row in self.segment_def_table:
 			if row.segment_def_id in self.segment_lists:
-				raise ValueError, "segment_definer table contains duplicate segment_definer keys"
+				raise ValueError, "duplicate ID '%s' in segment_definer table" % str(row.segment_def_id)
 			self.segment_lists[row.segment_def_id] = LigolwSegmentList(instruments = row.get_ifos(), name = row.name, comment = row.comment)
 		del self.segment_def_table[:]
 
@@ -185,7 +165,7 @@ class LigolwSegments(object):
 		segment_def_map = {}
 		for row in self.segment_def_map_table:
 			if row.segment_id in segment_def_map:
-				raise ValueError, "segment_def_map table contains duplicate segment keys"
+				raise ValueError, "duplicate segment ID '%s' in segment_def_map table" % str(row.segment_id)
 			segment_def_map[row.segment_id] = self.segment_lists[row.segment_def_id]
 		del self.segment_def_map_table[:]
 
@@ -194,12 +174,10 @@ class LigolwSegments(object):
 			active = row.get_active()
 			if active is True:
 				segment_def_map[row.segment_id].active.append(row.get())
-			elif row.active is False:
+			elif active is False:
 				segment_def_map[row.segment_id].inactive.append(row.get())
-			elif row.active is None:
-				segment_def_map[row.segment_id].unknown.append(row.get())
 			else:
-				raise ValueError, "execution should not get to this line of code"
+				raise ValueError, "invalid activity flag '%s' for segment '%s'" % (repr(row.activity), str(row.segment_id))
 		del self.segment_table[:]
 		del segment_def_map
 
@@ -209,67 +187,95 @@ class LigolwSegments(object):
 
 
 	def coalesce(self):
-		#
-		# Coalesce the segment lists.
-		#
-
+		"""
+		Coalesce the segment lists.
+		"""
 		for ligolw_segment_list in self.segment_lists.values():
 			ligolw_segment_list.coalesce()
 
 
-	def sort(self):
+	def sort(self, *args):
+		"""
+		Sort the segment lists.  The optional args are passed to
+		the .sort() methods of the segment lists.  This can be used
+		to control the sort order by providing an alternate
+		comparison function (the default is to sort all lists by
+		segment start time with ties broken by end time).
+		"""
 		for ligolw_segment_list in self.segment_lists.values():
-			ligolw_segment_list.sort()
+			ligolw_segment_list.sort(*args)
 
 
 	def optimize(self):
 		"""
 		Identifies segment lists that differ only in their
-		instruments --- they have the same active, inactive, and
-		unknown segments, the same name and the same comment ---
-		and then deletes all but one of them, leaving just a single
-		list having the union of the instruments.
+		instruments --- they have the same active and inactive
+		segments, the same name and the same comment --- and then
+		deletes all but one of them, leaving just a single list
+		having the union of the instruments.
 		"""
 		self.sort()
 		mergelist = []
 		for (def_id_a, seglist_a), (def_id_b, seglist_b) in iterutils.choices(self.segment_lists.items(), 2):
-			if seglist_a.active == seglist_b.active and seglist_a.inactive == seglist_b.inactive and seglist_a.unknown == seglist_b.unknown and seglist_a.name == seglist_b.name and seglist_a.comment == seglist_b.comment:
+			if seglist_a.active == seglist_b.active and seglist_a.inactive == seglist_b.inactive and seglist_a.name == seglist_b.name and seglist_a.comment == seglist_b.comment:
 				mergelist.append((def_id_a, def_id_b))
 		for target, source in mergelist:
 			if source in self.segment_lists:
 				self.segment_lists[target].instruments |= self.segment_lists.pop(source).instruments
 
 
-	def finalize(self):
-		#
-		# Restore the LigolwSegmentList objects to the XML tables
-		# in preparation for output.  All segments from all segment
-		# lists are inserted into the tables in time order, but
-		# this is NOT behaviour external applications should rely
-		# on.  This is done simply in the belief that it might
-		# assist in constructing well balanced indexed databases
-		# from the resulting files.  If that proves not to be the
-		# case, or for some reason this behaviour proves
-		# inconvenient to preserve, then it might be discontinued
-		# without notice.  You've been warned.
-		#
-
-		def row_iterator(rows, segment_definition):
-			for row in rows:
-				yield row, segment_definition
+	def finalize(self, process):
+		"""
+		Restore the LigolwSegmentList objects to the XML tables in
+		preparation for output.  All segments from all segment
+		lists are inserted into the tables in time order, but this
+		is NOT behaviour external applications should rely on.
+		This is done simply in the belief that it might assist in
+		constructing well balanced indexed databases from the
+		resulting files.  If that proves not to be the case, or for
+		some reason this behaviour proves inconvenient to preserve,
+		then it might be discontinued without notice.  You've been
+		warned.
+		"""
+		def time_order_rows(ligolw_segment_list, segment_table, process, segment_def_row):
+			ligolw_segment_list.sort()
+			for seg, activity in iterutils.inorder(((seg, True) for seg in ligolw_segment_list.active), ((seg, False) for seg in ligolw_segment_list.inactive)):
+				segment_row = segment_table.RowType()
+				segment_row.set(seg)
+				segment_row.set_active(activity)
+				segment_row.process_id = process.process_id
+				segment_row.segment_id = segment_table.get_next_id()
+				yield segment_row, segment_def_row
 
 		row_iterators = []
 		for segment_def_id, ligolw_segment_list in self.segment_lists.items():
-			segment_definition = segment_def(ligolw_segment_list.instruments, ligolw_segment_list.name, ligolw_segment_list.comment, segment_def_id, self.process)
-			self.segment_def_table.append(segment_definition)
-			row_iterators.append(row_iterator(ligolw_segment_list.time_order_rows(self.segment_table, self.process), segment_definition))
+			segment_def_row = segment_def(ligolw_segment_list.instruments, ligolw_segment_list.name, ligolw_segment_list.comment, segment_def_id, process)
+			self.segment_def_table.append(segment_def_row)
+			row_iterators.append(time_order_rows(ligolw_segment_list, self.segment_table, process, segment_def_row))
 
-		for row, segment_definition in iterutils.inorder(*row_iterators):
-			self.segment_table.append(row)
-			self.segment_def_map_table.append(seg_def_map(row, segment_definition, self.segment_def_map_table, self.process))
+		for segment_row, segment_def_row in iterutils.inorder(*row_iterators):
+			self.segment_table.append(segment_row)
+			self.segment_def_map_table.append(seg_def_map(segment_row, segment_def_row, self.segment_def_map_table, process))
 
 		self.segment_lists.clear()
 
 
-	def insert_from_segwizard(self, fileobj, instruments, name, comment):
-		self.segment_lists[self.segment_def_table.get_next_id()] = LigolwSegmentList(active = segmentsUtils.fromsegwizard(fileobj, coltype = LIGOTimeGPS), instruments = instruments, name = name, comment = comment)
+#
+# =============================================================================
+#
+#                                  Utilities
+#
+# =============================================================================
+#
+
+
+def insert_from_segwizard(ligolw_segments, fileobj, instruments, name, comment):
+	"""
+	Parse the contents of the file object fileobj as a segwizard-format
+	segment list, and insert the result as a new list of "active"
+	segments into the LigolwSegments object ligolw_segments.  A new
+	entry is created in the segment_definer table for the segment list,
+	and instruments, name and comment are used to populate the entry's
+	metadata.
+	"""
+	ligolw_segments.segment_lists[ligolw_segments.segment_def_table.get_next_id()] = LigolwSegmentList(active = segmentsUtils.fromsegwizard(fileobj, coltype = LIGOTimeGPS), instruments = instruments, name = name, comment = comment)
