bc6b7a8f91ce48165275a5fefa25620b4a513337
spxiwh <spxiwh>
2009-01-07 13:57:15 +0000

    Adding script to gather main results

---
+++ pylal/bin/upperlimits_mainresults.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+
+import sys,os,math
+from optparse import *
+import ConfigParser
+import random
+import shutil
+
+##############################################################################
+usage = """
+usage: %prog [options] 
+
+"""
+
+def parse_command_line():
+  """
+  Parser function dedicated
+  """
+  parser = OptionParser( usage=usage, version="%prog CVS $Id$ " )
+  parser.add_option("-f", "--config-file",action="store",type="string",\
+      metavar=" FILE", help="use configuration file FILE")
+
+  (options,args) = parser.parse_args()
+  return options, sys.argv[1:]
+
+def make_dir(directory):
+  if not os.path.isdir(directory):
+    os.makedirs(directory)
+
+# ============================================================================
+# -- get command line arguments
+opts, args = parse_command_line()
+if not opts.config_file:
+  print >> sys.stderr , 'You must specify a config file'
+  sys.exit(1)
+
+###################################
+
+cp = ConfigParser.ConfigParser()
+cp.read(opts.config_file)
+runs = []
+spins = []
+sourceTypes = []
+userTag = cp.get('main','user-tag')
+if cp.has_option('run-options','run-spin'):
+  spins.append('spin')
+if cp.has_option('run-options','run-non-spin'):
+  spins.append('nonspin')
+if cp.has_option('run-options','run-gaussian'):
+  for item in cp.options('gaussian-types'):
+    sourceTypes.append(item)
+if cp.has_option('run-options','run-total-mass'):
+  for item in cp.options('total-mass-ranges'):
+    sourceTypes.append(item)
+if cp.has_option('run-options','run-component-mass'):
+  for item in cp.options('component-mass-ranges'):
+    sourceTypes.append(item)
+
+for spin in spins:
+  for sourceType in sourceTypes:
+    dir = spin + '/' + sourceType + '/'
+    runs.append((dir,spin,sourceType))
+
+for dir,type,item in runs:
+  outdir = 'results/' + dir
+  resultDir = 'plotnumgalaxies_files/' + dir
+  resultFile = resultDir + '/' + userTag + '_' + item + '_' + type
+  resultFile += '_combos-combined-posterior.txt'
+  outFile = outdir + '/' + userTag + '_posterior-pdf.txt'
+  make_dir(outdir)
+  shutil.copy(resultFile,outFile)
+
+make_dir('key_plots')
+combinedPlotulvsmassDir = 'combined_plotulvsmass/'
+combinedPlotulvsmassFiles = ['mcomp_nonspin-combined-rate-v-mass.png']
+combinedPlotulvsmassFiles.append('mcomp_spin-combined-rate-v-mass.png')
+combinedPlotulvsmassFiles.append('mtotal_nonspin-combined-rate-v-mass.png')
+combinedPlotulvsmassFiles.append('mtotal_spin-combined-rate-v-mass.png')
+for file in combinedPlotulvsmassFiles:
+  shutil.copy(combinedPlotulvsmassDir + '/' + file, 'key_plots/' + file)
+
+combinePosteriorDir = 'combineposterior_files/'
+for item in ('bbh','bns','nsbh'):
+  for type in ('spin','nonspin'):
+    file = userTag + '_' + item + '_' + type + '-posterior-comparison.png'
+    shutil.copy(combinePosteriorDir + '/' + file, 'key_plots/' + file)
+
