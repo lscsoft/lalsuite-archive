0f66fd247e04dfe69e9908fe799931503dac5d24
duncan <duncan>
2009-02-07 23:10:03 +0000

    fixed except statement

--- glue/glue/ldbd.py
+++ glue/glue/ldbd.py
@@ -347,8 +347,8 @@ class LIGOLwParser:
                 lst[j][k] = None
               else:
                 lst[j][k] = self.types[table[tab]['column'][thiscol]](stream[i])
-            except KeyError, ValueError as errmsg:
-              msg = "stream translation error (%s) " % errmsg
+            except (KeyError, ValueError), errmsg:
+              msg = "stream translation error (%s) " % str(errmsg)
               msg += "for column %s in table %s: %s -> %s" \
                 % (tab,thiscol,stream[i],str(table[tab])) 
               raise LIGOLwParseError, msg
