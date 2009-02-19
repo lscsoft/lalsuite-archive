385489773dc66c9e211e17d42eacd79d0e5819b7
spxiwh <spxiwh>
2009-02-19 17:14:39 +0000

    Not running on injections

--- lalapps/src/inspiral/lalapps_write_ifar_result_dag.sh
+++ lalapps/src/inspiral/lalapps_write_ifar_result_dag.sh
@@ -74,41 +74,41 @@ if [ 1 ]; then
   echo "MAXJOBS plotifar 20"
 fi > ifar_result_${data_type}.dag
 
-if [ 1 ]; then
-  for injstring in BNSLININJ BNSLOGINJ BNSSPINLININJ BNSSPINLOGINJ NSBHLININJ NSBHLOGINJ NSBHSPINLININJ NSBHSPINLOGINJ BBHLININJ BBHLOGINJ BBHSPINLININJ BBHSPINLOGINJ; do
-    #run plotifar on the individual mass bins
-    for mass in mchirp_2_8 mchirp_8_17 mchirp_17_35; do
-      for combo in H1L1 H2L1 H1H2L1; do
-        job_name="${combo}-plotifar_${mass}_${injstring}"
-        glob_files="corse_all_data_files/${injstring}/${combo}*CORSE_${injstring}_${mass}_${cat}-${month_gps_time}-${month_duration}.xml.gz"
-        outpath="ifar_result_files/${injstring}/${mass}"
-        time_correct_file="second_coire_files/summ_files_${injstring}/${combo}-SECOND_COIRE_${cat}_${combo}-${month_gps_time}-${month_duration}.txt"
-        user_tag="${mass}-${injstring}"
-        echo "JOB $job_name ifar_result_injection.plotifar.sub"
-        echo "RETRY $job_name 1"
-        echo "VARS $job_name macroglob=\"$glob_files\" macrooutpath=\"$outpath\" macroifotimes=\"$combo\"  macrotcorrfile=\"$time_correct_file\" macrousertag=\"$user_tag\"" 
-        echo "CATEGORY $job_name plotifar"
-        echo
-      done
-    done
-    # run plotifar on all the mass bins globbed together 
-    for combo in H1L1 H2L1 H1H2L1; do
-      job_name="${combo}-plotifar_ALL_MASSES_${injstring}"
-      glob_files="corse_all_data_files/${injstring}/${combo}*CORSE_${injstring}_*_${cat}-${month_gps_time}-${month_duration}.xml.gz"
-      outpath="ifar_result_files/${injstring}/"
-      time_correct_file="second_coire_files/summ_files_${injstring}/${combo}-SECOND_COIRE_${cat}_${combo}-${month_gps_time}-${month_duration}.txt"
-      user_tag="ALL_MASSES-${injstring}"
-      echo "JOB $job_name ifar_result_injection.plotifar.sub"
-      echo "RETRY $job_name 1"
-      echo "VARS $job_name macroglob=\"$glob_files\" macrooutpath=\"$outpath\" macroifotimes=\"$combo\" macrotcorrfile=\"$time_correct_file\" macrousertag=\"$user_tag\" macroplotslideopts=\"\""
-      echo "CATEGORY $job_name plotifar"
-      echo
-    done
-  done
-  echo "MAXJOBS filter_coire 20"
-  echo "MAXJOBS coire 20"
-  echo "MAXJOBS plotifar 20"
-fi > ifar_result_injection.dag
+#if [ 1 ]; then
+#  for injstring in BNSLININJ BNSLOGINJ BNSSPINLININJ BNSSPINLOGINJ NSBHLININJ NSBHLOGINJ NSBHSPINLININJ NSBHSPINLOGINJ BBHLININJ BBHLOGINJ BBHSPINLININJ BBHSPINLOGINJ; do
+#    #run plotifar on the individual mass bins
+#    for mass in mchirp_2_8 mchirp_8_17 mchirp_17_35; do
+#      for combo in H1L1 H2L1 H1H2L1; do
+#        job_name="${combo}-plotifar_${mass}_${injstring}"
+#        glob_files="corse_all_data_files/${injstring}/${combo}*CORSE_${injstring}_${mass}_${cat}-${month_gps_time}-${month_duration}.xml.gz"
+#        outpath="ifar_result_files/${injstring}/${mass}"
+#        time_correct_file="second_coire_files/summ_files_${injstring}/${combo}-SECOND_COIRE_${cat}_${combo}-${month_gps_time}-${month_duration}.txt"
+#        user_tag="${mass}-${injstring}"
+#        echo "JOB $job_name ifar_result_injection.plotifar.sub"
+#        echo "RETRY $job_name 1"
+#        echo "VARS $job_name macroglob=\"$glob_files\" macrooutpath=\"$outpath\" macroifotimes=\"$combo\"  macrotcorrfile=\"$time_correct_file\" macrousertag=\"$user_tag\"" 
+#        echo "CATEGORY $job_name plotifar"
+#        echo
+#      done
+#    done
+#    # run plotifar on all the mass bins globbed together 
+#    for combo in H1L1 H2L1 H1H2L1; do
+#      job_name="${combo}-plotifar_ALL_MASSES_${injstring}"
+#      glob_files="corse_all_data_files/${injstring}/${combo}*CORSE_${injstring}_*_${cat}-${month_gps_time}-${month_duration}.xml.gz"
+#      outpath="ifar_result_files/${injstring}/"
+#      time_correct_file="second_coire_files/summ_files_${injstring}/${combo}-SECOND_COIRE_${cat}_${combo}-${month_gps_time}-${month_duration}.txt"
+#      user_tag="ALL_MASSES-${injstring}"
+#      echo "JOB $job_name ifar_result_injection.plotifar.sub"
+#      echo "RETRY $job_name 1"
+#      echo "VARS $job_name macroglob=\"$glob_files\" macrooutpath=\"$outpath\" macroifotimes=\"$combo\" macrotcorrfile=\"$time_correct_file\" macrousertag=\"$user_tag\" macroplotslideopts=\"\""
+#      echo "CATEGORY $job_name plotifar"
+#      echo
+#    done
+#  done
+#  echo "MAXJOBS filter_coire 20"
+#  echo "MAXJOBS coire 20"
+#  echo "MAXJOBS plotifar 20"
+#fi > ifar_result_injection.dag
 
 if [ 1 ]; then
   echo "universe = vanilla"
@@ -123,18 +123,18 @@ if [ 1 ]; then
   echo "queue 1"
 fi > ifar_result.plotifar.sub
 
-if [ 1 ]; then
-  echo "universe = vanilla"
-  echo "executable = ${plotifar_path}"
-  echo "arguments = --glob \$(macroglob) --output-path \$(macrooutpath) --enable-output --ifo-times \$(macroifotimes) --gps-start-time ${month_gps_time} --gps-end-time ${gps_end_time} --ifan-dist --ifar-dist --plot-uncombined --plot-combined --show-min-bkg --show-max-bkg --show-two-sigma-error --time-correct-file \$(macrotcorrfile) --user-tag \$(macrousertag)"
-  echo "getenv = True"
-  echo "log = " `mktemp -p ${log_path}`
-  echo "error = logs/plotifar-\$(cluster)-\$(process).err"
-  echo "output = logs/plotifar-\$(cluster)-\$(process).out"
-  echo "notification = never"
-  echo "priority = ${condor_priority}"
-  echo "queue 1"
-fi > ifar_result_injection.plotifar.sub
+#if [ 1 ]; then
+#  echo "universe = vanilla"
+#  echo "executable = ${plotifar_path}"
+#  echo "arguments = --glob \$(macroglob) --output-path \$(macrooutpath) --enable-output --ifo-times \$(macroifotimes) --gps-start-time ${month_gps_time} --gps-end-time ${gps_end_time} --ifan-dist --ifar-dist --plot-uncombined --plot-combined --show-min-bkg --show-max-bkg --show-two-sigma-error --time-correct-file \$(macrotcorrfile) --user-tag \$(macrousertag)"
+#  echo "getenv = True"
+#  echo "log = " `mktemp -p ${log_path}`
+#  echo "error = logs/plotifar-\$(cluster)-\$(process).err"
+#  echo "output = logs/plotifar-\$(cluster)-\$(process).out"
+#  echo "notification = never"
+#  echo "priority = ${condor_priority}"
+#  echo "queue 1"
+#fi > ifar_result_injection.plotifar.sub
 
 #make directory structure
 if [ ! -d ifar_result_files ] ; then
@@ -143,7 +143,7 @@ fi
 if [ ! -d ifar_result_files/summ_files ] ; then
   mkdir ifar_result_files/summ_files
 fi
-for string in ${data_type} BNSLININJ BNSLOGINJ BNSSPINLININJ BNSSPINLOGINJ NSBHLININJ NSBHLOGINJ NSBHSPINLININJ NSBHSPINLOGINJ BBHLININJ BBHLOGINJ BBHSPINLININJ BBHSPINLOGINJ; do
+for string in ${data_type}; do
   if [ ! -d ifar_result_files/${string} ] ; then
     mkdir ifar_result_files/${string}
   fi
@@ -156,6 +156,6 @@ done
 echo " done."
 echo "*******************************************************************"
 echo "  Now run: condor_submit_dag ifar_result_${data_type}.dag"
-echo "      and: condor_submit_dag ifar_result_injection.dag"
+#echo "      and: condor_submit_dag ifar_result_injection.dag"
 echo "*******************************************************************"
 
