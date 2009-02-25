ebd5a539597fd22af5631a31745f7e4a1ab25ec1
cristina <cristina>
2009-02-25 21:50:25 +0000

    made branch look more like post tag fixes in 1.9[3,4,5]

--- pylal/pylal/fu_utils.py
+++ pylal/pylal/fu_utils.py
@@ -1568,7 +1568,6 @@ class followupdqdb:
             self.createDB()
             self.__connectDB__()
         else:
-            self.__disconnectDB__()
             self.db=None
     #End __init__()
 
@@ -1648,7 +1647,7 @@ path %s\n"%(myPath))
             self.db.commit()
             fp.close()
         self.db.close()
-        sys.stderr.write("Created SQLite database.\n")
+        sys.stderr.write("Created SQLite database and closed it.\n")
     #End method createDB            
 
 
@@ -1680,6 +1679,8 @@ path %s\n"%(myPath))
                 self.dbFiles[i][1]=file
                 if os.path.isfile(self.dbFiles[i][1]):
                     self.dbFiles[i][3]=True
+                else:
+                  self.dbFiles[i][3]=False
         return
     #End setFile()
 
@@ -1691,8 +1692,9 @@ path %s\n"%(myPath))
         """
         if file==None:
             return
-        self.sqlFile=os.path.basename("/"+str(file))
-        self.sqlPath=os.path.dirname(file)
+        filename=os.path.normpath(file)
+        self.sqlFile=os.path.basename(filename)
+        self.sqlPath=os.path.dirname(filename)
         return
 
     def setVersion(self,version=None):
