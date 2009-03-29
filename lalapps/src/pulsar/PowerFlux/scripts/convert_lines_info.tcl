9b7085c34752f8cc26ed8aec362bb6f398929e68
volodya <volodya>
2009-03-29 00:29:46 +0000

    Add script to format line info data.

---
+++ lalapps/src/pulsar/PowerFlux/scripts/convert_lines_info.tcl
@@ -0,0 +1,14 @@
+#!/usr/bin/env tclsh
+
+puts "instance\tfirst_bin\tdataset\tbin\tz\tstrength\tflag"
+while { ! [eof stdin] } {
+	gets stdin line
+	if { $line == "" } { continue }
+
+	if { ! [regexp {output/([^/]*)/([^/-]*)-([^/-]*)/powerflux.log:(.*) background line detected: bin=(.*) z=(.*) strength=(.*) flag=(.*)$} $line {} instance first_bin frequency dataset bin z strength flag] } {
+		puts stderr "Could not parse line \"$line\""
+		continue
+		}
+	puts "$instance\t$first_bin\t$frequency\t\"$dataset\"\t$bin\t$z\t$strength\t[expr $flag]"
+	}
+ 
