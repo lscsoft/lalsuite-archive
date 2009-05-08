632a6b5b12f4a1b5dfe6c1696f9ad1af14769b54
Ping Wei <ping.wei@ligo.org>
2009-05-08 09:07:58 -0400

    add index

--- glue/src/conf/s6_db2/segment_definer.sql
+++ glue/src/conf/s6_db2/segment_definer.sql
@@ -62,4 +62,5 @@ CREATE INDEX segdef_cind ON segment_definer(name, version) CLUSTER
 CREATE INDEX segdef_ind_value ON segment_definer(ifos)
 ;
 
-
+CREATE INDEX segdef_dind ON segment_definer(segment_def_id,creator_db)
+;
