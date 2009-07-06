a3f74b648c31fd5530de7ec37b0cbed00d273338
Alexander Dietz <alexander.dietz@ligo.org>
2009-07-06 05:35:05 -0500

    basic changes for the exttrig s6 low latency analysis

--- pylal/setup.py
+++ pylal/setup.py
@@ -262,6 +262,8 @@ setup(
 		os.path.join("bin", "pylal_grblikelihood"),
 		os.path.join("bin", "pylal_grbUL"),
 		os.path.join("bin", "pylal_grbtimeslide_stats"),
+		os.path.join("bin", "pylal_exttrig_llmonitor"),
+		os.path.join("bin", "pylal_exttrig_llsummary"),
 		os.path.join("bin", "pylal_query_dq"),
 		os.path.join("bin", "pylal_relic"),
 		os.path.join("bin", "plotethinca"),
