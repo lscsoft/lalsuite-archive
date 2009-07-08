lscsoft/lalsuite-archive

commit 152537f4cb976e668ae0e6b89047bc9a0c16c4a4

more robust check if git is installed

glue/misc/determine_git_version.py
@@ -65,15 +65,10 @@ def run_external_command(command, honour_ret_code=True):
 def in_git_repository():
   """
   Return True if we are in a git repository and False if not.
-
-  NB: Unfortunately there is a magic number without any documentation to back
-  it up. It turns out that git status returns non-zero exit codes for all sorts
-  of success conditions, but I cannot find any documentation of them. 128 was
-  determined empirically.  I sure hope that it's portable.
-  The return code of 127 indicates that the git command cannot be found.
   """
-  ret_code = run_external_command('git status', honour_ret_code=False)[0]
-  return ret_code not in (127, 128)
+  # check if we are in a git repo
+  ret_code, output = run_external_command('which git', honour_ret_code=False)
+  return (ret_code != 1) and not output.startswith('no')
 
 def write_git_version(fileobj):
   """

commit 0eb9a937a3b1b4b03f511ac2544ec9e2fde363d2

also check that we are in a git repo
152537f4cb976e668ae0e6b89047bc9a0c16c4a4 accidentally removed the check
that we were in a git repository

glue/misc/determine_git_version.py
@@ -65,10 +65,15 @@ def run_external_command(command, honour_ret_code=True):
 def in_git_repository():
   """
   Return True if we are in a git repository and False if not.
+
+  NB: Unfortunately there is a magic number without any documentation to back
+  it up. It turns out that git status returns non-zero exit codes for all sorts
+  of success conditions, but I cannot find any documentation of them. 128 was
+  determined empirically. I sure hope that it's portable.
   """
-  # check if we are in a git repo
   ret_code, output = run_external_command('which git', honour_ret_code=False)
-  return (ret_code != 1) and not output.startswith('no')
+  return (ret_code != 1) and not output.startswith('no') \
+      and (run_external_command('git status', honour_ret_code=False)[0] != 128)
 
 def write_git_version(fileobj):
   """