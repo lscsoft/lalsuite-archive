ec06d5a33e19020bea4c0fcbe5d84fd5c7040a6c
Sukanta Bose <sukanta.bose@ligo.org>
2009-08-12 16:18:58 -0500

    Fixed a bug in the coherent follow-up code whereby the absence of event-id in the name of COHIRE files caused some plotchiatimeseries jobs to fail when more than one follow-up candidates exist in a single 2048s single-ifo chunk.

--- pylal/pylal/fu_utils.py
+++ pylal/pylal/fu_utils.py
@@ -1967,8 +1967,8 @@ def generateCohbankXMLfile(ckey,triggerTime,ifoTag,ifolist_in_coinc,search,outpu
   utils.write_filename(xmldoc, fileName, verbose = False, gz = True)
 
   #Also write input file for clustering code "cohire"
-  chiaFileName = 'followUpChiaJob/' + ifoTag + '-CHIA_1-' + str(int(triggerTime)-1) + "-2.xml.gz"
-  cohireInputFile = ifoTag + '-COHIRE-' + str(int(triggerTime)-1) + "-2.txt"
+  chiaFileName = 'followUpChiaJob/' + ifoTag + '-CHIA_1_' + str(int(ckey.event_id)) + '-' + str(int(triggerTime)-1) + "-2.xml.gz"
+  cohireInputFile = ifoTag + '-COHIRE_FOLLOWUP_' + str(int(ckey.event_id)) + '-' + str(int(triggerTime)-1) + "-2.txt"
   if outputPath:
     cohireInputFile = outputPath + '/' + cohireInputFile
   ff = open(cohireInputFile,'w')
