a73706225fbb2fb84b7c34a1d8b2571c1b7d5aaa
Alexander Dietz <alexander.dietz@ligo.org>
2009-08-14 05:18:37 -0500

    fixed total screwup because I do not know how to undo a not-committed change

--- pylal/pylal/pylal_exttrig_llutils.py
+++ pylal/pylal/pylal_exttrig_llutils.py
@@ -43,6 +43,38 @@ ifo_list = ['H1','L1','V1']
 
 offset_gps_to_linux = 315982785
 
+total_summary_prefix = """
+<body style="color: rgb(0, 0, 0); background-color: rgb(221, 255, 255);" alink="#000099" link="#000099" vlink="#990099">
+
+<h1>Summary of Gamma Ray Burst low-latency results during S6</h1>
+
+<span style="font-weight: bold;"><br><br>
+The following table contain a list of Gamma Ray Bursts occured during S6, with information about time, position on the sky, as well as duration and redshift (if available). This table has been automatically created by pylal_exttrig_llmonitor (in pylal_exttrig_llutils.py) to show a summary of the low-latency inspiral analysis of the GRBs during S6. A page describing this search can be found in the <a href="https://www.lsc-group.phys.uwm.edu/ligovirgo/cbcnote/S6Plan/090706044855TriggeredSearchLow_Latency_Exttrig_Search#preview">wiki</a>.<br><br>
+
+The number in the IFO columns indicate the antenna factor for this GRB and this detector, if there is data available at this time (1 meaning optimal location, 0 meaning worst location). In addition, a <b>bold</b> number indicates that the current segment is long enough to contain the required number of off-source segments around the GRB, not required to be symmetrical.<br><br>
+Date of last creation: %s<br><br>
+
+</span><span style="font-weight: bold;">
+<br><br>
+</div>
+<table border="1" cellpadding="2" cellspacing="2">
+  <tbody>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">Nr</td>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">GRB</td>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">Status</td>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">GPS<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">Date<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">redshift<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">duration [s]<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">RA<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">DEC<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">H1<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">L1<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">V1<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">Result<br>
+  <td style="vertical-align: top; font-weight: bold; font-style: italic; color: rgb(51, 51, 255); background-color: rgb(255, 153, 0);">Link<br>
+"""
+
 # -----------------------------------------------------
 def system_call(command):
   """
@@ -70,8 +102,6 @@ def info(text):
   logfile = open(log_file,'a')
   logfile.write(msg+'\n')
   logfile.close()
-
-  print msg
 
 
 # -----------------------------------------------------
@@ -191,10 +221,11 @@ def get_empty_exttrig_row():
   row.event_status = 0
   return row
 
+# --------------------------------------
 def generate_summary(monitor_file):
 
   def add(table, text):
-    print text
+    #print text
     return table + '<td>' +str(text)+'</td>' 
 
   # Read the list of all processed GRBs
@@ -204,7 +235,7 @@ def generate_summary(monitor_file):
   time_unsort = [grb['triggertime'] for grb in monitor_list]
   index = np.argsort(time_unsort)
 
-  table = ''
+  table = total_summary_prefix % get_time()
 
   # Loop over all GRBs in reverse order
   for i in index[::-1]:
@@ -219,7 +250,7 @@ def generate_summary(monitor_file):
 
     table = add(table, i+1)
     table = add(table, name) 
-    table = add(table, item['stage'])
+    table = add(table, item['status'])
     table = add(table, item['triggertime'])
     asctime = time.asctime(time.gmtime(item['triggertime']+offset_gps_to_linux))
     table = add(table, asctime)
@@ -248,12 +279,16 @@ def generate_summary(monitor_file):
       table = add(table, '&mdash')
       table = add(table, '&mdash')
 
+    table +='</tr>'
 
     # Processing only the one having no data of having finished
     #if item['stage']=='NoData' or item['stage']=='Finished'
-    table += ""
+    #table += ""
     
-    print monitor_list[i]['triggertime']
+    filename = cp.get('paths','publishing_path')+'/total_summary.html'
+    f = open(filename,'w')
+    f.write(table)
+    f.close()
 
 # -----------------------------------------------------
 class ExttrigDag(object):
@@ -314,8 +349,8 @@ class ExttrigDag(object):
     """
     
     cmd = 'cd %s;' % destdir
-    for exec in list_exec:
-      cmd += 'ln -s %s/%s .;' % (sourcedir, exec)
+    for execfile in list_exec:
+      cmd += 'ln -s %s/%s .;' % (sourcedir, execfile)
     system_call(cmd)
 
 
@@ -363,8 +398,8 @@ class ExttrigDag(object):
     system_call(cmd)
 
     executable = self.lalapps_dir+'/bin/lalapps_online_datafind'
-    print self.offsource_segment
-    print executable
+    #print self.offsource_segment
+    #print executable
     starttime = self.offsource_segment[0]
     endtime = self.offsource_segment[1]
 
@@ -422,10 +457,10 @@ class ExttrigDag(object):
     (self.lalapps_dir, self.analysis_dir)
     system_call(cmd)
 
-    self.make_links(self, self.glue_dir+'/bin', self.analysis_dir, ['ligo_data_find','ligolw_add'])
-    #cmd = 'cd %s/bin; cp ligo_data_find ligolw_add %s' % \
-    #  (self.glue_dir, self.analysis_dir)
-    #system_call(cmd)
+    #self.make_links(self, self.glue_dir+'/bin', self.analysis_dir, ['ligo_data_find','ligolw_add'])
+    cmd = 'cd %s/bin; cp ligo_data_find ligolw_add %s' % \
+      (self.glue_dir, self.analysis_dir)
+    system_call(cmd)
 
     cmd = 'cd %s/bin; cp pylal_relic pylal_exttrig_llsummary %s' % \
       (self.pylal_dir, self.analysis_dir)
@@ -503,62 +538,7 @@ class ExttrigDag(object):
     else:
 
       return 1
-      # call the last bit of the process, updating the database
-      #self.update_database(1)
       
-  # -----------------------------------------------------
-  def update_database_todelete(self):
-
-    #
-    # add this DAG to the monitor database
-    #
-    
-    starttime = self.offsource_segment[0]
-    endtime = self.offsource_segment[1]
-
-    dag_dict = {'name':self.name, 'path':self.analysis_dir, \
-                 'dag-name':self.dag_file,\
-                 'status':'Running', 'stage':'Inspiral',\
-                 'starttime':starttime, 'endtime':endtime, \
-                 'ifos':self.ifos, 'condorlogpath':self.condor_log_path,\
-                 'triggertime':self.time,\
-                 'right_ascension': self.ra,'declination':self.de,
-                 'qvalues':self.qvalues}
-
-    # update the monitor file
-    monitor_list = pickle.load(file(self.monitor_file))
-    monitor_list.append(dag_dict)
-    pickle.dump(monitor_list, file(self.monitor_file,'w'))
-
-    info('The GRB %s has been added to the database'%self.name)
-    
-  # -----------------------------------------------------
-  def update_database_nodata(self):
-    """
-    Update the database in case not enough data has been found
-    for this GRB:
-    """
-
-    #
-    # add this DAG to the monitor database
-    #
-
-    dag_dict = {'name':self.name, 'path':self.analysis_dir, \
-                 'dag-name':self.dag_file,\
-                 'status':'NoData', 'stage':'NoData',\
-                 'starttime':None, 'endtime':None, \
-                 'ifos':'', 'condorlogpath':'',\
-                 'triggertime':self.time,\
-                 'right_ascensioin': self.ra,'declination':self.de,
-                 'qvalues':self.qvalues}
-
-    # update the monitor file
-    monitor_list = pickle.load(file(self.monitor_file))
-    monitor_list.append(dag_dict)
-    pickle.dump(monitor_list, file(self.monitor_file,'w'))
-
-    info('The GRB %s has been added to the database with NoData.'%self.name)
-
   # -----------------------------------------------------
   def update_database(self, has_data):
     """
@@ -645,7 +625,7 @@ class ExttrigDag(object):
     @param timeoffset: The offset in time for downloading those segments
     """
 
-    seg_names = ['H1:DMT-SCIENCE:1','H1:DMT-SCIENCE:1','V1:ITF_SCIENCEMODE']
+    seg_names = ['H1:DMT-SCIENCE:1','L1:DMT-SCIENCE:1','V1:ITF_SCIENCEMODE']
     ifo_list = ['H1','L1','V1']
     starttime = self.time-timeoffset
     endtime = self.time+timeoffset
@@ -704,16 +684,10 @@ class ExttrigDag(object):
     grb_ifolist.sort()
     ifo_times = "".join(grb_ifolist)
 
-    # Calculate the antenna factors (for informational purpose only)
-    for ifo in ifo_list:
-      _, _, _, q = antenna.response(self.time, self.ra, self.de, \
-                                    0.0, 0.0, 'degree', ifo)
-      self.qvalues[ifo]=q
-
     # make a plot of the segments if required
     if plot_segments_file:
 
-      plot_offset = 100
+      plot_offset = 1000
 
       length_off_source = num_trials*(abs(onSourceSegment))
       plot_offSourceSegment = segments.segment(onSourceSegment[0] - length_off_source,
@@ -724,7 +698,7 @@ class ExttrigDag(object):
       effective_segdict = segdict.map(lambda sl: sl & effective_window)
       plot = PlotSegmentsPlot(trigger)
       plot.add_contents(effective_segdict)
-      plot.set_window(plot_offSourceSegment, plot_offset)
+      plot.set_window(offSourceSegment, plot_offset)
       plot.highlight_segment(onSourceSegment)
       plot.finalize()
       plot.ax.set_title('Segments for GRB '+self.name)
@@ -734,4 +708,12 @@ class ExttrigDag(object):
     # return the main values from this function    
     return offSourceSegment, grb_ifolist
 
+  # -----------------------------------------------------
+  def calculate_optimality(self):
+
+    # Calculate the antenna factors (for informational purpose only)
+    for ifo in ifo_list:
+      _, _, _, q = antenna.response(self.time, self.ra, self.de, \
+                                    0.0, 0.0, 'degree', ifo)
+      self.qvalues[ifo]=q
 
