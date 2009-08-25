057961659b548e486cd696988081b5faa2f1089e
Drew Keppel <drew.keppel@ligo.org>
2009-08-25 08:17:37 -0700

    Adding author info to some ligolw codes

--- pylal/pylal/snglcoinc.py
+++ pylal/pylal/snglcoinc.py
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Copyright (C) 2006  Kipp C. Cannon
+# Copyright (C) 2006  Kipp C. Cannon, Drew G. Keppel
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
