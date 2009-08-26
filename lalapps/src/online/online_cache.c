a20dc77144feed74091a1c80ca51ab9fe929c11f
Adam Mercer <adam.mercer@ligo.org>
2009-08-26 09:55:00 -0500

    Revert "use round(x) instead of floor(x + 0.5)"
    This reverts commit 0ec14a5b1f9c0796725dbdaa170f91d3c0b47c8f.

--- lalapps/src/online/online_cache.c
+++ lalapps/src/online/online_cache.c
@@ -269,7 +269,7 @@ INT4 main(INT4 argc, CHAR *argv[])
   if (XLALGPSCmp(&time_now, &gps_end) == -1)
   {
     /* determine wait time */
-    wait_time = (INT4)round(XLALGPSDiff(&gps_end, &time_now));
+    wait_time = (INT4)floor(XLALGPSDiff(&gps_end, &time_now) + 0.5);
 
     /* wait for data to be available */
     fprintf(stdout, "requested data is in the future, waiting: %ds\n", wait_time);
@@ -280,7 +280,7 @@ INT4 main(INT4 argc, CHAR *argv[])
   if (XLALGPSCmp(latest_time, &gps_end) == -1)
   {
     /* determine wait time */
-    wait_time = (INT4)round(XLALGPSDiff(&gps_end, latest_time));
+    wait_time = (INT4)floor(XLALGPSDiff(&gps_end, latest_time) + 0.5);
 
     /* does wait exceed timeout? */
     if (wait_time > timeout)
@@ -310,7 +310,7 @@ INT4 main(INT4 argc, CHAR *argv[])
       if (XLALGPSCmp(latest_gps, &gps_end) == -1)
       {
         /* determine wait time */
-        wait_time = (INT4)round(XLALGPSDiff(&gps_end, latest_gps));
+        wait_time = (INT4)floor(XLALGPSDiff(&gps_end, latest_gps) + 0.5);
 
         /* does required wait exceed timeout? */
         if ((total_wait + wait_time) > timeout)
