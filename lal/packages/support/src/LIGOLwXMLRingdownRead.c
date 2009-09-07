4ee76d13e62e01bdafec68c64b674750d48c46f1
Nickolas Fotopoulos <nvf@gravity.phys.uwm.edu>
2009-09-07 18:22:40 -0500

    Revert "patch for metaio-8.x"
    This reverts commit 488db0be3677c50d6b97a65cf05f2674b55331e3.

--- lal/packages/support/src/LIGOLwXMLRingdownRead.c
+++ lal/packages/support/src/LIGOLwXMLRingdownRead.c
@@ -248,9 +248,12 @@ SnglRingdownTable* XLALSnglRingdownTableFromLIGOLw (
       }
       else if ( tableDir[j].idx == 19 )
       {
+        /* JC: AVOID BUG IN METAIO -- BAD */
+        union { const char *cs; const unsigned char *cus; } bad;
         thisEvent->event_id = (EventIDColumn *) 
           LALCalloc( 1, sizeof(EventIDColumn) );
-        sscanf( env->ligo_lw.table.elt[tableDir[j].pos].data.lstring.data, "sngl_ringdown:event_id:%" LAL_UINT8_FORMAT, &(thisEvent->event_id->id) );
+        bad.cus = env->ligo_lw.table.elt[tableDir[j].pos].data.ilwd_char.data;
+        sscanf( bad.cs, "sngl_ringdown:event_id:%" LAL_UINT8_FORMAT, &(thisEvent->event_id->id) );
         thisEvent->event_id->snglRingdownTable = thisEvent;
       }
       else
