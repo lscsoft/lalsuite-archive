62c1282a57584732576ff262234b5fbc5fa16e0d
Chad Hanna <chad.hanna@ligo.org>
2009-09-13 17:07:37 -0700

    added extension

--- pylal/setup.py
+++ pylal/setup.py
@@ -220,6 +220,14 @@ setup(
 			libraries = lal_pkg_config.libs,
 			library_dirs = lal_pkg_config.libdirs,
 			runtime_library_dirs = lal_pkg_config.libdirs
+		),
+		Extension(
+			"pylal.spawaveform",
+			["src/spawaveform.c"],
+			include_dirs = lal_pkg_config.incdirs + [numpy_get_include()],
+			libraries = lal_pkg_config.libs,
+			library_dirs = lal_pkg_config.libdirs,
+			runtime_library_dirs = lal_pkg_config.libdirs
 		)
 	],
 	scripts = [
