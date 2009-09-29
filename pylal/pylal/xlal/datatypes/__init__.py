39a45555142359286acd26abc7461fb17089f7dc
Kipp Cannon <kipp.cannon@ligo.org>
2009-09-29 15:50:50 -0700

    add wrapping of COMPLEX16FrequencySeries

--- pylal/pylal/xlal/datatypes/__init__.py
+++ pylal/pylal/xlal/datatypes/__init__.py
@@ -2,4 +2,4 @@ __author__ = "Kipp Cannon <kipp.cannon@gligo.org>"
 __date__ = "$Date$"[7:-2]
 __version__ = "$Revision$"[11:-2]
 
-__all__ = ["lalunit", "ligotimegps", "real8frequencyseries"]
+__all__ = ["complex16frequencyseries", "lalunit", "ligotimegps", "real8frequencyseries"]
