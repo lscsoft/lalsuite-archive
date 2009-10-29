f25a3b36bc735cadd6c14b88373cdbf4ba7ad0b2
Romain Gouaty <romain.gouaty@ligo.org>
2009-10-29 11:39:31 -0700

    set up V1 rawdata scans as remote jobs on LIGO clusters

--- pylal/bin/wscan_background.py
+++ pylal/bin/wscan_background.py
@@ -8,7 +8,7 @@ __title__ = "Generate bakground of omega scans"
 
 ##############################################################################
 
-import os, sys, subprocess, string
+import os, sys, subprocess, string, socket
 from optparse import *
 import ConfigParser
 import time
@@ -67,8 +67,15 @@ class create_default_config(object):
     for config in ["H1config","H2config","L1config","V1config"]:
       cp.set("fu-bg-seismic-qscan",config,self.__qscan_config("s5_foreground_" + self.__config_name(config[:2],'seismic') + ".txt"))
 
+    # OUTPUT SECTION
     cp.add_section("fu-output")
     cp.set("fu-output","log-path","/usr1/" + os.getenv("USER"))
+
+    # REMOTE JOBS SECTION
+    cp.add_section("fu-remote-jobs")
+    remoteIfos,remoteJobs = self.get_remote_jobs()
+    cp.set('fu-remote-jobs','remote-ifos',remoteIfos)
+    cp.set('fu-remote-jobs','remote-jobs',remoteJobs)
 
     self.cp = cp
 
@@ -95,6 +102,19 @@ class create_default_config(object):
             "V1":{"hoft":"V1_hoft_cbc","rds":"V1-raw-cbc","seismic":"V1-raw-seismic-cbc"}
             }
     return fileMap[ifo][type]
+
+  def get_remote_jobs(self):
+    host = self.__get_hostname()
+    #FIXME add more hosts as you need them
+    if 'ligo.caltech.edu' or 'ligo-la.caltech.edu' or 'ligo-wa.caltech.edu' or 'phys.uwm.edu' or 'aei.uni-hannover.de' or 'phy.syr.edu' in host:
+      remote_ifos = "V1"
+      remote_jobs = "ligo_data_find_Q_RDS_,wpipeline_BG_RDS_,wpipeline_BG_SEIS_RDS_"
+      return remote_ifos, remote_jobs
+    return '', ''
+
+  def __get_hostname(self):
+    host = socket.getfqdn()
+    return host
 
   def which(self,prog):
     which = subprocess.Popen(['which',prog], stdout=subprocess.PIPE)
@@ -191,7 +211,7 @@ if opts.config_file:
  config.read(opts.config_file)
  cp = overwrite_config(cp,config)
 
-ifo_list = []
+ifos_list = []
 for j in range(0,len(opts.ifos)-1,2):
   ifo = opts.ifos[j:j+2]
   ifos_list.append(ifo)
