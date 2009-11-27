2015cc8f75595b1a95fea498af32b4e199575a17
Kipp Cannon <kipp.cannon@ligo.org>
2009-11-26 17:59:43 -0800

    handle some exceptions better

--- pylal/src/xlal/tools.c
+++ pylal/src/xlal/tools.c
@@ -775,8 +775,7 @@ static PyObject *pylal_XLALCalculateEThincaParameter(PyObject *self, PyObject *a
 	result = XLALCalculateEThincaParameter(&row1->sngl_inspiral, &row2->sngl_inspiral, &accuracyparams);
 
 	if(XLAL_IS_REAL8_FAIL_NAN(result)) {
-		XLALClearErrno();
-		PyErr_SetString(PyExc_ValueError, "not coincident");
+		pylal_set_exception_from_xlalerrno();
 		return NULL;
 	}
 
@@ -819,8 +818,7 @@ static PyObject *pylal_XLAL3DRinca(PyObject *self, PyObject *args)
 	result = XLAL3DRinca(&row1->sngl_ringdown, &row2->sngl_ringdown);
 
 	if(XLAL_IS_REAL8_FAIL_NAN(result)) {
-		XLALClearErrno();
-		PyErr_SetString(PyExc_ValueError, "not coincident");
+		pylal_set_exception_from_xlalerrno();
 		return NULL;
 	}
 
