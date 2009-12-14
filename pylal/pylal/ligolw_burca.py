0e439e6ed466bba1d9a4cc1fe7e919e5f34ae783
Chad Hanna <chad.hanna@ligo.org>
2009-12-14 15:41:45 -0800

    stopped kipp from whining about his precious burca and its busted table

--- pylal/pylal/ligolw_burca.py
+++ pylal/pylal/ligolw_burca.py
@@ -163,6 +163,10 @@ def make_multi_burst(process_id, coinc_event_id, events):
 
 	multiburst.amplitude = max((event.ms_confidence, event.ms_hrss) for event in events)[1]
 
+	# populate the piece of shit known as false alarm rate that chad insists is useful but he sucks.
+
+	multiburst.false_alarm_rate = None
+
 	# done
 
 	return multiburst
