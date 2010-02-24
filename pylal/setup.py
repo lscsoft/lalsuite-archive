77bd97b124c4d1acbe4b3d55576877bf5989916c
Alexander Dietz <alexander.dietz@ligo.org>
2010-02-24 08:06:22 -0600

    setup.py: reflect the new script 'llclosebox' in the setup

--- pylal/setup.py
+++ pylal/setup.py
@@ -260,6 +260,7 @@ setup(
 		os.path.join("bin", "pylal_exttrig_llmonitor"),
 		os.path.join("bin", "pylal_exttrig_llsummary"),
 		os.path.join("bin", "pylal_exttrig_llopenbox"),
+		os.path.join("bin", "pylal_exttrig_llclosebox"),
 		os.path.join("bin", "pylal_query_dq"),
 		os.path.join("bin", "pylal_relic"),
 		os.path.join("bin", "plotethinca"),
