fc31d759a328548c544bdfb7f45da0e1db5326e5
Kipp Cannon <kipp.cannon@ligo.org>
2010-03-07 19:41:09 -0800

    add lalapps_string_plot_binj

--- pylal/setup.py
+++ pylal/setup.py
@@ -450,6 +450,7 @@ setup(
 		os.path.join("bin", "lalapps_run_sqlite"),
 		os.path.join("bin", "lalapps_stringfinal"),
 		os.path.join("bin", "lalapps_string_meas_likelihood"),
+		os.path.join("bin", "lalapps_string_plot_binj"),
 		os.path.join("bin", "lalapps_string_plot_likelihood"),
 		os.path.join("bin", "wscan_background.py"),
 		os.path.join("bin", "wscan_bg_setup_log.py"),
