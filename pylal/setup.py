71efe842e413b809bb99524d10361688aaa31e5d
Kipp Cannon <kipp.cannon@ligo.org>
2010-03-12 16:28:37 -0800

    add lalapps_string_calc_likelihood

--- pylal/setup.py
+++ pylal/setup.py
@@ -449,6 +449,7 @@ setup(
 		os.path.join("bin", "lalapps_remote_cache"),
 		os.path.join("bin", "lalapps_run_sqlite"),
 		os.path.join("bin", "lalapps_stringfinal"),
+		os.path.join("bin", "lalapps_string_calc_likelihood"),
 		os.path.join("bin", "lalapps_string_meas_likelihood"),
 		os.path.join("bin", "lalapps_string_plot_binj"),
 		os.path.join("bin", "lalapps_string_plot_likelihood"),
