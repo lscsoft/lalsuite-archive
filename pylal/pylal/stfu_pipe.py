ec40ee923d9affd6888443d0124fc88d8f5c424b
Romain Gouaty <romain.gouaty@ligo.org>
2010-05-20 00:56:52 -0700

    cancel patch for new patch for omega scan backgroud output
    Cristina is taking care of this.

--- pylal/pylal/stfu_pipe.py
+++ pylal/pylal/stfu_pipe.py
@@ -66,14 +66,6 @@ def get_day_boundaries(time):
   end_gps = start_gps + 86400
   return str(start_gps),str(end_gps)
 
-def figure_out_site(ifo):
-
-	siteDico = {"H1":"LHO","H2":"LHO","L1":"LLO","V1":"Virgo"}
-	if ifo in siteDico:
-		return siteDico[ifo]
-	else:
-		print >> sys.stderr, "ifo " + ifo + "is not defined in siteDico dictionary"
-		sys.exit(1)
 
 def figure_out_type(time, ifo=None, data_type='hoft'):
 	"""
@@ -800,7 +792,7 @@ The omega scan command line is
 
 		if variety == "bg":
 			self.add_var_arg('scan')
-			preString = "omega/" + science_run(time).upper() + "/background/" + figure_out_site(ifo)
+			preString = "omega/" + science_run(time).upper() + "/background"
 		else:
 			self.add_var_arg('scan -r')
 			preString = "omega/" + science_run(time).upper() + "/foreground"
@@ -935,7 +927,7 @@ class fuRemoteQscanNode(pipeline.CondorDAGNode,FUNode):
 		self.scan_ifo = ifo
 
 		if variety == "bg":
-			preString = "omega/" + science_run(time).upper() + "/background/" + figure_out_site(ifo)
+			preString = "omega/" + science_run(time).upper() + "/background"
 		else:
 			preString = "omega/" + science_run(time).upper() + "/foreground"
 		config = cp.get('fu-'+variety+'-'+type+'-qscan', ifo+'config').strip()
