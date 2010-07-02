b24eb2de9d6f199f633d53c60fa00e8750213164
Vladimir Dergachev <vladimir.dergachev@ligo.org>
2010-07-02 09:28:20 -0700

    Allow to proceed with all SFTs vetoed if no-demodulation or no-decomposition is requested. Useful for dumping data from pure injections (no background noise). Make sure timebase test works when no spindown start time has been passed on command line.

--- lalapps/src/pulsar/PowerFlux/powerflux2.c
+++ lalapps/src/pulsar/PowerFlux/powerflux2.c
@@ -568,7 +568,7 @@ if(args_info.dataset_given) {
 	fprintf(stderr, "Loading data from dataset %s\n", args_info.dataset_arg);
 	load_dataset_from_file(args_info.dataset_arg);
 	nsegments=total_segments();
-	if(nsegments==vetoed_segments()) {
+	if(nsegments==vetoed_segments() && !args_info.no_demodulation_arg && !args_info.no_decomposition_arg) {
 		fprintf(LOG, "All SFTs vetoed, aborting!\n");
 		fprintf(stderr, "All SFTs vetoed, aborting!\n");
 		exit(-1);
@@ -786,8 +786,8 @@ dump_floats("fine_longitude.dat", fine_grid->longitude, fine_grid->npoints, 1);
 /* COMP3 stage */
 
 if(args_info.no_decomposition_arg){
-	fprintf(stderr,"Exiting as requested (--no-decomposition=1\n");
-	fprintf(LOG,"Exiting as requested (--no-decomposition=1\n");
+	fprintf(stderr,"Exiting as requested (--no-decomposition=1)\n");
+	fprintf(LOG,"Exiting as requested (--no-decomposition=1)\n");
 	wrap_up();
 	exit(0);
 	}
@@ -796,8 +796,8 @@ if(args_info.no_decomposition_arg){
 
 
 if(args_info.no_demodulation_arg){
-	fprintf(stderr,"Exiting as requested (--no-demodulation=1\n");
-	fprintf(LOG,"Exiting as requested (--no-demodulation=1\n");
+	fprintf(stderr,"Exiting as requested (--no-demodulation=1)\n");
+	fprintf(LOG,"Exiting as requested (--no-demodulation=1)\n");
 	wrap_up();
 	exit(0);	
 	}
@@ -886,8 +886,7 @@ if((fabs(max_gps()-spindown_start)>args_info.expected_timebase_arg*24.0*3600.0*3
 		(max_gps()-spindown_start)/(24.0*3600.0*31.0), args_info.expected_timebase_arg);
 	exit(-1);
 	}
-if((fabs(spindown_start-args_info.spindown_start_time_arg)>args_info.expected_timebase_arg*24*3600*31 )||
-	(fabs(args_info.spindown_start_time_arg-min_gps())>args_info.expected_timebase_arg*24*3600*31)) {
+if((fabs(spindown_start-min_gps())>args_info.expected_timebase_arg*24*3600*31 )) {
 	fprintf(stderr, "**** ERROR(2): loaded timebase is larger than expected %f(loaded) vs %f(--expected-timebase)\n", 
 		(spindown_start-min_gps())/(24.0*3600.0*31.0), args_info.expected_timebase_arg);
 	fprintf(LOG, "**** ERROR(2): loaded timebase is larger than expected %f(loaded) vs %f(--expected-timebase)\n", 
