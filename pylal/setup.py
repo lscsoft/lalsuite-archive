ed8a3ecb7790af5a6ac105d6bed273efaa836366
Sarah Caudill <sarah.caudill@ligo.org>
2010-07-11 09:56:18 -0700

    added ring_post to path

--- pylal/setup.py
+++ pylal/setup.py
@@ -562,7 +562,8 @@ setup(
 		os.path.join("bin", "run_skypoints.py"),
 		os.path.join("bin", "make_skypoints_grids.py"),
 		os.path.join("bin", "make_skypoints_rankings.py"),
-                os.path.join("bin", "plot_skypoints.py")
+                os.path.join("bin", "plot_skypoints.py"),
+                os.path.join("bin", "ring_post")
 	],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
