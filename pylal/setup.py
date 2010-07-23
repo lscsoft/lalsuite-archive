33633965b56ed012c8a6f99e6e10c653f64b8d12
Alexander Dietz <alexander.dietz@ligo.org>
2010-07-23 03:23:35 -0500

    setup.py: inserted new code: pylal_exttrig_llpage

--- pylal/setup.py
+++ pylal/setup.py
@@ -259,6 +259,7 @@ setup(
 		os.path.join("bin", "pylal_grbtimeslide_stats"),
 		os.path.join("bin", "pylal_exttrig_llmonitor"),
 		os.path.join("bin", "pylal_exttrig_llsummary"),
+		os.path.join("bin", "pylal_exttrig_llpage"),
 		os.path.join("bin", "pylal_exttrig_llbox"),
 		os.path.join("bin", "pylal_query_dq"),
 		os.path.join("bin", "pylal_relic"),
