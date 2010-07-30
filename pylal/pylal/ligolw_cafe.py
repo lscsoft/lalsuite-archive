d2f0fe6aadd290f1c87804fea790c5d32e7c76f1
Kipp Cannon <kipp.cannon@ligo.org>
2010-07-30 16:48:27 -0500

    increase verbosity

--- pylal/pylal/ligolw_cafe.py
+++ pylal/pylal/ligolw_cafe.py
@@ -503,7 +503,7 @@ def ligolw_cafe(cache, offset_vectors, verbose = False, extentlimit = None):
 	packer = CafePacker(outputcaches)
 	packer.set_offset_vectors(offset_vectors)
 	if verbose:
-		print >>sys.stderr, "packing files ..."
+		print >>sys.stderr, "packing files (considering %s offset vectors) ..." % len(offset_vectors)
 	for n, cacheentry in enumerate(cache):
 		if verbose and not n % 13:
 			print >>sys.stderr, "\t%.1f%%\t(%d files, %d caches)\r" % (100.0 * n / len(cache), n + 1, len(outputcaches)),
