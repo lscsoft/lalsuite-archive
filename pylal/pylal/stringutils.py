3f5c0d6d32d8c873ef69387d7bbf23d1d0522aca
Kipp Cannon <kipp.cannon@ligo.org>
2010-08-11 13:58:19 -0500

    move create_recovered_likelihood_view() into library code

--- pylal/pylal/stringutils.py
+++ pylal/pylal/stringutils.py
@@ -220,3 +220,48 @@ def time_slides_livetime(seglists, time_slides, min_instruments, verbose = False
 def get_coincparamsdistributions(xmldoc):
 	coincparamsdistributions, process_id = ligolw_burca_tailor.coinc_params_distributions_from_xml(xmldoc, u"string_cusp_likelihood")
 	return coincparamsdistributions
+
+
+#
+# =============================================================================
+#
+#                              Database Utilities
+#
+# =============================================================================
+#
+
+
+def create_recovered_likelihood_table(connection, bb_coinc_def_id):
+	"""
+	Create a temporary table containing two columns:  the simulation_id
+	of an injection, and the highest likelihood ratio at which that
+	injection was recovered by a coincidence of type bb_coinc_def_id.
+	"""
+	cursor = connection.cursor()
+	cursor.execute("""
+CREATE TEMPORARY TABLE recovered_likelihood (simulation_id TEXT PRIMARY KEY, likelihood REAL)
+	""")
+	cursor.execute("""
+INSERT OR REPLACE INTO
+	recovered_likelihood
+SELECT
+	sim_burst.simulation_id AS simulation_id,
+	MAX(coinc_event.likelihood) AS likelihood
+FROM
+	sim_burst
+	JOIN coinc_event_map AS a ON (
+		a.table_name == "sim_burst"
+		AND a.event_id == sim_burst.simulation_id
+	)
+	JOIN coinc_event_map AS b ON (
+		b.coinc_event_id == a.coinc_event_id
+	)
+	JOIN coinc_event ON (
+		b.table_name == "coinc_event"
+		AND b.event_id == coinc_event.coinc_event_id
+	)
+WHERE
+	coinc_event.coinc_def_id == ?
+GROUP BY
+	sim_burst.simulation_id
+	""", (bb_coinc_def_id,))
