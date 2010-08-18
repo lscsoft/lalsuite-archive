f4e681a0a4eb164763bce223bdafe4d385a15540
Kipp Cannon <kipp.cannon@ligo.org>
2010-08-18 11:15:50 -0500

    fix disk-full failure in add_noninjections()
    by reverting to an earlier implementation that doesn't require sqlite to
    build an intermediate query result on disk but is slower
    
    also clean up add_injections()

--- pylal/pylal/ligolw_burca_tailor.py
+++ pylal/pylal/ligolw_burca_tailor.py
@@ -317,43 +317,46 @@ class Stats(object):
 
 	def add_noninjections(self, param_func, database, *args):
 		# iterate over burst<-->burst coincs
-		for coinc_event_id, rows in itertools.groupby(database.connection.cursor().execute("""
+		cursor = database.connection.cursor()
+		for coinc_event_id, time_slide_id in database.connection.cursor().execute("""
 SELECT
-	coinc_event.coinc_event_id,
-	time_slide.offset,
-	sngl_burst.*
+	coinc_event_id,
+	time_slide_id
 FROM
-	sngl_burst
-	JOIN coinc_event_map ON (
+	coinc_event
+WHERE
+	coinc_def_id == ?
+		""", (database.bb_definer_id,)):
+			rows = [(database.sngl_burst_table.row_from_cols(row), row[-1]) for row in cursor.execute("""
+SELECT
+	sngl_burst.*
+	time_slide.offset,
+FROM
+	coinc_event_map
+	JOIN sngl_burst ON (
 		coinc_event_map.table_name == 'sngl_burst'
-		AND coinc_event_map.event_id == sngl_burst.event_id
-	)
-	JOIN coinc_event ON (
-		coinc_event.coinc_event_id == coinc_event_map.coinc_event_id
+		AND sngl_burst.event_id == coinc_event_map.event_id
 	)
 	JOIN time_slide ON (
-		coinc_event.time_slide_id == time_slide.time_slide_id
-		AND sngl_burst.ifo == time_slide.instrument
+		time_slide.instrument == sngl_burst.ifo
 	)
 WHERE
-	coinc_event.coinc_def_id == ?
-ORDER BY
-	coinc_event.coinc_event_id
-		""", (database.bb_definer_id,)), lambda row: row[0]):
-			events = []
-			offsetdict = {}
-			for row in rows:
-				events.append(database.sngl_burst_table.row_from_cols(row[2:]))
-				offsetdict[events[-1].ifo] = row[1]
+	coinc_event_map.coinc_event_id == ?
+	AND time_slide.time_slide_id == ?
+			""", (coinc_event_id, time_slide_id))]
+			events = [event for event, offset in rows]
+			offsetdict = dict((event.ifo, offset) for event, offset in rows)
 			if any(offsetdict.values()):
 				self._add_background(param_func, events, offsetdict, *args)
 			else:
 				self._add_zero_lag(param_func, events, offsetdict, *args)
+		cursor.close()
 
 
 	def add_injections(self, param_func, database, *args):
 		# iterate over burst<-->burst coincs matching injections
 		# "exactly"
+		cursor = database.connection.cursor()
 		for values in database.connection.cursor().execute("""
 SELECT
 	sim_burst.*,
@@ -380,9 +383,7 @@ WHERE
 
 			# retrieve the list of the sngl_bursts in this
 			# coinc, and their time slide dictionary
-			events = []
-			offsetdict = {}
-			for values in database.connection.cursor().execute("""
+			rows = [(database.sngl_burst_table.row_from_cols(row), row[-1]) for row in cursor.execute("""
 SELECT
 	sngl_burst.*,
 	time_slide.offset
@@ -401,18 +402,13 @@ FROM
 	)
 WHERE
 	coinc_event.coinc_event_id == ?
-			""", (coinc_event_id,)):
-				# reconstruct the burst event
-				event = database.sngl_burst_table.row_from_cols(values)
-
-				# add to list
-				events.append(event)
-
-				# store the time slide offset
-				offsetdict[event.ifo] = values[-1]
+			""", (coinc_event_id,))]
+			events = [event for event, offset in rows]
+			offsetdict = dict((event.ifo, offset) for event, offset in rows)
 
 			# pass the events to whatever wants them
 			self._add_injections(param_func, sim, events, offsetdict, *args)
+		cursor.close()
 
 	def finish(self):
 		pass
