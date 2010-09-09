708494810ee8d0fd00db123387182856ecc9de6f
Thomas Dent <thomas.dent@ligo.org>
2010-09-09 11:33:29 -0500

    Add column 'minimum_duration' to CoincInspiralTable & calculate it in thinca_to_coinc PR 3529

--- glue/glue/ligolw/lsctables.py
+++ glue/glue/ligolw/lsctables.py
@@ -1417,6 +1417,7 @@ class CoincInspiralTable(table.Table):
 		"end_time_ns": "int_4s",
 		"mass": "real_8",
 		"mchirp": "real_8",
+		"minimum_duration": "real_8",
 		"snr": "real_8",
 		"false_alarm_rate": "real_8",
 		"combined_far": "real_8"
