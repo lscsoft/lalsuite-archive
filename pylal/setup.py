e1cc8c3a3ccb5ac0f287429b4a4089ef7d6834ae
Benjamin Aylott <benjamin.aylott@ligo.org>
2010-10-28 18:24:13 +0100

    Fixed some space-tab mixing I introduced in earlier commit.

--- pylal/setup.py
+++ pylal/setup.py
@@ -598,8 +598,7 @@ setup(
 		os.path.join("bin", "coh_PTF_trig_cluster"),
 		os.path.join("bin", "coh_PTF_trig_combiner"),
 		os.path.join("bin", "ring_post"),
-        os.path.join("bin","cbcBayesPostProc.py")
-
+		os.path.join("bin","cbcBayesPostProc.py")
 	],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
