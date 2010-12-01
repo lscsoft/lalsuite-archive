fc074f48019fc4a02f33cb3a88f4ec268f4b5c0f
Thomas Dent <thomas.dent@ligo.org>
2010-12-01 14:37:50 +0100

    pipeline.py: method splitting a long segment into minimum-length subsegments
    max segment length will be 3dt/2, short segments stay short :(

--- glue/glue/pipeline.py
+++ glue/glue/pipeline.py
@@ -2780,9 +2780,10 @@ class ScienceData:
     self.coalesce()
     return len(self)
 
+
   def split(self, dt):
     """
-      Split the segments in the list is subsegments at least as long as dt
+      Split the segments in the list into subsegments with maximum length dt
     """
     outlist=[]
     for seg in self:
@@ -2804,6 +2805,34 @@ class ScienceData:
     self.__sci_segs = outlist
     return len(self)
 
+  def split_generously(self, dt):
+    """
+      Split the segments in the list into subsegments with default length dt. 
+      Excess time at the end of a long segment will be distributed into the 
+      last two sub segments. Max segment length will be 3dt/2. 
+      Short segments stay short :(     
+    """
+    outlist=[]
+    for seg in self:
+      start = seg.start()
+      stop = seg.end()
+      id = seg.id()
+
+      while start < stop:
+        tmpstop = start + dt
+        # test if a lengthened subsegment would go over the end of the segment
+        if tmpstop + int( dt/2 ) > stop:
+          tmpstop = stop
+        elif tmpstop + 2*dt > stop:
+          # test if there is less than 3*dt left in the segment
+          tmpstop = int( (start + stop)/2 )
+        x = ScienceSegment(tuple([id,start,tmpstop,tmpstop-start]))
+        outlist.append(x)
+        start = tmpstop
+
+    # save the split list and return length
+    self.__sci_segs = outlist
+    return len(self)
 
 
 class LsyncCache:
