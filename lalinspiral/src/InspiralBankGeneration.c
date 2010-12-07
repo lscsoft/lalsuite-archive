7332a3942d652995b70dfa4732b04f0f20fc3308
Diego Fazi <diego.fazi@ligo.org>
2010-12-07 07:56:29 +0100

    Removed restrictions on mass2

--- lalinspiral/src/InspiralBankGeneration.c
+++ lalinspiral/src/InspiralBankGeneration.c
@@ -346,8 +346,7 @@ LALInspiralBankGeneration(
         for( cnt = 0; cnt < *ntiles; cnt++ )
         {
           /* restrict the bank boundaries to the region of validity of PTF */
-          if ( coarseList[cnt].params.mass1 < 6.0 ||
-               coarseList[cnt].params.mass2 > 3.0 ) continue;
+          if ( coarseList[cnt].params.mass1 < 6.0 ) continue;
           bank = bank->next = (SnglInspiralTable *) LALCalloc( 1, sizeof(
                 SnglInspiralTable ) );
           if (bank == NULL)
