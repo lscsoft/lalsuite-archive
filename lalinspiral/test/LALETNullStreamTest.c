7443e4b13647f8022e5cfb9f91f3f7d394f875f7
Walter Del Pozzo <walter.delpozzo@ligo.org>
2011-02-26 13:19:23 +0000

    fixed the nullstream calculation. the PSD is calculated correctly. need to fix the argument parsing.

--- lalinspiral/test/LALETNullStreamTest.c
+++ lalinspiral/test/LALETNullStreamTest.c
@@ -23,7 +23,7 @@
 
 NRCSID( LALETNULLSTREAMTESTC, "$Id$" );
 
-#define USAGE "Usage: %s [--GPSStart INT] [--duration REAL8] [--Srate REAL8]\n"
+#define USAGE "Usage: %s [--GPSStart INT] [--Nsegs INT] [--SegDur REAL8] [--Srate REAL8]\n"
 
 extern int lalDebugLevel;
 
@@ -34,9 +34,9 @@ extern int lalDebugLevel;
  ******************************************************************************/
 
 // GLOBAL VARIABLES USED IN ARGUMENT PARSING - SETS THE DEFAULT VALUE
-REAL8 duration_global=0;
+REAL8 duration_global=1000.0;
 LIGOTimeGPS GPSStart_global; // NB: NO INFRASTRUCTURE FOR NANOSECOND INPUT!
-REAL8 SampleRate_global=1024.0; // JUSTIFY CHOICE
+REAL8 SampleRate_global=2048.0; // JUSTIFY CHOICE WDP: the data have been generated with this Srate
 
 /*******************************************************************************
  *
@@ -79,10 +79,23 @@ int main( int argc, char *argv[] )
     REAL8TimeSeries *NullStream;
     REAL8FrequencySeries *inverse_spectrum;
     REAL8FFTPlan *fwdplan = NULL;
-	REAL8Window  *windowplan = NULL;
+    REAL8FFTPlan *revplan = NULL;
+    REAL8Window  *windowplan = NULL;
     REAL8 deltaF=0.0;
-    
+    UINT4 nSegs=100;
+    REAL8 segDur,strideDur;
+    UINT4 seglen,stride;
+    GPSStart_global.gpsSeconds=802338816;
     NullStream = (REAL8TimeSeries *) LALMalloc(sizeof(REAL8TimeSeries));
+    segDur = duration_global/(REAL8)nSegs;
+    seglen=(UINT4)(segDur*SampleRate_global);
+    segDur = seglen/SampleRate_global;
+    nSegs =(INT4)floor(duration_global/segDur);
+
+    fprintf(stderr,"Choosing %i segments length %i, (%f s)\n",nSegs,seglen,segDur);
+
+    stride = seglen; /* Overlap the padding */
+    strideDur = stride / SampleRate_global;
     
     /***************************************************************************
      *
@@ -93,11 +106,17 @@ int main( int argc, char *argv[] )
     /* Get the arguments and act on them */
     initialise(argc,argv); 
     NullStream = LALETNullStream(&GPSStart_global,duration_global);
-    
+    FILE *nsout;
+    nsout=fopen("temp_ns.dat","w");
+    uint k;
+    for (k=0;k<NullStream->data->length;k++){
+	fprintf(nsout,"%g %g\n",k*NullStream->deltaT,NullStream->data->data[k]);
+	}
+    fclose(nsout); 
     // RESAMPLE TIMESERIES (WHY IS THIS DONE?)
-    XLALResampleREAL8TimeSeries(NullStream,1.0/SampleRate_global);
-    deltaF=(REAL8)SampleRate_global/duration_global;
-    
+    //XLALResampleREAL8TimeSeries(NullStream,1.0/SampleRate_global);
+    deltaF=(REAL8)SampleRate_global/seglen;//(REAL8)1.0/((REAL8) NullStream->data->length*NullStream->deltaT);
+    printf("deltaF : %g\n",deltaF);    
     /***************************************************************************
      *
      *  COMPUTE PSD (PUT IN FUNCTION INSTEAD OF IN THE TEST FUNCTION)
@@ -105,18 +124,18 @@ int main( int argc, char *argv[] )
      * 
      **************************************************************************/
     
-    windowplan = XLALCreateTukeyREAL8Window( duration_global, 0.1);
-    fwdplan = XLALCreateForwardREAL8FFTPlan(duration_global, 0 );
-    inverse_spectrum = (REAL8FrequencySeries *)XLALCreateREAL8FrequencySeries("inverse spectrum",&(NullStream->epoch),0.0,deltaF,&lalDimensionlessUnit,NullStream->data->length/2);
-    
-    XLALREAL8AverageSpectrumMedian(inverse_spectrum ,
-                                         NullStream,
-                                         ((size_t)duration_global),
-                                         0.0,
-                                         windowplan,
-                                         fwdplan);
-    
-    
+    windowplan = XLALCreateTukeyREAL8Window(seglen,0.1*(REAL8)8.0*SampleRate_global/(REAL8)seglen);
+    int check=0;
+    fwdplan = XLALCreateForwardREAL8FFTPlan( seglen, 0 );
+    revplan = XLALCreateReverseREAL8FFTPlan( seglen, 0 );
+    fprintf(stderr,"Shrinking... (lost %d samples from end)\n",NullStream->data->length-(seglen*nSegs));
+    NullStream=(REAL8TimeSeries *)XLALShrinkREAL8TimeSeries(NullStream,(size_t) 0, (size_t) seglen*nSegs);
+    fprintf(stderr,"Computing power spectrum, seglen %i stride %i\n",seglen,stride);
+    inverse_spectrum = (REAL8FrequencySeries *)XLALCreateREAL8FrequencySeries("inverse spectrum",&(NullStream->epoch),0.0,deltaF,&lalDimensionlessUnit,seglen/2+1);
+    check=XLALREAL8AverageSpectrumMedian(inverse_spectrum,NullStream,(UINT4)seglen,(UINT4)stride,windowplan,fwdplan);
+    if (check) {fprintf(stderr,"Failed! \n");exit(-1);}
+    check|=XLALREAL8SpectrumInvertTruncate(inverse_spectrum, 10.0, seglen, (seglen-stride)/4, fwdplan, revplan ); 
+    if (check) {fprintf(stderr,"Failed computing spectrum! Exiting...\n");exit(-1);}    
     // PSD OUTPUT FILE
     FILE *psdout;
 	char psdname[100];
