06f07f2af363b42657c757a7ffde9c7bc5139a78
Ben Aylott <benjamin.aylott@ligo.org>
2011-04-01 02:30:03 +0100

    added addVariable wrapper

--- pylal/src/lalinference.c
+++ pylal/src/lalinference.c
@@ -10,7 +10,7 @@ const char LIDocString[] =
 typedef struct {
     PyObject_HEAD
     /* Type-specific fields go here */
-    LALVariables var;
+    LALVariables* var;
 } li_LALVariablesObject;
 
 /* Destructor for LALVariables */
@@ -25,10 +25,74 @@ static PyMethodDef LALVariables_methods[]= {
     {NULL} /* Sentinel */
 };
 
+static int add_variable(li_LALVariablesObject *self,PyObject* args){
+    PyObject *pyname=NULL,*pyvalue=NULL,*pyvarytype=NULL;
+
+    char *name;char* temp;void* value;VariableType type;ParamVaryType varytype;
+    
+    if (! PyArg_ParseTuple(args,"OOO",&pyname,&pyvalue,&pyvarytype)) return -1;
+
+    /*Extract name of variable*/
+    if(PyString_Check(pyname)){
+        name=PyString_AsString(pyname);
+    }
+    else{
+        PyErr_SetObject(PyExc_TypeError, pyname);
+        return -1;
+    }
+
+    /*Extract and determine type of parameter value*/
+    if(PyInt_Check(pyvalue)){
+        value=(void*)((INT8_t*)PyInt_AsLong(pyvalue));
+        type=INT8_t;
+    }   
+    else if(PyFloat_Check(pyvalue)){
+        value=(void*)((REAL8_t*)PyFloat_AsDouble(pyvalue));
+        type=REAL8_t;
+    }
+    else{
+        PyErr_SetObject(PyExc_TypeError, pyvalue);
+        return -1;
+    }
+
+    /*Determine variable wrapping type from string*/
+    if(PyString_Check(pyvarytype)){
+        temp=PyString_AsString(pyvarytype);
+        Py_INCREF(pyvarytype);
+        if(!strcmp(temp,"linear")){
+            varytype=PARAM_LINEAR;
+        }
+        else if(!strcmp(temp,"circular")){
+            varytype=PARAM_CIRCULAR;
+        }
+        else if(!strcmp(temp,"fixed")){
+            varytype=PARAM_FIXED;
+        }
+        else if(!strcmp(temp,"output")){
+            varytype=PARAM_OUTPUT;
+        }
+        else {
+            PyErr_SetObject(PyExc_ValueError,pyvarytype);
+            return -1;
+        }
+    }
+    else{
+        PyErr_SetObject(PyExc_TypeError, pyvarytype);
+        return -1;
+    }
+
+    /* If we survived then call addVariable with self->var ...*/
+
+    addVariable(self->var,name,value,type,varytype);
+
+}
+
 static int LALVariables_init(li_LALVariablesObject *self, PyObject *args, PyObject *kwds)
 {
     /* Should fill in the array using a dictionary as input */
     
+    memset(self->var,0,sizeof(LALVariables));
+
     return 0;
 }
 
@@ -74,9 +138,6 @@ static PyTypeObject li_LALVariablesType = {
     PyType_GenericNew,                 /* tp_new */
 };
 
-static PyMethodDef li_methods[]={
-    {NULL} /* Sentinel */
-};
 
 PyMODINIT_FUNC
 init_lalinference(void)
