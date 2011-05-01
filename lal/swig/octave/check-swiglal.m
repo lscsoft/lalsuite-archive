cc814c893ec1f57c89de90d93debb1b0c24fadea
Karl Wette <karl.wette@ligo.org>
2011-05-01 22:02:19 +0200

    swiglal: allocate LAL structs using LAL memory functions
    - Adding the symbol 'SWIGLAL_STRUCT_LALALLOC' to a LAL struct
      will make SWIG's default constructor/destructors use LAL
      memory allocation functions to create/destroy the struct.
    
    - Added test code for Octave and Python

--- lal/swig/octave/check-swiglal.m
+++ lal/swig/octave/check-swiglal.m
@@ -5,6 +5,38 @@ function msg(varargin)
   disp([program_name, ": ", sprintf(varargin{:})]);
 endfunction
 
+## check memory allocation
+if !cvar.swiglal_debug
+  msg("skipping memory allocation");
+else
+  try
+    LALCheckMemoryLeaks();   # should NOT throw an exception
+    mem1 = new_LALDetector();
+    mem2 = new_LALStringVector();
+    mem3 = new_COMPLEX8Vector();
+    mem4 = XLALCreateREAL8Vector(3);
+    try
+      msg("*** below should be an error message from LALCheckMemoryLeaks() ***");
+      LALCheckMemoryLeaks();   # should throw an exception
+      msg("FAILED memory allocation #2");
+      exit(1);
+    catch
+      msg("*** above should be an error message from LALCheckMemoryLeaks() ***");
+      clear mem1 mem2 mem3;
+      XLALDestroyREAL8Vector(mem4);
+      try
+        LALCheckMemoryLeaks();   # should NOT throw an exception
+      catch
+        msg("FAILED memory allocation #3");
+        exit(1);
+      end_try_catch
+    end_try_catch
+  catch
+    msg("FAILED memory allocation #1");
+    exit(1);
+  end_try_catch
+endif
+
 ## passed all tests!
 msg("================");
 msg("PASSED all tests");
--- lal/swig/python/check-swiglal.py
+++ lal/swig/python/check-swiglal.py
@@ -6,6 +6,34 @@ import os
 def msg(str):
     print(os.path.basename(__file__) + ": " + str)
 
+# check memory allocation
+if not cvar.swiglal_debug:
+    msg("skipping memory allocation")
+else:
+    try:
+        LALCheckMemoryLeaks()   # should NOT throw an exception
+        mem1 = LALDetector()
+        mem2 = LALStringVector()
+        mem3 = COMPLEX8Vector()
+        mem4 = XLALCreateREAL8Vector(3)
+        try:
+            msg("*** below should be an error message from LALCheckMemoryLeaks() ***");
+            LALCheckMemoryLeaks()   # should throw an exception
+            msg("FAILED memory allocation #2")
+            exit(1)
+        except:
+            msg("*** above should be an error message from LALCheckMemoryLeaks() ***");
+            del mem1, mem2, mem3
+            XLALDestroyREAL8Vector(mem4)
+            try:
+                LALCheckMemoryLeaks()   # should NOT throw an exception
+            except:
+                msg("FAILED memory allocation #3")
+                exit(1)
+    except:
+        msg("FAILED memory allocation #1")
+        exit(1)
+
 # passed all tests!
 msg("================")
 msg("PASSED all tests")
