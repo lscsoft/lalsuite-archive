576a5a7eb7cb00a69568aa82b50f8c01c4aa40ce
Duncan Macleod <duncan.macleod@ligo.org>
2011-05-06 07:43:27 -0500

    Major changes to all coh_PTF post processing codes.

--- pylal/pylal/coh_PTF_pyutils.py
+++ pylal/pylal/coh_PTF_pyutils.py
@@ -1,155 +1,136 @@
-import os,sys
-import pylab
-from pylal import grbsummary
-from pylal import antenna
-import scipy.stats
+import os,sys,numpy
 
-def get_signal_vetoes(trigger,bankq=0,bankn=0,autoq=0,auton=0,chiq=0,chin=0,sigmaVals = None,fResp = None):
+from pylal import grbsummary,antenna,llwapp
+from glue import segmentsUtils
+from glue.ligolw import lsctables
+from glue.ligolw.utils import process as ligolw_process
+
+# define newsnr
+def new_snr( snr, chisq, W, Q, N ):
+
+  return snr / ( ( 1 + (chisq/W)**(Q/N))/2 ) ** (1/Q)
+
+def get_signal_vetoes( trigger, bankq=0, bankn=0, autoq=0, auton=0, chiq=0,\
+                       chin=0, fResp = None ):
+
+  """
+    Calculate and apply all signal based vetoes for the given trigger. This
+    will generate eight different SNRs for the different stages of this
+    process and return them as a dictionary.
+    
+    The signal based vetoes are applied in the following order:
+    1) Chi-squared
+    2) Coherent SNR < 6
+    3) Bank chi-squared
+    4) Auto veto (continuous chi-squared)
+    5) Null SNR ( ( CoincSNR^2 - CohSNR^2 )^(1/2) )
+    6) Single-detector SNR (from two most sensitive IFOs)
+    7) None
+    8) None
+  """
+
+  # set variables
   sbvs = {}
   q = bankq
   nhigh = bankn
   q2 = autoq
   nhigh2 = auton
 
+  # get new SNR (from chisq)
   if trigger.chisq == 0:
-    sbvs['BestNR1'] = 0
+    sbvs[1] = 0
   else:
     if trigger.chisq < 60:
-      sbvs['BestNR1'] = trigger.snr
+      sbvs[1] = trigger.snr
     else:
-      sbvs['BestNR1'] = trigger.snr/((1 + (trigger.chisq/60.)**(chiq/chin))/2.)**(1./chiq)
+      sbvs[1] = new_snr( trigger.snr, trigger.chisq, 60, chiq, chin )
 
-#  traceM = (27.5-6.5)/(30-6)
-#  traceC = 6.5 - traceM * 6
-#  if trigger.null_stat_degen > traceM * trigger.snr + traceC and trigger.snr < 30:
-#    sbvs['BestNR2'] = 0
-#  else:
-#    sbvs['BestNR2'] = sbvs['BestNR1']
-#  sbvs['BestNR2'] = sbvs['BestNR1'] * (trigger.snr/trigger.null_stat_degen)**2
-
+  # get SNR2
   if trigger.snr < 6.:
-    sbvs['BestNR2'] = 0
+    sbvs[2] = 0
   else:
-    sbvs['BestNR2'] = sbvs['BestNR1']
+    sbvs[2] = sbvs[1]
 
+  # get new bank SNR
   if trigger.bank_chisq < 40:
-    sbvs['BestNR3'] = sbvs['BestNR2']
+    sbvs[3] = sbvs[2]
   else:
-    bank_new_snr = trigger.snr/((1 + (trigger.bank_chisq/40.)**(q/nhigh))/2.)**(1./q)
+    bank_new_snr = new_snr( trigger.snr, trigger.bank_chisq, 40, q, nhigh )
     if bank_new_snr < 6.:
-      sbvs['BestNR3'] = 0
+      sbvs[3] = 0
     else:
-      sbvs['BestNR3'] = sbvs['BestNR2']
+      sbvs[3] = sbvs[2]
 
+  # get new auto SNR
   if trigger.cont_chisq < 40:
-    sbvs['BestNR4'] = sbvs['BestNR3']
+    sbvs[4] = sbvs[3]
   else:
-    auto_new_snr = trigger.snr/((1 + (trigger.cont_chisq/160.)**(q2/nhigh2))/2.)**(1./q2)
+    auto_new_snr = new_snr( trigger.snr, trigger.cont_chisq, 160, q2, nhigh2 )
     if auto_new_snr < 6.:
-      sbvs['BestNR4'] = 0
+      sbvs[4] = 0
     else:
-      sbvs['BestNR4'] = sbvs['BestNR3']
+      sbvs[4] = sbvs[3]
 
-  traceM = (21.5/24.)
-  traceC = 27.5 - 30.*(traceM)
+  # get null affected SNR
+  ifos = [ trigger.ifos[i*2:(i*2)+2]\
+           for i in range(int(len(trigger.ifos)/2)) ]
+  ifoAtt = { 'G1':'g', 'H1':'h1', 'H2':'h2', 'L1':'l', 'T1':'t', 'V1':'v' }
+  # null SNR = sqrt( coincSNR^2 - cohSNR^2 )
+  null_snr = ( sum([ trigger.__getattribute__('snr_%s' % ifoAtt[ifo])**2\
+                     for ifo in ifos ]) - trigger.snr**2 )**0.5
 
-#  if trigger.null_stat_degen > traceM * trigger.snr + traceC and trigger.snr < 30:
-#    sbvs['BestNR5'] = 0
-#  else:
-#    sbvs['BestNR5'] = sbvs['BestNR4'] 
-
-
-#  compsList = [trigger.snr_h1,trigger.snr_l,trigger.snr_v]
-#  compsList.sort()
-#  if pylab.sqrt(compsList[1]) < (secondM*trigger.snr + secondC):
-#    sbvs['BestNR6'] = 0
-#  else:
-#    sbvs['BestNR6'] = sbvs['BestNR5']
-
-#  sbvs['BestNR6'] = sbvs['BestNR5']
-
-#  if trigger.snr_h1 < 4 or trigger.snr_l < 4:
-#    sbvs['BestNR6'] = 0
-#  else:
-#    sbvs['BestNR6'] = sbvs['BestNR5']
-
-#  if pylab.sqrt(compsList[1]) < 4:
-#    sbvs['BestNR6'] = 0
-#  else:
-#    sbvs['BestNR6'] = sbvs['BestNR5']
-
-  if trigger.null_statistic > 3.5 and trigger.snr < 30:
-    sbvs['BestNR5'] = sbvs['BestNR4'] * 1/(trigger.null_statistic - 2.5)
+  if null_snr > 3.5 and trigger.snr < 30:
+    sbvs[5] = sbvs[4] * 1/(null_snr - 2.5)
   elif trigger.snr > 30:
     null_threshold = 3.5 + (trigger.snr -30)*50./700.
-    if trigger.null_statistic > null_threshold:
-      sbvs['BestNR5']=sbvs['BestNR4']*1/(trigger.null_statistic-(null_threshold-1))
+    if null_snr > null_threshold:
+      sbvs[5]=sbvs[4]*1/(null_snr-(null_threshold-1))
     else:
-      sbvs['BestNR5'] = sbvs['BestNR4']
+      sbvs[5] = sbvs[4]
   else:
-    sbvs['BestNR5'] = sbvs['BestNR4']
+    sbvs[5] = sbvs[4]
 
-  if trigger.snr_h1 < 4 or trigger.snr_v < 4:
-    sbvs['BestNR8'] = 0
-  else:
-    sbvs['BestNR8'] = sbvs['BestNR5']
-
-
-  if sbvs['BestNR5'] == 0:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(0.00135/2.,2,fResp['H1']*sigmaVals['H1min']*trigger.snr**2) > trigger.snr_h1**2:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.00135/2.,2,fResp['H1']*sigmaVals['H1max']*trigger.snr**2) < trigger.snr_h1**2:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(0.00135/2.,2,fResp['L1']*sigmaVals['L1min']*trigger.snr**2) > trigger.snr_l**2:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.00135/2.,2,fResp['L1']*sigmaVals['L1max']*trigger.snr**2) < trigger.snr_l**2:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(0.00135/2.,2,fResp['V1']*sigmaVals['V1min']*trigger.snr**2) > trigger.snr_v**2:
-    sbvs['BestNR6'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.00135/2.,2,fResp['V1']*sigmaVals['V1max']*trigger.snr**2) < trigger.snr_v**2:
-    sbvs['BestNR6'] = 0
-  else: 
-    sbvs['BestNR6'] = sbvs['BestNR5']
-
-  if sbvs['BestNR6'] == 0:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(0.0455/2.,2,fResp['H1']*sigmaVals['H1min']*trigger.snr**2) > trigger.snr_h1**2:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.0455/2.,2,fResp['H1']*sigmaVals['H1max']*trigger.snr**2) < trigger.snr_h1**2:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(0.0455/2.,2,fResp['L1']*sigmaVals['L1min']*trigger.snr**2) > trigger.snr_l**2:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.0455/2.,2,fResp['L1']*sigmaVals['L1max']*trigger.snr**2) < trigger.snr_l**2:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(0.0455/2.,2,fResp['V1']*sigmaVals['V1min']*trigger.snr**2) > trigger.snr_v**2:
-    sbvs['BestNR7'] = 0
-  elif scipy.stats.ncx2.ppf(1-0.0455/2.,2,fResp['V1']*sigmaVals['V1max']*trigger.snr**2) < trigger.snr_v**2:
-    sbvs['BestNR7'] = 0
-  else:
-    sbvs['BestNR7'] = sbvs['BestNR6']
+  # apply IFO sensitivity cut
+  ifoSens = []
+  # get ifo sensitivity measure
+  for ifo in ifos:
+    ifoSens.append( (ifo, trigger.__getattribute__('sigmasq_%s' %ifoAtt[ifo])\
+                              * fResp[ifo] ) )
+  # rank and test two most sensitive IFOs
+  ifoSens.sort( key=lambda (ifo,sens): sens, reverse=True )
+  sbvs[8] = sbvs[5]
+  for i in range(2):
+    ifo = ifoSens[i][0]
+    if trigger.__getattribute__('snr_%s' % ifoAtt[ifo]) < 4:
+      sbvs[8] = 0
+    
+  # set old BestNRs
+  sbvs[6] = sbvs[8]
+  sbvs[7] = sbvs[6]
 
   return sbvs
 
-def calculate_contours(bankq=0,bankn=0,autoq=0,auton=0,chiq=0,chin=0,nullt=0,nullo=0):
-  cont_vals = [5.5,6,6.5,7,8,9,10,11]
-  num_vals = len(cont_vals)
-  colors = ['y-','k-','y-','y-','y-','y-','y-','y-']
+def calculate_contours( bankq=0, bankn=0, autoq=0, auton=0, chiq=0, chin=0,\
+                        nullt=0, nullo=0 ):
 
-  snr_vals = pylab.arange(6,30,0.1)
-  snr_high_vals = pylab.arange(30,500,1)
-  snr_all = []
+  cont_vals = [5.5,6,6.5,7,8,9,10,11]
+  num_vals  = len(cont_vals)
+  colors    = ['y-','k-','y-','y-','y-','y-','y-','y-']
+
+  snr_vals      = numpy.arange(6,30,0.1)
+  snr_high_vals = numpy.arange(30,500,1)
+  snr_all       = []
   for snr in snr_vals:
     snr_all.append(snr)
   for snr in snr_high_vals:
     snr_all.append(snr)
 
-  snr_vals = pylab.asarray(snr_all)
+  snr_vals = numpy.asarray(snr_all)
 
   bank_conts = [[],[],[],[],[],[],[],[]]
   auto_conts = [[],[],[],[],[],[],[],[]]
-  chi_conts = [[],[],[],[],[],[],[],[]]
-  null_cont = []
+  chi_conts  = [[],[],[],[],[],[],[],[]]
+  null_cont  = []
 
   for snr in snr_vals:
     for i in range(num_vals):
@@ -181,7 +162,8 @@ def calculate_contours(bankq=0,bankn=0,autoq=0,auton=0,chiq=0,chin=0,nullt=0,nul
 
   return bank_conts,auto_conts,chi_conts,null_cont,snr_vals,colors
 
-def plot_contours(snr_vals,contours,colors):
+def plot_contours( axis, snr_vals, contours, colors ):
+
   for i in range(len(contours)):
     plot_vals_x = []
     plot_vals_y = []
@@ -189,57 +171,113 @@ def plot_contours(snr_vals,contours,colors):
       if contours[i][j] > 1E-15:
         plot_vals_x.append(snr_vals[j])
         plot_vals_y.append(contours[i][j])
-    pylab.plot(plot_vals_x,plot_vals_y,colors[i])
+    axis.plot(plot_vals_x,plot_vals_y,colors[i])
 
-def readSegFiles(segFileLocation):
+def readSegFiles(segdir):
+
   times = {}
-  for name,fileName in zip(["buffer","off","on"],["bufferSeg.txt","offSourceSeg.txt","onSourceSeg.txt"]):
-    file = open(os.path.join(segFileLocation,fileName),"r")
-    for line in file:
-      line = line.split('\t')
-      if line[0] == '0':
-        times[name+"_start"] = line[1]
-        times[name+"_end"] = line[2]
+  for name,fileName in\
+      zip( ["buffer",       "off",             "on"],\
+           ["bufferSeg.txt","offSourceSeg.txt","onSourceSeg.txt"] ):
+
+    segs = segmentsUtils.fromsegwizard(open(os.path.join(segdir,fileName), 'r'))
+    if len(segs)>1:
+      raise AttributeError, 'More than one segment, an error has occured.'
+    times[name] = segs[0]
   return times
        
 def makePaperPlots():
+
+  import pylab
+
   pylab.rcParams.update({
-#    "text.usetex": True,
+    "text.usetex": True,
     "text.verticalalignment": "center",
 #    "lines.markersize": 12,
 #    "lines.markeredgewidth": 2,
 #    "lines.linewidth": 2.5,
-    "font.size": 218,
-    "axes.titlesize": 24,
+    "figure.figsize": [8.0, 6.0],
+    "font.size": 20,
+    "axes.titlesize": 16,
     "axes.labelsize": 24,
     "xtick.labelsize": 18,
     "ytick.labelsize": 18,
     "legend.fontsize": 18,
+    "font.family": "serif",
+    "font.weight": "bold",
     })
 
 def get_ra_dec(grbFile):
+
+  """
+    DEPRECATED
+  """
+
   ext_trigs = grbsummary.load_external_triggers(grbFile)
   ra = ext_trigs[0].event_ra
   dec = ext_trigs[0].event_dec
   return ra,dec
 
-def read_sigma_vals(sigmaFile):
+def read_sigma_vals( sigmaFile ):
+
+  """
+    DEPRECATED
+  """
+
   sigmaVals = {}
   file = open(sigmaFile,'r')
-  ifos = ['H1','H2','L1','V1']
-  iter = 0
   for line in file:
+
     line = line.replace('\n','')
-    line = line.split(' ')
-    sigmaVals[ifos[iter] + 'min'] = float(line[0])
-    sigmaVals[ifos[iter] + 'max'] = float(line[1])
-    iter += 1
+    ifo,min,max = line.split(' ')
+    sigmaVals[ifo + 'min'] = float(min)
+    sigmaVals[ifo + 'max'] = float(max)
+
   return sigmaVals
     
+def get_det_response( ra, dec, triggerTime ):
 
-def get_det_response(ra,dec,triggerTime):
-  f_plus = {}
+  """
+    Return detector response for complete set of IFOs for given sky location
+    and time. Assumed inclination = 0, and polarization = 0. 
+  """
+
+  f_plus  = {}
   f_cross = {}
-  for det in ['H1','H2','L1','T1','V1','G1']:
-    f_plus[det], f_cross[det],crap1,crap2 = antenna.response( triggerTime, ra,dec, 0,0, 'degree', det )
+  inclination   = 0
+  polarization  = 0
+  for ifo in ['G1','H1','H2','L1','T1','V1']:
+    f_plus[ifo],f_cross[ifo],_,_ = antenna.response( triggerTime, ra, dec,\
+                                                     inclination, polarization,\
+                                                     'degree', ifo )
   return f_plus,f_cross
+
+def append_process_params( xmldoc, args, version, date ):
+
+  """
+    Construct and append process and process_params tables to ligolw.Document
+    object xmldoc, using the given sys.argv variable args and other parameters.
+  """
+
+  xmldoc.childNodes[-1].appendChild(lsctables.New(lsctables.ProcessTable))
+  xmldoc.childNodes[-1].appendChild(lsctables.New(lsctables.ProcessParamsTable))
+
+  # build and seed process params
+  progName = args[0]
+  process = llwapp.append_process( xmldoc, program=progName,\
+                                   version=version,\
+                                   cvs_repository = 'lscsoft',\
+                                   cvs_entry_time = date)
+  params = []
+  for i in range(len(args)):
+    p = args[i]
+    if not p.startswith('-'):
+      continue
+    v = ''
+    if i < len(sys.argv)-1:
+      v = sys.argv[i+1]
+    params.append( map( unicode, (p,'string',v) ) )
+
+  ligolw_process.append_process_params(xmldoc,process,params)
+
+  return xmldoc
