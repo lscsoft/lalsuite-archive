58ba35b700954f5f07fa2dbee74ef12872845105
Ian Harry <ian.harry@ligo.org>
2011-05-17 07:15:49 -0700

    PR3811: Move all code in lalapss/src/ring to measurelvl=1 for FFT plans

--- lalapps/src/ring/coh_PTF_spin_checker.c
+++ lalapps/src/ring/coh_PTF_spin_checker.c
@@ -308,7 +308,7 @@ int main( int argc, char **argv )
   fcTmpltParams->PTFe1 = XLALCreateVectorSequence( 3, numPoints );
   fcTmpltParams->PTFe2 = XLALCreateVectorSequence( 3, numPoints );
   fcTmpltParams->fwdPlan =
-        XLALCreateForwardREAL4FFTPlan( numPoints, 0 );
+        XLALCreateForwardREAL4FFTPlan( numPoints, 1 );
   fcTmpltParams->deltaT = 1.0/params->sampleRate;
   for( ifoNumber = 0; ifoNumber < LAL_NUM_IFO; ifoNumber++)
   {
@@ -320,7 +320,7 @@ int main( int argc, char **argv )
     }
   }
   /* Create an inverser FFT plan */
-  invPlan = XLALCreateReverseCOMPLEX8FFTPlan( numPoints, 0 );
+  invPlan = XLALCreateReverseCOMPLEX8FFTPlan( numPoints, 1 );
 
   /* Read in the tmpltbank xml file */
   numTmplts = InspiralTmpltBankFromLIGOLw( &PTFtemplate,bankFileName,
