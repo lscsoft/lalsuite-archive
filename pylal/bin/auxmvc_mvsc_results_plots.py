ac54b857a16c9be0f87721427da459cbbee84d5a
Kari Hodge <kari.hodge@ligo.org>
2011-05-30 14:24:11 -0700

    changed the axes and labeling

--- pylal/bin/auxmvc_mvsc_results_plots.py
+++ pylal/bin/auxmvc_mvsc_results_plots.py
@@ -11,6 +11,7 @@ from pylal import auxmvc_utils
 
 parser=OptionParser(usage="Generates summary plots for KW aux-triggers processed by StatPatternRecognition", version = "Kari Hodge")
 parser.add_option("","--histograms", action="store_true", default=False, help="use if you want to produce histograms for each dimension")
+parser.add_option("","--tag", help="filename will be ROC_tag.png")
 (opts,files)=parser.parse_args()
 
 print files
@@ -18,9 +19,6 @@ data = auxmvc_utils.ReadMVSCTriggers(files)
 print data.shape
 clean_data = data[numpy.nonzero(data['i']==0)[0],:]
 glitch_data = data[numpy.nonzero(data['i']==1)[0],:]
-print clean_data.shape, 'background (clean events)'
-print glitch_data.shape, 'signal (glitches)'
-
 
 all_ranks = numpy.concatenate((clean_data['Bagger'],glitch_data['Bagger']))
 all_ranks_sorted = numpy.sort(all_ranks)
@@ -40,13 +38,13 @@ FAP = []
 DP = []
 # classic ROC curve
 for i,rank in enumerate(clean_ranks_sorted):
-	# get the number of background triggers with rank greater than or equal to a given rank
+	# get the number of clean triggers with rank greater than or equal to a given rank
 	number_of_false_alarms = len(clean_ranks_sorted) -	numpy.searchsorted(clean_ranks_sorted,rank)
-	# get the number of signal (glitch) triggers with rank greater than or equal to a given rank
+	# get the number of glitches with rank greater than or equal to a given rank
 	number_of_true_alarms = len(glitch_ranks_sorted) -	numpy.searchsorted(glitch_ranks_sorted,rank)
 	# calculate the total deadime if this given rank is used as the threshold
 	FAP.append( number_of_false_alarms / float(len(clean_ranks_sorted)))
-	# calculate the fraction of correctly flagged signals (glitches)
+	# calculate the fraction of correctly flagged glitches
 	DP.append(number_of_true_alarms / float(len(glitch_ranks_sorted)))
 
 pylab.figure(1)
@@ -56,7 +54,9 @@ x = numpy.arange(min(DP), max(DP) + (max(DP) - min(DP))/1000.0, (max(DP) - min(D
 pylab.plot(x,x, linestyle="dashed", linewidth = 2.0)
 pylab.xlabel('False Alarm Probability')
 pylab.ylabel('Efficiency')
-pylab.savefig('classic_ROC.png')	
+pylab.xlim([0,1])
+pylab.ylim([0,1])
+pylab.savefig('ROC_'+opts.tag+'.png')	
 pylab.close()
 
 	
@@ -67,7 +67,7 @@ if opts.histograms:
 		#print clean_data[var]
 		print var
 		pylab.hist(clean_data[var],100)
-		pylab.savefig("hist_twosided"+var+"_background") 
+		pylab.savefig("hist_twosided"+var+"_cleantimes") 
 		pylab.figure(100+i)
 		pylab.hist(glitch_data[var],bins=100)
-		pylab.savefig("hist_twosided"+var+"_signal") 
+		pylab.savefig("hist_twosided"+var+"_glitches") 
