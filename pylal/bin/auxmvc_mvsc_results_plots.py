d9a77fc37e84ab6a3304faee3adbcbe487e43339
Kari Hodge <kari.hodge@ligo.org>
2011-05-30 11:58:08 -0700

    just changed the names of the variables, no code changes

--- pylal/bin/auxmvc_mvsc_results_plots.py
+++ pylal/bin/auxmvc_mvsc_results_plots.py
@@ -9,45 +9,45 @@ import pdb
 import numpy
 from pylal import auxmvc_utils 
 
-parser=OptionParser(usage="Generates summary plots for MVSC that processed KW aux-triggers", version = "Kari Hodge")
+parser=OptionParser(usage="Generates summary plots for KW aux-triggers processed by StatPatternRecognition", version = "Kari Hodge")
 parser.add_option("","--histograms", action="store_true", default=False, help="use if you want to produce histograms for each dimension")
 (opts,files)=parser.parse_args()
 
 print files
 data = auxmvc_utils.ReadMVSCTriggers(files)        
 print data.shape
-bk_data = data[numpy.nonzero(data['i']==0)[0],:]
-sg_data = data[numpy.nonzero(data['i']==1)[0],:]
-print bk_data.shape, 'background (clean events)'
-print sg_data.shape, 'signal (glitches)'
+clean_data = data[numpy.nonzero(data['i']==0)[0],:]
+glitch_data = data[numpy.nonzero(data['i']==1)[0],:]
+print clean_data.shape, 'background (clean events)'
+print glitch_data.shape, 'signal (glitches)'
 
 
-all_ranks = numpy.concatenate((bk_data['Bagger'],sg_data['Bagger']))
+all_ranks = numpy.concatenate((clean_data['Bagger'],glitch_data['Bagger']))
 all_ranks_sorted = numpy.sort(all_ranks)
-bk_ranks_sorted = numpy.sort(bk_data['Bagger'])
-sg_ranks_sorted = numpy.sort(sg_data['Bagger'])
+clean_ranks_sorted = numpy.sort(clean_data['Bagger'])
+glitch_ranks_sorted = numpy.sort(glitch_data['Bagger'])
 number_of_false_alarms=[]
 number_of_true_alarms=[]
 deadtime=[] #fraction
 efficiency=[] #fraction
 
 print len(all_ranks_sorted), "all ranks"
-print len(sg_ranks_sorted), "sg ranks"
-print len(bk_ranks_sorted), "bk ranks"
+print len(glitch_ranks_sorted), "glitch ranks"
+print len(clean_ranks_sorted), "clean ranks"
 
 
 FAP = []
 DP = []
 # classic ROC curve
-for i,rank in enumerate(bk_ranks_sorted):
+for i,rank in enumerate(clean_ranks_sorted):
 	# get the number of background triggers with rank greater than or equal to a given rank
-	number_of_false_alarms = len(bk_ranks_sorted) -	numpy.searchsorted(bk_ranks_sorted,rank)
+	number_of_false_alarms = len(clean_ranks_sorted) -	numpy.searchsorted(clean_ranks_sorted,rank)
 	# get the number of signal (glitch) triggers with rank greater than or equal to a given rank
-	number_of_true_alarms = len(sg_ranks_sorted) -	numpy.searchsorted(sg_ranks_sorted,rank)
+	number_of_true_alarms = len(glitch_ranks_sorted) -	numpy.searchsorted(glitch_ranks_sorted,rank)
 	# calculate the total deadime if this given rank is used as the threshold
-	FAP.append( number_of_false_alarms / float(len(bk_ranks_sorted)))
+	FAP.append( number_of_false_alarms / float(len(clean_ranks_sorted)))
 	# calculate the fraction of correctly flagged signals (glitches)
-	DP.append(number_of_true_alarms / float(len(sg_ranks_sorted)))
+	DP.append(number_of_true_alarms / float(len(glitch_ranks_sorted)))
 
 pylab.figure(1)
 pylab.plot(FAP,DP, linewidth = 2.0)
@@ -60,14 +60,14 @@ pylab.savefig('classic_ROC.png')
 pylab.close()
 
 	
-#print bk_data
+#print clean_data
 if opts.histograms:
 	for i,var in enumerate(variables):
 		pylab.figure(i)
-		#print bk_data[var]
+		#print clean_data[var]
 		print var
-		pylab.hist(bk_data[var],100)
+		pylab.hist(clean_data[var],100)
 		pylab.savefig("hist_twosided"+var+"_background") 
 		pylab.figure(100+i)
-		pylab.hist(sg_data[var],bins=100)
+		pylab.hist(glitch_data[var],bins=100)
 		pylab.savefig("hist_twosided"+var+"_signal") 
