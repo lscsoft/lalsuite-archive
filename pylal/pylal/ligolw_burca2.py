ligolw_burca2:  fix an off-by-one bug in interp2d
really interp2d is a little messed up, but this fix gets it working with
data sets that have genuinely integer co-ordinates.  without it was picking
the next bin over

pylal/pylal/ligolw_burca2.py
@@ -27,7 +27,7 @@
 import bisect
 import math
 import numpy
-from scipy.interpolate import interpolate
+from scipy import interpolate
 import sys
 
 
@@ -84,7 +84,7 @@ class interp1d(interpolate.interp1d):
 
 class interp2d(interpolate.interp2d):
 	# unlike the real interp2d, this version requires the x and y
-	# arrays to be just the co-ordinates of the columns and rows of a
+	# arrays to be the co-ordinates of the columns and rows of a
 	# regular mesh.
 	def __init__(self, x, y, z, fill_value = 0.0):
 		# Extrapolate x, y and z arrays by half a bin at each end.
@@ -105,17 +105,18 @@ class interp2d(interpolate.interp2d):
 		# the return value for co-ordinates outside the domain of
 		# the interpolator.
 
-		# FIXME:  scipy's 2D interpolator is busted, put this back
-		# when it's fixed.
+		# FIXME:  my use of scipy's 2D interpolator is busted, put
+		# this back when it's fixed.  we have problems with bin
+		# centres at +/- inf
 		#interpolate.interp2d.__init__(self, x, y, z, kind = "linear", bounds_error = False, fill_value = fill_value)
 		self.x = x
 		self.y = y
 		self.z = z
 
-	# FIXME:  scipy's 2D interpolator is busted.  remove this when it's
-	# fixed.
+	# FIXME:  my use of scipy's 2D interpolator is busted.  remove this
+	# when it's fixed.  we have problems with bin centres at +/- inf
 	def __call__(self, x, y):
-		return (self.z[bisect.bisect(self.x, x), bisect.bisect(self.y, y)],)
+		return (self.z[bisect.bisect_left(self.x, x), bisect.bisect_left(self.y, y)],)
 
 
 # starting from Bayes' theorem: