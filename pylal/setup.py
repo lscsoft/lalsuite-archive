850b4aa9dcf5bebc186a6e2823cf96844a2e5226
Ruslan Vaulin <vaulin@pcdev1.phys.uwm.edu>
2011-06-01 20:57:53 -0500

    Added bin/auxmvc_create_dag to the installation setup.

--- pylal/setup.py
+++ pylal/setup.py
@@ -629,7 +629,8 @@ setup(
         os.path.join("bin", "pylal_exttrig_dataquery"),
         os.path.join("bin", "pylal_exttrig_allquery"),
         os.path.join("bin", "auxmvc_mvsc_results_plots.py"),
-        os.path.join("bin", "generate_spr_files.py") 
+        os.path.join("bin", "generate_spr_files.py"),
+        os.path.join("bin", "auxmvc_create_dag") 
 	],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
