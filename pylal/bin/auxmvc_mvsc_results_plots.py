313bd136c3b0ca9cd0e8f7ad95946f8dd6187b80
Ruslan Vaulin <vaulin@pcdev1.phys.uwm.edu>
2011-06-09 11:52:50 -0500

    Added plots of rank vs dt

--- pylal/bin/auxmvc_mvsc_results_plots.py
+++ pylal/bin/auxmvc_mvsc_results_plots.py
@@ -14,7 +14,8 @@ import pickle
 
 parser=OptionParser(usage="Generates summary plots for KW aux-triggers processed by StatPatternRecognition", version = "Kari Hodge")
 parser.add_option("","--histograms", action="store_true", default=False, help="use if you want to produce histograms for each dimension")
-parser.add_option("","--plot-rank-vs-significance", action="store_true", default=False, help="Make scatter plots of ranke vs Aux KW significance")
+parser.add_option("","--plot-rank-vs-significance", action="store_true", default=False, help="Make scatter plots of rank vs Aux KW significance")
+parser.add_option("","--plot-rank-vs-dt", action="store_true", default=False, help="Make scatter plots of rank vs dt")
 parser.add_option("","--tag", help="filenames will be ROC_tag.png and efficiency_deadtime_tag.txt")
 parser.add_option("","--output-dir", help="directory where output files will be written to")
 (opts,files)=parser.parse_args()
@@ -28,11 +29,15 @@ data = auxmvc_utils.ReadMVSCTriggers(files)
 clean_data = data[numpy.nonzero(data['i']==0)[0],:]
 glitch_data = data[numpy.nonzero(data['i']==1)[0],:]
 
-all_ranks = numpy.concatenate((clean_data['Bagger'],glitch_data['Bagger']))
+
+rank_name = 'Bagger'
+
+
+all_ranks = numpy.concatenate((clean_data[rank_name],glitch_data[rank_name]))
 all_ranks_sorted = numpy.sort(all_ranks)
 
 
-FAP, DP = auxmvc_utils.ROC(clean_data['Bagger'], glitch_data['Bagger'])
+FAP, DP = auxmvc_utils.ROC(clean_data[rank_name], glitch_data[rank_name])
 
 
 # Plot ROC curve
@@ -66,16 +71,17 @@ for threshold in [.01,.05,.1]:
 	edfile.write("deadtime: "+str(FAP[tmpindex])+" efficiency: "+str(DP[len(FAP)-tmpindex-1])+"\n")
 
 
+# plot rank vs significance
 
 if opts.plot_rank_vs_significance:
 
-  # plot rank vs significance
   variables = data.dtype.names
   sig_variables = [s for s in variables if s.endswith("_sig")]
 
   #form arrays with significances for clean data sets
   clean_sig_rss = []
   clean_sig_max = []
+  clean_rank = []
   for i in range(len(clean_data)):
     sig_rss = 0
     sig_max = 0
@@ -85,11 +91,13 @@ if opts.plot_rank_vs_significance:
         sig_max = clean_data[var][i]
     clean_sig_rss.append(sig_rss**0.5)
     clean_sig_max.append(sig_max)
+    clean_rank.append(clean_data[rank_name][i])
     
 
   #form arrays with significances for glitch data sets
   glitch_sig_rss = []
   glitch_sig_max = []
+  glitch_rank = []
   for i in range(len(glitch_data)):
     sig_rss = 0
     sig_max = 0
@@ -99,12 +107,13 @@ if opts.plot_rank_vs_significance:
         sig_max = glitch_data[var][i]
     glitch_sig_rss.append(sig_rss**0.5)
     glitch_sig_max.append(sig_max)
+    glitch_rank.append(glitch_data[rank_name][i])
     
 
   pylab.figure(1)
-  pylab.semilogx(clean_sig_max,clean_data['Bagger'], "kx", label="clean")
+  pylab.loglog(clean_sig_max,clean_rank, "kx", label="clean")
   pylab.hold(True)
-  pylab.semilogx(glitch_sig_max,glitch_data['Bagger'], "r+", label="glitch")
+  pylab.loglog(glitch_sig_max,glitch_rank, "r+", label="glitch")
   pylab.hold(True)
   pylab.xlabel('Max Aux KW significance')
   pylab.ylabel('MVSC rank')
@@ -113,9 +122,9 @@ if opts.plot_rank_vs_significance:
 
 
   pylab.figure(1)
-  pylab.semilogx(clean_sig_rss,clean_data['Bagger'], "kx", label="clean")
+  pylab.loglog(clean_sig_rss,clean_rank, "kx", label="clean")
   pylab.hold(True)
-  pylab.semilogx(glitch_sig_rss,glitch_data['Bagger'], "r+", label="glitch")
+  pylab.loglog(glitch_sig_rss,glitch_rank, "r+", label="glitch")
   pylab.hold(True)
   pylab.xlabel('rss Aux KW significance')
   pylab.ylabel('MVSC rank')
@@ -123,6 +132,73 @@ if opts.plot_rank_vs_significance:
   pylab.close()
 
 
+# plot rank vs dt
+
+if opts.plot_rank_vs_dt:
+
+  variables = data.dtype.names
+  sig_variables = [s for s in variables if s.endswith("_sig")]
+  dt_variables = [s for s in variables if s.endswith("_dt")]
+  
+  #form arrays with dt for clean data sets
+  clean_dt_min = []
+  clean_dt_sig_max = []
+  clean_rank = []
+  for i in range(len(clean_data)):
+    dt_min = numpy.inf
+    sig_max = 0
+    for (dt_var,sig_var) in zip(dt_variables, sig_variables):
+      if (clean_data[sig_var][i] > 0) and (abs(clean_data[dt_var][i]) < dt_min):
+        dt_min = abs(clean_data[dt_var][i])
+      if clean_data[sig_var][i] > sig_max:
+        dt_sig_max = abs(clean_data[dt_var][i])
+      
+      
+    clean_dt_min.append(dt_min)
+    clean_dt_sig_max.append(dt_sig_max)
+    clean_rank.append(clean_data[rank_name][i])
+
+
+
+  #form arrays with dt for glitch data sets
+  glitch_dt_min = []
+  glitch_dt_sig_max = []
+  glitch_rank = []
+  for i in range(len(glitch_data)):
+    dt_min = numpy.inf
+    sig_max = 0
+    for (dt_var,sig_var) in zip(dt_variables, sig_variables):
+      if (glitch_data[sig_var][i] > 0) and (abs(glitch_data[dt_var][i]) < dt_min):
+        dt_min = abs(glitch_data[dt_var][i])
+      if glitch_data[sig_var][i] > sig_max:
+        dt_sig_max = abs(glitch_data[dt_var][i])
+      
+      
+    glitch_dt_min.append(dt_min)
+    glitch_dt_sig_max.append(dt_sig_max)
+    glitch_rank.append(glitch_data[rank_name][i])
+    
+    
+
+  pylab.figure(1)
+  pylab.loglog(clean_dt_min,clean_rank, "kx", label="clean")
+  pylab.hold(True)
+  pylab.loglog(glitch_dt_min,glitch_rank, "r+", label="glitch")
+  pylab.hold(True)
+  pylab.xlabel('min dt, seconds')
+  pylab.ylabel('MVSC rank')
+  pylab.savefig(opts.output_dir + "/"+'rank_vs_dt_min_'+opts.tag+'.png')	
+  pylab.close()
+
+  pylab.figure(1)
+  pylab.loglog(clean_dt_sig_max, clean_rank, "kx", label="clean")
+  pylab.hold(True)
+  pylab.loglog(glitch_dt_sig_max, glitch_rank, "r+", label="glitch")
+  pylab.hold(True)
+  pylab.xlabel('dt for trigger with highest sig, seconds')
+  pylab.ylabel('MVSC rank')
+  pylab.savefig(opts.output_dir + "/"+'rank_vs_dt_sig_max_'+opts.tag+'.png')	
+  pylab.close()
 
 
 if opts.histograms:
