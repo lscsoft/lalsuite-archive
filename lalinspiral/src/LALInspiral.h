6fd49cc71cc6884aba98bbd63912ff8bf3880825
Drew Keppel <drew.keppel@ligo.org>
2011-08-17 11:49:16 +0200

    XLALifying TaylorN

--- lalinspiral/src/LALInspiral.h
+++ lalinspiral/src/LALInspiral.h
@@ -1094,6 +1094,9 @@ void LALTaylorNWaveform (
      REAL4Vector      *signalvec,
      InspiralTemplate *params);
 
+int  XLALTaylorNWaveform (
+     REAL4Vector      *signalvec,
+     InspiralTemplate *params);
 
 
 
