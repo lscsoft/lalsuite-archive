d50d0c66aa1cf5c3bed6de1654ae1c0bf1e599bc
Sarah Caudill <sarah.caudill@ligo.org>
2011-10-24 16:19:17 -0700

    Reinstating inspiral version of mvsc_queries

--- pylal/pylal/mvsc_queries.py
+++ pylal/pylal/mvsc_queries.py
@@ -25,34 +25,44 @@ __author__ = "Kari Hodge <khodge@ligo.caltech.edu>"
 
 class CandidateEventQuery:
 	# this is the list of parameters that will describe each event in the training and testing sets:
-	parameters = "ds_sq delta_t df dQ a_snr b_snr coinc_snr null_stat eff_coh_snr"
+	parameters = "ethinca delta_t ab_dmchirp_rel ab_deta_rel a_snr b_snr a_chisq_red b_chisq_red a_effective_snr b_effective_snr a_rsq_veto_duration b_rsq_veto_duration a_cont_chisq_red b_cont_chisq_red coinc_inspiral_snr"
 	# these are the sqlite queries used to extract these parameters (the dimensions to be considered in the multivariate statitical classification algorithm)
 	select_dimensions="""
 		SELECT
-			coinc_ringdown.coinc_event_id,
+			coinc_inspiral.coinc_event_id,
 			snglA.*,
 			snglB.*,
 			insp_coinc_event.time_slide_id,
-			calc_delta_t(snglA.ifo, snglA.start_time, snglA.start_time_ns, snglB.ifo, snglB.start_time, snglB.start_time_ns, insp_coinc_event.time_slide_id),
-			abs(snglA.frequency - snglB.frequency),
-			abs(snglA.Quality - snglB.Quality),
+			calc_delta_t(snglA.ifo, snglA.end_time, snglA.end_time_ns, snglB.ifo, snglB.end_time, snglB.end_time_ns, insp_coinc_event.time_slide_id),
+			abs(2*(snglA.mchirp - snglB.mchirp)/(snglA.mchirp+snglB.mchirp)),
+			abs(2*(snglA.eta - snglB.eta)/(snglA.eta+snglB.eta)),
 			snglA.snr,
 			snglB.snr,
-			coinc_ringdown.snr,
-			coinc_ringdown.null_stat,
-			coinc_ringdown.eff_coh_snr"""
+			snglA.chisq/(2*snglA.chisq_dof-2),
+			snglB.chisq/(2*snglB.chisq_dof-2),
+			calc_effective_snr(snglA.snr, snglA.chisq, snglA.chisq_dof),
+			calc_effective_snr(snglB.snr, snglB.chisq, snglB.chisq_dof),
+			snglA.rsqveto_duration,
+			snglB.rsqveto_duration,
+			CASE snglA.cont_chisq_dof
+				WHEN 0.0 THEN 1.0
+				ELSE snglA.cont_chisq/snglA.cont_chisq_dof END,
+			CASE snglB.cont_chisq_dof
+				WHEN 0.0 THEN 1.0
+				ELSE snglB.cont_chisq/snglB.cont_chisq_dof END,
+			coinc_inspiral.snr"""
 	add_join_injections="""
 		,	CASE (SELECT value FROM process_params WHERE program == "inspinj" AND param == "--d-distr" AND process_params.process_id == sim_inspiral.process_id)
 				WHEN "log10" THEN 3*sim_inspiral.distance*sim_inspiral.distance*sim_inspiral.distance
 				WHEN "uniform" THEN 3*sim_inspiral.distance*sim_inspiral.distance
 				ELSE 1.0 END
 		FROM
-			coinc_ringdown
-			JOIN coinc_event_map AS mapA ON (mapA.coinc_event_id == coinc_ringdown.coinc_event_id)
-			JOIN coinc_event_map AS mapB ON (mapB.coinc_event_id == coinc_ringdown.coinc_event_id)
-			JOIN sngl_ringdown AS snglA ON (snglA.event_id == mapA.event_id)
-			JOIN sngl_ringdown AS snglB ON (snglB.event_id == mapB.event_id)
-			JOIN coinc_event_map AS mapC ON (mapC.event_id == coinc_ringdown.coinc_event_id)
+			coinc_inspiral
+			JOIN coinc_event_map AS mapA ON (mapA.coinc_event_id == coinc_inspiral.coinc_event_id)
+			JOIN coinc_event_map AS mapB ON (mapB.coinc_event_id == coinc_inspiral.coinc_event_id)
+			JOIN sngl_inspiral AS snglA ON (snglA.event_id == mapA.event_id)
+			JOIN sngl_inspiral AS snglB ON (snglB.event_id == mapB.event_id)
+			JOIN coinc_event_map AS mapC ON (mapC.event_id == coinc_inspiral.coinc_event_id)
 			JOIN coinc_event_map AS mapD ON (mapD.coinc_event_id == mapC.coinc_event_id)
 			JOIN sim_inspiral ON (sim_inspiral.simulation_id == mapD.event_id)
 			JOIN coinc_event AS sim_coinc_event ON (sim_coinc_event.coinc_event_id == mapD.coinc_event_id)
@@ -60,10 +70,10 @@ class CandidateEventQuery:
 			JOIN coinc_definer ON (coinc_definer.coinc_def_id == sim_coinc_event.coinc_def_id)
 			JOIN process_params ON (process_params.process_id == sim_inspiral.process_id)
 		WHERE
-			( coinc_definer.search == 'ring' OR coinc_definer.search =='ringdown' )
+			coinc_definer.search == 'inspiral'
 			AND coinc_definer.search_coinc_type == 2
-			AND mapA.table_name == 'sngl_ringdown'
-			AND mapB.table_name == 'sngl_ringdown'
+			AND mapA.table_name == 'sngl_inspiral'
+			AND mapB.table_name == 'sngl_inspiral'
 			AND mapC.table_name == 'coinc_event'
 			AND mapD.table_name == 'sim_inspiral'
 			AND snglA.ifo == ?
@@ -72,19 +82,19 @@ class CandidateEventQuery:
 	add_join_fulldata="""
 		, experiment_summary.datatype
 		FROM
-			coinc_ringdown
-			JOIN coinc_event_map AS mapA ON (mapA.coinc_event_id == coinc_ringdown.coinc_event_id)
-			JOIN coinc_event_map AS mapB ON (mapB.coinc_event_id == coinc_ringdown.coinc_event_id)
-			JOIN sngl_ringdown AS snglA ON (snglA.event_id == mapA.event_id)
-			JOIN sngl_ringdown AS snglB ON (snglB.event_id == mapB.event_id)
+			coinc_inspiral
+			JOIN coinc_event_map AS mapA ON (mapA.coinc_event_id == coinc_inspiral.coinc_event_id)
+			JOIN coinc_event_map AS mapB ON (mapB.coinc_event_id == coinc_inspiral.coinc_event_id)
+			JOIN sngl_inspiral AS snglA ON (snglA.event_id == mapA.event_id)
+			JOIN sngl_inspiral AS snglB ON (snglB.event_id == mapB.event_id)
 			JOIN coinc_event AS insp_coinc_event ON (mapA.coinc_event_id == insp_coinc_event.coinc_event_id)
 			JOIN coinc_definer ON (coinc_definer.coinc_def_id == insp_coinc_event.coinc_def_id)
-			JOIN experiment_map ON (experiment_map.coinc_event_id == coinc_ringdown.coinc_event_id)
+			JOIN experiment_map ON (experiment_map.coinc_event_id == coinc_inspiral.coinc_event_id)
 			JOIN experiment_summary ON (experiment_summary.experiment_summ_id == experiment_map.experiment_summ_id)
 		WHERE
-			coinc_definer.search == 'ring'
+			coinc_definer.search == 'inspiral'
 			AND coinc_definer.search_coinc_type == 0
-			AND mapA.table_name == 'sngl_ringdown'
-			AND mapB.table_name == 'sngl_ringdown'
+			AND mapA.table_name == 'sngl_inspiral'
+			AND mapB.table_name == 'sngl_inspiral'
 			AND snglA.ifo == ?
 			AND snglB.ifo == ?"""
