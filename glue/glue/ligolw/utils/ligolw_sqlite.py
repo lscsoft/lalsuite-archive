bc371c598c64ecc827d20a02b7d442d4674a4f1c
Thomas Dent <thomas.dent@ligo.org>
2011-12-28 18:47:53 +0100

    Revert "ligolw_sqlite:  fix ID remapping when merging sqlite databases"
    This reverts commit 080d02c92b6791524cb0c84872baf1d3538598ab.

--- glue/glue/ligolw/utils/ligolw_sqlite.py
+++ glue/glue/ligolw/utils/ligolw_sqlite.py
@@ -78,19 +78,11 @@ def setup(target, check_same_thread=True):
 #
 
 
-def update_ids(connection, xmldoc, verbose = False):
+def update_ids(connection, verbose = False):
 	"""
 	For internal use only.
 	"""
-	# NOTE:  it's critical that the xmldoc object be retrieved *before*
-	# the rows whose IDs need to be updated are inserted.  The xml
-	# retrieval resets the "last max row ID" values inside the table
-	# objects, so if retrieval of the xmldoc is deferred until after
-	# the rows are inserted, nothing will get updated.  therefore, the
-	# connection and xmldoc need to be passed separately to this
-	# function, even though it seems this function could reconstruct
-	# the xmldoc itself from the connection.
-	table_elems = xmldoc.getElementsByTagName(ligolw.Table.tagName)
+	table_elems = dbtables.get_xml(connection).getElementsByTagName(ligolw.Table.tagName)
 	for i, tbl in enumerate(table_elems):
 		if verbose:
 			print >>sys.stderr, "updating IDs: %d%%\r" % (100.0 * i / len(table_elems)),
@@ -109,26 +101,19 @@ def insert_from_url(connection, url, preserve_ids = False, verbose = False):
 	database the at the given connection.
 	"""
 	#
-	# retrieve an XML-ish representation of the database.  see the note
-	# in update_ids() about the order in which this must be done
-	#
-
-	xmldoc = dbtables.get_xml(connection)
-
-	#
 	# load document.  this process inserts the document's contents into
 	# the database.  the document is unlinked to delete database cursor
 	# objects it retains
 	#
 
-	utils.load_url(url, xmldoc = xmldoc, verbose = verbose).unlink()
+	utils.load_url(url, verbose = verbose).unlink()
 
 	#
 	# update references to row IDs
 	#
 
 	if not preserve_ids:
-		update_ids(connection, xmldoc, verbose)
+		update_ids(connection, verbose)
 
 
 def insert_from_xmldoc(connection, source_xmldoc, preserve_ids = False, verbose = False):
@@ -137,21 +122,13 @@ def insert_from_xmldoc(connection, source_xmldoc, preserve_ids = False, verbose 
 	the given connection.
 	"""
 	#
-	# retrieve an XML-ish representation of the database.  see the note
-	# in update_ids() about the order in which this must be done
-	#
-
-	xmldoc = dbtables.get_xml(connection)
-
-	#
 	# iterate over tables in the XML tree, reconstructing each inside
 	# the database
 	#
 
 	for tbl in source_xmldoc.getElementsByTagName(ligolw.Table.tagName):
 		#
-		# instantiate the correct table class, connected to the
-		# target database
+		# instantiate the correct table class
 		#
 
 		name = dbtables.table.StripTableName(tbl.getAttribute("Name"))
@@ -189,7 +166,7 @@ def insert_from_xmldoc(connection, source_xmldoc, preserve_ids = False, verbose 
 	#
 
 	if not preserve_ids:
-		update_ids(connection, xmldoc, verbose)
+		update_ids(connection, verbose)
 
 
 def insert_from_urls(connection, urls, preserve_ids = False, verbose = False):
