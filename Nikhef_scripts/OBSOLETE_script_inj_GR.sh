af85ff302d48f88aafad2a2d663caa6f68134c70
Salvatore Vitale <salvatore.vitale@ldas-grid.ligo.caltech.edu>
2012-01-06 07:00:31 -0800

    New folder containing the scripts

---
+++ Nikhef_scripts/OBSOLETE_script_inj_GR.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+seed=7000
+outname=injections_GR_$seed.xml
+
+lalapps_inspinj \
+--output $outname \
+--f-lower 20.0 \
+--gps-start-time 932170000 \
+--gps-end-time 932765900 \
+--seed $seed \
+--waveform IMRPhenomFBTestthreePointFivePN \
+--min-distance 3.00e+05 \
+--max-distance 1.25e+06 \
+--d-distr volume \
+--l-distr random \
+--i-distr uniform \
+--min-mass1 5.0 \
+--max-mass1 15.0 \
+--min-mass2 5.0 \
+--max-mass2 15.0 \
+--m-distr componentMass \
+--min-mtotal 10.0 \
+--max-mtotal 30.0 \
+--disable-spin \
+--time-step 1.0e+03 \
+--amp-order 0
+
+seed_line=`cat $outname | grep seed`
+temp=`echo ${seed_line%\"*}`
+temp2=`echo ${temp##*\"}`
+echo "The file was created with seed $temp2 "
+
