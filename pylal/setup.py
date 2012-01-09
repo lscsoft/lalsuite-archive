30911ab1603dc679d39d0739ea33f8d0361f8a51
Nickolas Fotopoulos <nickolas.fotopoulos@ligo.org>
2012-01-09 15:51:07 -0800

    Sync pylal versions everywhere they should be synced

--- pylal/setup.py
+++ pylal/setup.py
@@ -206,7 +206,7 @@ class pylal_sdist(sdist.sdist):
 
 setup(
 	name = "pylal",
-	version = "0.1.2",
+	version = "0.1.3",
 	author = "Kipp Cannon and Nickolas Fotopoulos",
 	author_email = "lal-discuss@gravity.phys.uwm.edu",
 	description = "Python LIGO Algorithm Library",
