7a04f4a79cc6630f8cb1c6eb5c67b97924775e7e
Stephen Privitera <stephen.privitera@ligo.org>
2012-02-16 12:26:11 -0800

    This commit goes with 54a2994b6e23a66

--- pylal/pylal/upper_limit_utils.py
+++ pylal/pylal/upper_limit_utils.py
@@ -271,35 +271,22 @@ def compute_volume_vs_mass(found, missed, mass_bins, bin_type, catalog=None, dbi
     return volArray, vol2Array, foundArray, missedArray, effvmass, errvmass
 
 
-def log_volume_derivative_fit(x, vols, xhat, mkplot=True, tag=""):
+def log_volume_derivative_fit(x, vols, xhat):
     '''
     Performs a linear least squares fit for each mass bin to find the 
     (logarithmic) derivative of the search volume vs x at the given xhat.
     '''
     if numpy.min(vols) == 0:
         print >> sys.stderr, "Warning: cannot fit log volume derivative as all volumes are zero!"
-        return 0
+        return (0,0)
 
     coeffs, resids, rank, svs, rcond = numpy.polyfit(x, numpy.log(vols), 1, full=True)
     if coeffs[0] < 0:
-        val = 0  #negative derivatives may arise from rounding error
+        coeffs[0] = 0  #negative derivatives may arise from rounding error
         print >> sys.stderr, "Warning: Derivative fit resulted in Lambda =", coeffs[0]
         print >> sys.stderr, "The value Lambda = 0 was substituted"
-    else:
-        val = coeffs[0]
 
-    if mkplot:
-        fars = numpy.linspace(min(x),max(x),100)
-        pyplot.plot(x,numpy.log(vols),'rx',label="data")
-        pyplot.plot(fars,coeffs[0]*fars+coeffs[1],label="fit")
-        pyplot.legend(loc="lower right")
-        pyplot.xlabel("FAN (per expt)")
-        pyplot.ylabel("log volume")
-        pyplot.title("lambda = %.2g at fan = %.2f"%(val,xhat))
-        pyplot.savefig(tag+"volume_derivative_fit")
-        pyplot.close()
-
-    return val
+    return coeffs
 
 
 def get_background_livetime(connection, verbose=False):
