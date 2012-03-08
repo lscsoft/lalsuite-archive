1fb2ee2022d2121f78cdc34026707da5b82a7630
Benjamin Farr <bfarr@u.northwestern.edu>
2012-03-08 08:26:14 -0600

    pylal: only print analytic param stats in Bayesian pp script

--- pylal/bin/cbcBayesPostProc.py
+++ pylal/bin/cbcBayesPostProc.py
@@ -199,7 +199,9 @@ def cbcBayesPostProc(
     analyticLikelihood = None
     if covarianceMatrices and meanVectors:
         analyticLikelihood = bppu.AnalyticLikelihood(covarianceMatrices, meanVectors)
-        oneDMenu = analyticLikelihood.names()
+
+        #Plot only analytic parameters
+        oneDMenu = analyticLikelihood.names
         twoDGreedyMenu = []
         for i in range(len(oneDMenu)):
             for j in range(i+1,len(oneDMenu)):
@@ -341,6 +343,9 @@ def cbcBayesPostProc(
     requested_params = set(pos.names).intersection(set(oneDMenu))
     pos.delete_NaN_entries(requested_params)
 
+    #Remove non-analytic parameters if analytic likelihood is given:
+    if analyticLikelihood:
+        [pos.pop(param) for param in pos.names if param not in analyticLikelihood.names]
 
     ##Print some summary stats for the user...##
     #Number of samples
