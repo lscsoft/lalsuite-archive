2547d624d9b501d5d9c7ea36357d19604a89ada0
Rory Smith <rory.smith@ligo.org>
2012-05-11 17:20:27 -0700

    minor update

--- lalinference/src/inference_interps_test/compiles.sh
+++ lalinference/src/inference_interps_test/compiles.sh
@@ -1,2 +1,2 @@
-gcc -g $(pkg-config --libs lal) cheby_interp.c -I./ -I/home/channa/gstopt/include --std=gnu99
+gcc -g $(pkg-config --libs lal) LALInferenceInterps_unit_test.c -I./ -I/home/rory.smith/gstlocal/include --std=gnu99
 #gcc -l $(pkg-config --libs gsl lal) cheby_interp.c -I./ -I/home/channa/gstopt/include --std=gnu99
--- lalinference/src/inference_interps_test/runme.sh
+++ lalinference/src/inference_interps_test/runme.sh
@@ -2,5 +2,5 @@
 # to run in gdb do this\
 # LD_PRELOAD=/home/channa/gstopt/lib/liblal.so.3 gdb
 # (gdb) run
-LD_PRELOAD=/home/channa/gstopt/lib/liblal.so.3 ./a.out
+LD_PRELOAD=/home/rory.smith/gstlocal/lib/liblal.so.3 ./a.out
 #./plot_svd ../bank/svd_bank.xml.gz ../bank/test_bank.xml.gz
