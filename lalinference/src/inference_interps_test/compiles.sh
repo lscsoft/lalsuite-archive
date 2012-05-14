be208efd7628f4d75cdd2bd281015bfcf756fc70
Rory Smith <rory.smith@ligo.org>
2012-05-14 09:35:22 -0500

    removed inference_interps_test dir

--- lalinference/src/inference_interps_test/compiles.sh
+++
@@ -1,2 +0,0 @@
-gcc -g $(pkg-config --libs lal) LALInferenceInterps_unit_test.c -I./ -I/home/rory.smith/gstlocal/include --std=gnu99
-#gcc -l $(pkg-config --libs gsl lal) cheby_interp.c -I./ -I/home/channa/gstopt/include --std=gnu99
