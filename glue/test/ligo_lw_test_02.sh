813109c6a1356545f52635825050e5c6869ccc2b
Kipp Cannon <kipp.cannon@ligo.org>
2012-06-05 23:05:05 -0700

    ligo_lw_test_02.sh:  don't print Pass/Fail message
    this is now taken care of by the Makefile that runs this

--- glue/test/ligo_lw_test_02.sh
+++ glue/test/ligo_lw_test_02.sh
@@ -1,7 +1,1 @@
-if ligolw_add --verbose inspiral_event_id_test_in?.xml* | diff -q -s - inspiral_event_id_test_out.xml ; then
-	echo "Pass"
-	true
-else
-	echo "Fail"
-	false
-fi
+ligolw_add --verbose inspiral_event_id_test_in?.xml* | diff -q -s - inspiral_event_id_test_out.xml
