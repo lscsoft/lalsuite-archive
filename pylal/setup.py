ef154638fc6d0e8a5ef5b994a3d675b61ab2e9d0
Jameson Rollins <jameson.rollins@ligo.org>
2012-06-27 19:08:47 -0700

    pylal/debian: add source/format file to conform to current packaging standards.
    This is required for the "3.0 (quilt)" source package format.
    
    The setup.py script is also modified to pull in the full debian
    directory with all of it's contents and subdirectories.

--- pylal/setup.py
+++ pylal/setup.py
@@ -185,7 +185,11 @@ class pylal_install(install.install):
 class pylal_sdist(sdist.sdist):
 	def run(self):
 		# customize tarball contents
-		self.distribution.data_files = ["debian/%s" % f for f in os.listdir("debian")] + ["pylal.spec"]
+		self.distribution.data_files = []
+		for root,dirs,files in os.walk("debian"):
+			for file in files:
+				self.distribution.data_files += [os.path.join(root,file)]
+		self.distribution.data_files += ["pylal.spec"]
 		self.distribution.scripts = []
 
 		# create the git_version module
