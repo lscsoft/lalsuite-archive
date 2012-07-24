665a8c81a46b52b2c188feb21299f5292ba4262a
Nickolas Fotopoulos <nickolas.fotopoulos@ligo.org>
2012-07-24 12:59:58 -0700

    Add -std=c99 to spawaveform build as well

--- pylal/setup.py
+++ pylal/setup.py
@@ -462,7 +462,8 @@ setup(
 			include_dirs = lal_pkg_config.incdirs + lalinspiral_pkg_config.incdirs + [numpy_get_include()],
 			libraries = lal_pkg_config.libs + lalinspiral_pkg_config.libs,
 			library_dirs = lal_pkg_config.libdirs + lalinspiral_pkg_config.libdirs,
-			runtime_library_dirs = lal_pkg_config.libdirs + lalinspiral_pkg_config.libdirs
+			runtime_library_dirs = lal_pkg_config.libdirs + lalinspiral_pkg_config.libdirs,
+			extra_compile_args = lal_pkg_config.extra_cflags
 		),
 		Extension(
 			"pylal._bayespputils",
