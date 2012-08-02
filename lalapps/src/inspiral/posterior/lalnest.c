d7c71b5c20b952e0a83fb01f3cf4f955e3829ddc
Salvatore Vitale <svitale@nikhef.nl>
2012-08-02 23:57:17 +0200

    Made the SnrName string bigger

--- lalapps/src/inspiral/posterior/lalnest.c
+++ lalapps/src/inspiral/posterior/lalnest.c
@@ -2909,7 +2909,7 @@ void InjectFD(LALStatus status, LALMCMCInput *inputMCMC, SimInspiralTable *inj_t
 
 void PrintSNRsToFile(REAL8* SNRs,SimInspiralTable *inj_table,LALMCMCInput *inputMCMC){
 /* open the SNR file */
-    char SnrName[300];
+    char SnrName[500];
     char ListOfIFOs[10];
     REAL8 NetSNR=0.0;
     sprintf(ListOfIFOs,"");
