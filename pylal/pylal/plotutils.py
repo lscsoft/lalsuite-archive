2c9049c1824e62cec58e48441fa4262bf462513c
Duncan Macleod <duncan.macleod@ligo.org>
2012-08-07 11:27:14 -0500

    pylal.plotutils: Minor fixes to log scale on colorbar, added month unit for time_axis

--- pylal/pylal/plotutils.py
+++ pylal/pylal/plotutils.py
@@ -335,8 +335,9 @@ def add_colorbar(ax, mappable=None, visible=True, log=False, clim=None,\
     # find mappable with lowest maximum
     if not mappable:
         if len(ax.collections+ax.images) == 0:
+            norm = log and pylab.matplotlib.colors.LogNorm() or none
             mappable = ax.scatter([1], [1], c=[clim[0]], vmin=clim[0],\
-                                   vmax=clim[1], visible=False)
+                                   vmax=clim[1], visible=False, norm=norm)
         else:
             minindex = numpy.asarray([c.get_array().min() for c in\
                                       ax.collections+ax.images]).argmin()
@@ -344,9 +345,9 @@ def add_colorbar(ax, mappable=None, visible=True, log=False, clim=None,\
 
     # make sure the mappable has at least one element
     if mappable.get_array() is None:
+        norm = log and pylab.matplotlib.colors.LogNorm() or none
         mappable = ax.scatter([1], [1], c=[clim[0]], vmin=clim[0],\
-                               vmax=clim[1], visible=False)
-        
+                               vmax=clim[1], visible=False, norm=norm)
     
     # generate colorbar
     colorbar = ax.figure.colorbar(mappable, cax=cax, **kwargs)
@@ -517,8 +518,10 @@ def time_axis_unit(duration):
         return 60,"minutes"
     elif (duration) >= 20000 and (duration) < 604800:
         return 3600,"hours"
-    else:
+    elif (duration) < 8640000:
         return 86400,"days"
+    else:
+        return 2592000,"months"
 
 def set_time_ticks(ax):
     """
@@ -899,8 +902,6 @@ class ImagePlot(BasicPlot):
     def finalize(self, colorbar=True, logcolor=False, minorticks=False,\
                  clim=None):
         from pylal import rate
-        if not self.image_sets:
-            raise ValueError("nothing to finalize")
 
         logx = False
         logy = False
