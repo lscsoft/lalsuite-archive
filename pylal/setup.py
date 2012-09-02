dea8ba86306e7656de4c5888db399664241a1cdd
Michael Coughlin <michael.coughlin@ligo.org>
2012-09-02 09:57:13 -0700

    Added pylal_seismon_run to setup script

--- pylal/setup.py
+++ pylal/setup.py
@@ -729,6 +729,7 @@ setup(
 		os.path.join("bin", "pylal_cbc_ulmc"),
 		os.path.join("bin","cbcBayesConvergence.py"),
 		os.path.join("bin", "pylal_noise_budget")
+                os.path.join("bin", "pylal_seismon_run")
 		],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
