d13243c757dd2f6c45e57166adc6d987fd652302
Duncan Macleod <duncan.macleod@ligo.org>
2012-09-12 18:56:34 +0200

    pylal/setup.py: fixed bug introduced by branch merge

--- pylal/setup.py
+++ pylal/setup.py
@@ -729,7 +729,7 @@ setup(
 		os.path.join("bin", "pylal_cbc_ulmc"),
 		os.path.join("bin","cbcBayesConvergence.py"),
 		os.path.join("bin", "pylal_noise_budget"),
-		os.path.join("bin", "pylal_seismon_run")
+		os.path.join("bin", "pylal_seismon_run"),
 		os.path.join("bin", "pylal_summary_page")
 		],
 	data_files = [ ("etc", [
