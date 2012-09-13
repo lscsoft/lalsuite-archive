af097511e675760b87483dd6c1a84879be1d1028
Walter Del Pozzo <walter.delpozzo@ligo.org>
2012-09-13 11:24:55 +0200

    first tf2test commit

--- lalsimulation/src/LALSimInspiralTestGRParams.h
+++ lalsimulation/src/LALSimInspiralTestGRParams.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Walter Del pozzo, Evan Ochsner
+/* Copyright (C) 2012 Walter Del Pozzo, Evan Ochsner
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
