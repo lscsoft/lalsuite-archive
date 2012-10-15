Fixing some int/int issues in the multi_inspiral part of lsctables and correcting the null SNR calculation (cherry picked from commit 0071584bb3e9f482f765db008e6b12e1ab9a545b)

glue/glue/ligolw/lsctables.py
@@ -1791,7 +1791,7 @@ class MultiInspiralTable(table.Table):
 
 	def get_coinc_snr(self):
 		return (numpy.asarray(self.get_sngl_snrs().values())**2)\
-			   .sum(axis=0)**(1/2)
+			   .sum(axis=0)**(1./2.)
 
 	def get_end(self):
 		return [row.get_end() for row in self]
@@ -1812,8 +1812,10 @@ class MultiInspiralTable(table.Table):
 		"""
 		Get the coherent Null SNR for each row in the table.
 		"""
+                # Factor of 0.01 is added to prevent code failing when nullsnr=0
 		return ((numpy.asarray(self.get_sngl_snrs().values())**2)\
-                             .sum() - self.get_column('snr')**2)**(1/2)
+                         .sum(axis=0) - self.get_column('snr')**2\
+                         + 0.01)**(1./2.)
 
 	def get_sigmasq(self, instrument):
 		"""
@@ -1936,7 +1938,7 @@ class MultiInspiral(object):
 		Get the coincident SNR for this row.
 		"""
 		return (numpy.asarray(self.get_sngl_snrs().values())**2)\
-		            .sum()**(1/2)
+		            .sum()**(1./2.)
 
         def get_new_snr(self,index=4.0, column='chisq'):
                 rchisq = getattr(self, column) /\
@@ -1953,7 +1955,7 @@ class MultiInspiral(object):
 		Get the coherent Null SNR for this row.
 		"""
 		return ((numpy.asarray(self.get_sngl_snrs().values())**2)\
-                             .sum() - self.snr**2)**(1/2)
+                             .sum() - self.snr**2)**(1./2.)
 
 	def get_sngl_snr(self, instrument):
 		"""