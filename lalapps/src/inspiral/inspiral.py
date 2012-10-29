fa1a0ef206793e7ad75d2d9ec4f0778490c29e17
Thomas Dent <thomas.dent@ligo.org>
2012-10-29 10:43:47 -0400

    pipedown, inspiral.py: sort out user tags consistently for upper limit jobs
    Also make consistent indentation
    (cherry picked from commit cf13132453658f93a75f38ced722d8c2a144fe15)

--- lalapps/src/inspiral/inspiral.py
+++ lalapps/src/inspiral/inspiral.py
@@ -3501,7 +3501,7 @@ class SearchVolumeNode(pipeline.SqliteNode):
   def set_output_cache(self, file):
     self.add_var_opt("output-cache", file)
 
-  def set_output_tag(self, tag):
+  def set_user_tag(self, tag):
     self.add_var_opt("user-tag", tag)
 
   def set_veto_segments_name(self, name):
@@ -3538,6 +3538,9 @@ class SearchUpperLimitNode(pipeline.SqliteNode):
 
   def add_input_cache(self, input_cache):
     self.add_var_arg(input_cache)
+
+  def set_user_tag(self, tag):
+    self.add_var_opt("user-tag", tag)
 
   def set_open_box(self):
     '''
