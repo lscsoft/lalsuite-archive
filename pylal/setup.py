ddc65c18df6db53ad62063ebaecbc56576b7a940
Duncan Macleod <duncan.macleod@ligo.org>
2012-11-06 11:17:55 -0600

    pylal_plot_triggers: added new generic trigger plotter (cherry picked from commit 276e89ba2d9c53f79c27d6379ae279b181c5355c)
    Conflicts:
    
    	pylal/setup.py

--- pylal/setup.py
+++ pylal/setup.py
@@ -718,7 +718,8 @@ setup(
 		os.path.join("bin", "pylal_cbc_ulmc"),
 		os.path.join("bin","cbcBayesConvergence.py"),
 		os.path.join("bin", "pylal_noise_budget"),
-		os.path.join("bin", "pylal_coherent_ihope")
+		os.path.join("bin", "pylal_coherent_ihope"),
+		os.path.join("bin", "pylal_plot_triggers")
 		],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
