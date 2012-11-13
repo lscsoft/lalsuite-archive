ce5ee2b952a39cb944d1dcc15492b653bd87b8ff
Duncan Macleod <duncan.macleod@ligo.org>
2012-11-13 08:28:12 -0600

    pylal/setup.py: added ligolw_miinjfind

--- pylal/setup.py
+++ pylal/setup.py
@@ -719,7 +719,8 @@ setup(
 		os.path.join("bin","cbcBayesConvergence.py"),
 		os.path.join("bin", "pylal_noise_budget"),
 		os.path.join("bin", "pylal_coherent_ihope"),
-		os.path.join("bin", "pylal_plot_triggers")
+		os.path.join("bin", "pylal_plot_triggers"),
+		os.path.join("bin", "ligolw_miinjfind")
 		],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
