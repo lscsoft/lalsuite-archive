40d097059da695780979ff844cef5fef5288fda9
John Veitch <john.veitch@ligo.org>
2012-11-23 07:45:22 -0500

    Changes to pipeline.py, lalinference_pipe, LALInference, and inspiral.c to add support for targetting Grid sites using Pegasus

--- pylal/bin/cbcBayesPostProc.py
+++ pylal/bin/cbcBayesPostProc.py
@@ -1228,6 +1228,7 @@ if __name__=='__main__':
     parser.add_option("-c","--covarianceMatrix",dest="covarianceMatrices",action="append",default=None,help="CSV file containing covariance (must give accompanying mean vector CSV. Can add more than one matrix.")
     parser.add_option("-m","--meanVectors",dest="meanVectors",action="append",default=None,help="Comma separated list of locations of the multivariate gaussian described by the correlation matrix.  First line must be list of params in the order used for the covariance matrix.  Provide one list per covariance matrix.")
     parser.add_option("--email",action="store",default=None,type="string",metavar="user@ligo.org",help="Send an e-mail to the given address with a link to the finished page.")
+    parser.add_option("--archive",action="store",default=None,type="string",metavar="results.tar.gz",help="Create the given tarball with all results")
     (opts,args)=parser.parse_args()
 
     datafiles=[]
@@ -1373,6 +1374,10 @@ if __name__=='__main__':
                         #header file for parameter names in posterior samples
                         header=opts.header
                     )
+
+    if opts.archive is not None:
+        import subprocess
+        subprocess.call(["tar","cvzf",opts.archive,opts.outpath])
 
     # Send an email, useful for keeping track of dozens of jobs!
     # Will only work if the local host runs a mail daemon
