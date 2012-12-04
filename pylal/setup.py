e8069420d98406771b67f66d72c569cf7c928d77
Duncan Macleod <duncan.macleod@ligo.org>
2012-12-04 07:56:07 -0600

    pylal_coh_PTF_cluster: reworking of old coh_PTF_trig_cluster   * same clustering algorithm   * accepts input from file lists and caches   * preserve SimInspiral information when clustering injection events (cherry picked from commit 8e251064c5d594febb9813e982320fbc939a5e24)
    Conflicts:
    	pylal/setup.py

--- pylal/setup.py
+++ pylal/setup.py
@@ -732,6 +732,7 @@ setup(
 		os.path.join("bin", "pylal_summary_page"),
 		os.path.join("bin", "pylal_plot_triggers"),
 		os.path.join("bin", "ligolw_miinjfind"),
+		os.path.join("bin", "pylal_coh_PTF_cluster"),
 		],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
