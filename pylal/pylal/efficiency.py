9f520ce6b47cb40a72b5c708558436fe3829f97f
Matthew West <matthew.west@ligo.org>
2012-12-07 16:24:51 -0500

    Expand and reorganize ligolw_cbc_plotvt and efficiency.py
    pylal/bin/ligolw_cbc_plotvt
    -- expand the list of command line options
    -- must specific a coinc-time type and DQ veto category
    -- distance bins max & min are defined by the injection sets themselves
    -- list of far thresholds and the zerolag coinc time are now in MAIN
    -- made the calculation of efficiency vs distance arrays a separate
       function from calculating VxT
    -- make more plots and the labeling is no longer specific to my
       pipeline comparison work
    
    pylal/pylal/efficiency.py
    -- rewrite chirp_distance function to not be BNS specific
    -- new function "inj_dist_range" that gives an array of the distance bin
       edges
    -- new function "detection_efficiency" does the efficiency calculation
       separate from determining the 4-volume
    -- cleaned up functions "successful_injections" & "found_injections"

--- pylal/pylal/efficiency.py
+++ pylal/pylal/efficiency.py
@@ -50,9 +50,17 @@ from pylal import ligolw_compute_durations as compute_dur
 #
 
 
-def chirp_distance(eff_dist_h, eff_dist_l, mchirp):
-    mchirp_dns = (1.4+1.4)*pow(0.25,3.0/5.0)
-    dec_chirp_dist = min(eff_dist_h, eff_dist_l)*pow(mchirp_dns/mchirp, 5.0/6.0)
+def chirp_distance(distance, mchirp, source_ref):
+    if source_ref == "NSNS":
+        mchirp_ref = (1.4+1.4) * (1./4)**(3.0/5.0)
+    elif source_ref == "NSBH":
+        mchirp_ref = (1.4+10.0) * (14.0/11.4**2)**(3.0/5.0)
+    elif source_ref == "BHBH":
+        mchirp_ref = (10.0+10.0) * (1./4)**(3.0/5.0)
+    chirp_dist = distance * (mchirp_ref/mchirp)**(5.0/6.0)
+
+    return chirp_dist
+
 
 def end_time_with_ns(end_time, end_time_ns):
     time = end_time + 1e-9*end_time_ns
@@ -82,7 +90,62 @@ def get_livetime(connection, veto_cat, on_ifos, datatype):
 # =============================================================================
 #
 
-def successful_injections(connection, tag, verbose = False):
+def inj_dist_range(
+    connection,
+    tag,
+    dist_type = "distance",
+    dist_scale = "linear",
+    source_ref = None,
+    num_bins = 10):
+
+    sql_params_dict = {}
+    # SQL query to get a list of the distances for desired injections
+    if dist_type == "chirp_dist":
+        connection.create_function('chirp_distance', 3, chirp_distance)
+        sqlquery += """
+        SELECT DISTINCT
+            chirp_distance(distance, mchirp, :source_ref)
+        FROM sim_inspiral """
+        sql_params_dict['source_ref'] = source_ref
+    elif dist_type == "distance":
+        sqlquery += """
+        SELECT DISTINCT
+            distance
+        FROM sim_inspiral """
+
+    if tag != 'ALL_INJ':
+        # if a specific injection set is wanted
+        sqlquery += """
+        JOIN process_params ON (
+            process_params.process_id == sim_inspiral.process_id)
+        WHERE process_params.value = :usertag) """
+        sql_params_dict["usertag"] = tag
+
+    distances = numpy.array([ dist[0]
+        for dist in connection.execute(sqlquery, sql_params_dict) ])
+    min_dist = numpy.min(distances)
+    max_dist = numpy.max(distances)
+
+    if dist_scale == "linear":
+       d = numpy.linspace(start=min_dist, stop=max_dist, num=num_bins)
+    elif dist_scale == "log":
+       d = numpy.logspace(
+           start=numpy.log10(min_dist),
+           stop=numpy.log10(max_dist),
+           num=num_bins)
+
+    return d
+
+
+def successful_injections(
+    connection,
+    tag,
+    on_ifos,
+    veto_cat,
+    distance_type = "distance",
+    source_ref = None,
+    verbose = False):
+
     """
     My attempt to get a list of the simulations that actually made
     it into some level of coincident time
@@ -94,113 +157,119 @@ def successful_injections(connection, tag, verbose = False):
     # Get the veto segments as dictionaries, keyed by veto category
     veto_segments = compute_dur.get_veto_segments(xmldoc, verbose)
 
-    # sqlite query to get a list of the simulations for a given tag
-    sqlquery = ''.join(["""
-    SELECT
-        simulation_id,
-        end_time_with_ns(geocent_end_time, geocent_end_time_ns),
-        distance
-    FROM sim_inspiral
+    # ------------------------ Get List of Injections ------------------------ #
+    sql_params_dict = {}
+    sqlquery = """
+        SELECT
+            simulation_id,
+            end_time_with_ns(geocent_end_time, geocent_end_time_ns),"""
+    # add the desired distance measure to the SQL query
+    if distance_type == "chirp_dist":
+        connection.create_function('chirp_distance', 3, chirp_distance)
+        sql_params_dict['source_ref'] = source_ref
+        sqlquery += """
+            chirp_distance(distance, mchirp, :source_ref)
+        FROM sim_inspiral """
+    elif distance_type == "distance":
+        sqlquery += """
+            distance
+        FROM sim_inspiral """
+
+    if tag != 'ALL_INJ':
+        # if a specific injection set is wanted
+        sqlquery += """
         JOIN process_params ON (
             process_params.process_id == sim_inspiral.process_id)
-    WHERE process_params.value = \"""", tag, """\" """])
-
-    if tag == 'ALL_INJ':
-        sqlquery = '\n'.join( sqlquery.split('\n')[0:6] )
-        # redefine tag for getting single-ifo segments
+        WHERE process_params.value = :usertag) """
+        sql_params_dict["usertag"] = tag
+    else:
+        # for all injections
         tag = 'FULL_DATA'
 
-    all_inj = connection.execute( sqlquery ).fetchall()
-
     # Get segments that define which time was filtered
-    livetime_program = "inspiral"
     ifo_segments = compute_dur.get_single_ifo_segments(
         connection,
-        program_name = livetime_program,
+        program_name = "inspiral",
         usertag = tag)
 
-    zero_lag_dict = {}
-    for ifo in ifo_segments:
-        zero_lag_dict[ifo] = 0.0
+    zero_lag_dict = dict([(ifo, 0.0) for ifo in ifo_segments])
 
-    successful_inj = {}
-    post_veto_ifosegs = segments.segmentlistdict()
+    successful_inj = []
+    # determine coincident segments for that veto category 
+    coinc_segs = compute_dur.get_coinc_segments(
+        ifo_segments - veto_seg_dict,
+        zero_lag_dict)
+
     # Apply vetoes to single-ifo filter segments
-    for category, veto_seg_dict in veto_segments.items():
-        successful_inj[category] = {}
-
-        # determine coincident segments for that veto category 
-        coinc_segs = compute_dur.get_coinc_segments(
-            ifo_segments - veto_seg_dict,
-            zero_lag_dict)
-
-        for on_ifos in coinc_segs:
-            successful_inj[category][on_ifos] = []
-            for idx, injection in enumerate(all_inj):
-                inj_segment = segments.segment(injection[1], injection[1])
-                if coinc_segs[on_ifos].intersects_segment( inj_segment ):
-                    successful_inj[category][on_ifos].append( injection )
+    for injection in connection.execute(sqlquery, sql_params_dict):
+        inj_segment = segments.segment(injection[1], injection[1])
+        if coinc_segs[on_ifos].intersects_segment( inj_segment ):
+            successful_inj.append( injection )
 
     return successful_inj
 
-def found_injections(connection, tag, verbose):
+
+def found_injections(
+    connection,
+    tag,
+    on_ifos,
+    veto_cat,
+    verbose = False):
+
     connection.create_function('end_time_with_ns', 2, end_time_with_ns)
-    xmldoc = dbtables.get_xml(connection)
-    veto_def_names = set( table.get_table(xmldoc, lsctables.SegmentDefTable.tableName).getColumnByName('name') )
 
-    ifos = [ifo[0] for ifo in connection.execute('SELECT DISTINCT ifos FROM process WHERE program = "inspiral"')]
+    ifos_str = ','.join(on_ifos)
 
-    found_inj = {}
-    inj_fars = {}
-    for category in veto_def_names:
-        found_inj[category] = {}
-        inj_fars[category] = {}
-        for on_ifos in compute_dur.get_allifo_combos(ifos, 2)[0]:
-            sql_params_dict = {}
-            sqlquery = """
-            SELECT DISTINCT
-                sim_inspiral.simulation_id,
-                end_time_with_ns(geocent_end_time, geocent_end_time_ns),
-                distance,
-                false_alarm_rate
-            FROM
-                coinc_event_map AS coincs
-                JOIN coinc_event_map AS sims, coinc_inspiral, coinc_event, sim_inspiral ON (
-                    coincs.coinc_event_id == sims.coinc_event_id
-                    AND coinc_event.coinc_event_id == coincs.event_id
-                    AND coinc_inspiral.coinc_event_id == coincs.event_id
-                    AND sim_inspiral.simulation_id == sims.event_id)
-                JOIN process_params ON (
-                    process_params.process_id == sim_inspiral.process_id)
-            WHERE
-                coincs.table_name = "coinc_event"
-                AND sims.table_name = "sim_inspiral" """
+    sql_params_dict = {}
+    sqlquery = """
+    SELECT DISTINCT
+        sim_inspiral.simulation_id,
+        end_time_with_ns(geocent_end_time, geocent_end_time_ns),
+        false_alarm_rate,"""
+    # add the desired distance measure to the SQL query
+    if distance_type == "chirp_dist":
+        connection.create_function('chirp_distance', 3, chirp_distance)
+        sqlquery += "chirp_distance(distance, mchirp, :source_ref)"
+        sql_params_dict['source_ref'] = source_ref
+    elif distance_type == "distance":
+        sqlquery += "distance"
 
-            if tag != 'ALL_INJ':
-                sqlquery += """
-                AND coinc_event.instruments = :ifos
-                AND process_params.value = :usertag
-                """
-                sql_params_dict["ifos"] = on_ifos
-                sql_params_dict["tag"] = tag
+    sqlquery += """
+    FROM
+        coinc_event_map AS coincs
+        JOIN coinc_event_map AS sims, coinc_inspiral, coinc_event, sim_inspiral ON (
+            coincs.coinc_event_id == sims.coinc_event_id
+            AND coinc_event.coinc_event_id == coincs.event_id
+            AND coinc_inspiral.coinc_event_id == coincs.event_id
+            AND sim_inspiral.simulation_id == sims.event_id)
+        JOIN process_params ON (
+            process_params.process_id == sim_inspiral.process_id)
+    WHERE
+        coincs.table_name = "coinc_event"
+        AND sims.table_name = "sim_inspiral" """
 
-            injections = connection.execute(sqlquery, sql_params_dict).fetchall()
-            injections.sort( key=itemgetter(3), reverse=True)
+    if tag != 'ALL_INJ':
+        sqlquery += """
+        AND coinc_event.instruments = :ifos
+        AND process_params.value = :usertag
+        """
+        sql_params_dict["ifos"] = ifos_str
+        sql_params_dict["tag"] = tag
 
-            found_inj[category][on_ifos] = [inj[0:3] for inj in injections]
-            inj_fars[category][on_ifos] = [inj[3] for inj in injections]
+    injections = connection.execute(sqlquery, sql_params_dict).fetchall()
+    injections.sort( key=itemgetter(3), reverse=True)
+
+    found_inj = [inj[0:3] for inj in injections]
+    inj_fars = [inj[3] for inj in injections]
 
     return found_inj, inj_fars
 
 
-def get_four_volume(
+def detection_efficiency(
     successful_inj,
     found_inj,
     found_fars,
-    r,
-    connection,
-    on_ifos,
-    veto_cat):
+    far_list):
 
     # catching any edge cases were the injection end_time is nearly on a second boundary
     successful_inj = set(successful_inj) & set(found_inj)
@@ -209,15 +278,8 @@ def get_four_volume(
     N_success, junk = numpy.histogram(successful_dist, bins = r)
 
     significant_dist = [inj[2] for inj in found_inj]
-    # Determine the minimum false_alarm_rate one can estimate from slides
-    slide_time = get_livetime(connection, veto_cat, on_ifos, 'slide')
-    minFAR = (3600.0*24.0*365.25) / slide_time
-    far_list = minFAR * numpy.arange(1000,-1,-1)
-    # Calculate the foreground search time in years
-    zerolag_time = get_livetime(connection, veto_cat, on_ifos, 'all_data')
-    T = zerolag_time / (3600.0*24.0*365.25)
+
     eff = {}
-    VT = {}
     for threshold in far_list:
         for idx, far in enumerate(found_fars):
             if far <= threshold:
@@ -226,10 +288,19 @@ def get_four_volume(
         # Histogram found injections with FAR < threshold
         N_significant, junk = numpy.histogram(significant_dist[new_start:], bins = r)
         eff[threshold] = numpy.float_(N_significant)/N_success
-        VT[threshold] = 0.0
-        for idx, e in enumerate(eff[threshold]):
-            if not math.isnan(e) and e > 0.0:
-                VT[threshold] += 4.0/3.0*numpy.pi*e*T*(pow(r[idx+1]/2.0,3) - pow(r[idx]/2.0,3))
 
-    return eff, VT
+    return eff
+
+def get_four_volume(eff, r, T_z):
+    # calculate 3 volume in each shell
+    V = 4./3 * numpy.pi * (r[:-1]**3. - r[1:]**3.)
+
+    VT = {}
+    for threshold in eff:
+        VT[threshold] = eff[threshold] * V * T_z
+        for index, vt in VT[threshold]:
+            if numpy.isnan(vt):
+                VT[threshold][index] = 0.0
+
+    return VT
 
