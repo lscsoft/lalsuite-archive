e9a8925ca6b59d346fc5a4b038f194cafbefa407
Prayush Kumar <prayush.kumar@ligo.org>
2013-01-28 22:29:53 -0500

    Changed authorship information.

--- lalsimulation/src/LALSimIMRPhenomC.c
+++ lalsimulation/src/LALSimIMRPhenomC.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Prayush Kumar
+ * Copyright (C) 2012 Prayush Kumar, Frank Ohme
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
