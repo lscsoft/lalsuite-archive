ad8c4414c194c6c0ee999d12afcc6f0feffc9f39
Ruslan Vaulin <ruslan.vaulin@ligo.org>
2013-01-30 11:19:58 -0600

    Fixed syntax errors.

--- pylal/pylal/auxmvc.py
+++ pylal/pylal/auxmvc.py
@@ -3,7 +3,7 @@
 from glue import pipeline
 
 def construct_command(node):
-  command_string = node.job().get_executable() + " " + node.get_cmd_line()))
+  command_string = node.job().get_executable() + " " + node.get_cmd_line()
   return command_string.split(" ")
   
   
@@ -86,14 +86,14 @@ class auxmvc_analysis_job(pipeline.AnalysisJob, pipeline.CondorDAGJob):
     """
     self.__exec_name = exec_name
     self.__extension = extension
-	self.tag_base = tag_base
+    self.tag_base = tag_base
     universe = cp.get('condor','universe')
     executable = cp.get('condor',exec_name)
     pipeline.CondorDAGJob.__init__(self,universe,executable)
     pipeline.AnalysisJob.__init__(self,cp,dax)
     self.add_condor_cmd('copy_to_spool','False')
-	self.add_condor_cmd('getenv','True')
-	self.add_condor_cmd('environment',"KMP_LIBRARY=serial;MKL_SERIAL=yes")
+    self.add_condor_cmd('getenv','True')
+    self.add_condor_cmd('environment',"KMP_LIBRARY=serial;MKL_SERIAL=yes")
     self.__use_gpus = cp.has_option('condor', 'use-gpus')
 
     for sec in sections:
@@ -112,7 +112,7 @@ class auxmvc_analysis_job(pipeline.AnalysisJob, pipeline.CondorDAGJob):
     """
     self.__exec_name = exec_name
 	
-	 def set_exec_name(self,exec_name):
+  def set_exec_name(self,exec_name):
     """
     Set the exec_name name
     """
@@ -163,18 +163,18 @@ class build_auxmvc_vectors_node(pipeline.CondorDAGNode):
   Dag node for building axumvc feature vector.
   """
   def __init__(self, job, triggerfiles, gps_start_time, gps_end_time, p_node=[]):
-	pipeline.CondorDAGNode.__init__(self,job)
-	self.job.set_stdout_file('logs/' + job.tag_base + '-' str(gps_start_time) + '-'+ str(gps_end_time - gps_start_time) +'.out')
-    self.job.set_stderr_file('logs/' + job.tag_base + '-' str(gps_start_time) + '-'+ str(gps_end_time - gps_start_time) +'.err')
-	self.add_input_file(triggerfiles)
-	trigger_type = self.job.get_opt('trigger-type')
-	ifo = self.job.get_opt('ifo')
-	user_tag = self.job.get_opt('user-tag')
-	output_file_name= trigger_type + ifo + "-" + "evaluation_" + user_tag  + "-" + gps_start_time + "-" + str(gps_end_time - gps_start_time) + ".pat"
-	self.add_output_file(output_file_name)
-	self.add_var_opt('trigger-files', triggerfiles)
-	for p in p_node:
-		self.add_parent(p)
+    pipeline.CondorDAGNode.__init__(self,job)
+    self.job.set_stdout_file('logs/' + job.tag_base + '-' + str(gps_start_time) + '-' + str(gps_end_time - gps_start_time) +'.out')
+    self.job.set_stderr_file('logs/' + job.tag_base + '-' +  str(gps_start_time) + '-' + str(gps_end_time - gps_start_time) +'.err')
+    self.add_input_file(triggerfiles)
+    trigger_type = self.job.get_opt('trigger-type')
+    ifo = self.job.get_opt('ifo')
+    user_tag = self.job.get_opt('user-tag')
+    output_file_name= trigger_type + ifo + "-" + "evaluation_" + user_tag  + "-" + gps_start_time + "-" + str(gps_end_time - gps_start_time) + ".pat"
+    self.add_output_file(output_file_name)
+    self.add_var_opt('trigger-files', triggerfiles)
+    for p in p_node:
+      self.add_parent(p)
   
 
 class train_forest_job(pipeline.CondorDAGJob):
@@ -182,12 +182,12 @@ class train_forest_job(pipeline.CondorDAGJob):
   Training job for random forets (MVSC). 
   """
   def __init__(self, cp):
-	"""
-	"""
-	sections = ['train-forest']
-	exec_name = 'SprBaggerDecisionTreeApp'
-	tag_base = 'train_forest'
-	auxmvc_analysis_job.__init__(self,cp,sections,exec_name,tag_base)	
+    """
+    """
+    sections = ['train-forest']
+    exec_name = 'SprBaggerDecisionTreeApp'
+    tag_base = 'train_forest'
+    auxmvc_analysis_job.__init__(self,cp,sections,exec_name,tag_base)	
 		
 
 class train_forest_node(pipeline.CondorDAGNode):
@@ -195,56 +195,56 @@ class train_forest_node(pipeline.CondorDAGNode):
   Dag node for training the random forest (MVSC).
   """
   def __init__(self, job, trainingdatafile, p_node=[]):
-	pipeline.CondorDAGNode.__init__(self,job)
-	self.job.set_stdout_file('logs/' + trainingdatafile.replace('.pat', '.out'))
+    pipeline.CondorDAGNode.__init__(self,job)
+    self.job.set_stdout_file('logs/' + trainingdatafile.replace('.pat', '.out'))
     self.job.set_stderr_file('logs/' + trainingdatafile.replace('.pat', '.err'))
-	self.add_input_file(trainingdatafile)
-	self.trainingdatafile = self.get_input_files()[0]
-	self.trainedforest = self.trainingdatafile.replace('.pat','.spr')
-	#self.trainedout = self.trainingdatafile.replace('.pat','.out')
-	self.add_file_arg(" %s %s" % (self.trainedforest, self.trainingdatafile))
-	self.add_output_file(self.trainedforest)
-	for p in p_node:
-		self.add_parent(p)
+    self.add_input_file(trainingdatafile)
+    self.trainingdatafile = self.get_input_files()[0]
+    self.trainedforest = self.trainingdatafile.replace('.pat','.spr')
+    #self.trainedout = self.trainingdatafile.replace('.pat','.out')
+    self.add_file_arg(" %s %s" % (self.trainedforest, self.trainingdatafile))
+    self.add_output_file(self.trainedforest)
+    for p in p_node:
+      self.add_parent(p)
 
 class use_forest_job(pipeline.CondorDAGJob):
   """
   Job using random forest to evaluate unclassified data.
   """
   def __init__(self, cp):
-	"""
-	"""
-	sections = ['evaluate-forest']
-	exec_name = 'SprOutputWriterApp'
-	tag_base = 'evaluate_forest'
-	auxmvc_analysis_job.__init__(self,cp, sections, exec_name, tag_base)
+    """
+    """
+    sections = ['evaluate-forest']
+    exec_name = 'SprOutputWriterApp'
+    tag_base = 'evaluate_forest'
+    auxmvc_analysis_job.__init__(self,cp, sections, exec_name, tag_base)
 
 class use_forest_node(pipeline.CondorDAGNode):
   """
   Node for radnom forest evaluation job. 
   """
   def __init__(self, job, trainedforest, file_to_rank, ranked_file,p_node=[]):
-	pipeline.CondorDAGNode.__init__(self,job)
-	self.job.set_stdout_file('logs/' + ranked_file.replace('.dat', '.out'))
+    pipeline.CondorDAGNode.__init__(self,job)
+    self.job.set_stdout_file('logs/' + ranked_file.replace('.dat', '.out'))
     self.job.set_stderr_file('logs/' + ranked_file.replace('.dat', '.err'))
-	self.add_input_file(trainedforest)
-	self.add_input_file(file_to_rank)
-	self.add_output_file(self.ranked_file)
-	self.trainedforest = self.get_input_files()[0]
-	self.file_to_rank = self.get_input_files()[1]
-	self.ranked_file = ranked_file
-	self.add_file_arg(" %s %s %s" % (self.trainedforest, self.file_to_rank, self.ranked_file))
-	  for p in p_node:
-		self.add_parent(p)
+    self.add_input_file(trainedforest)
+    self.add_input_file(file_to_rank)
+    self.add_output_file(self.ranked_file)
+    self.trainedforest = self.get_input_files()[0]
+    self.file_to_rank = self.get_input_files()[1]
+    self.ranked_file = ranked_file
+    self.add_file_arg(" %s %s %s" % (self.trainedforest, self.file_to_rank, self.ranked_file))
+    for p in p_node:
+      self.add_parent(p)
 
 class plot_channels_significance_job(pipeline.CondorDAGJob):
   """   
   Job that makes verious plots and histograms using significance of the axuiloary channels.      
   """
   def __init__(self, cp):
-	sections = ['plot-forest-channels-significance']
-	exec_name = 'auxmvc_plot_mvsc_channels_significance'
-	tag_base = 'plot_channels_signif'
+    sections = ['plot-forest-channels-significance']
+    exec_name = 'auxmvc_plot_mvsc_channels_significance'
+    tag_base = 'plot_channels_signif'
     auxmvc_analysis_job.__init__(self,cp, sections, exec_name, tag_base)      
 				
 	
@@ -253,10 +253,10 @@ class plot_channels_significance_node(pipeline.CondorDAGNode):
   Node for plot_channels_significance_job.
   """
   def __init__(self, job, input, p_node=[]):
-	pipeline.CondorDAGNode.__init__(self,job)
-	self.add_var_opt("input", input)
-	for p in p_node:                       
-	  self.add_parent(p)
+    pipeline.CondorDAGNode.__init__(self,job)
+    self.add_var_opt("input", input)
+    for p in p_node:                       
+      self.add_parent(p)
 			  
 
 
@@ -265,11 +265,11 @@ class result_plots_job(pipeline.CondorDAGJob):
   Job that makes plots based on results of evaluation e.g. ROC curves.
   """
   def __init__(self, cp, tag_base='RESULT_PLOTS'):
-	"""
-	"""
-	sections = ['result_plots']
-	exec_name = 'auxmvc_result_plots'
-	tag_base = 'auxmvc_result_plots'
+    """
+    """
+    sections = ['result_plots']
+    exec_name = 'auxmvc_result_plots'
+    tag_base = 'auxmvc_result_plots'
     auxmvc_analysis_job.__init__(self,cp, sections, exec_name, tag_base)  
 
 
@@ -278,11 +278,11 @@ class result_plots_node(pipeline.CondorDAGNode):
   Node for result_plots_job.
   """
   def __init__(self, job, datfiles, p_node=[]):
-	pipeline.CondorDAGNode.__init__(self,job)
-	for file in datfiles:
-	  self.add_file_arg(file[0])
-	for p in p_node:
-	  self.add_parent(p)
+    pipeline.CondorDAGNode.__init__(self,job)
+    for file in datfiles:
+      self.add_file_arg(file[0])
+    for p in p_node:
+      self.add_parent(p)
 		
 
 
