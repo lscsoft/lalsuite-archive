f6b5f0bdf6876d8a064d5b05d2061ca7c61d0810
Ian Harry <ian.harry@ligo.org>
2013-02-04 10:28:06 -0500

    First commit to add support for reading time-slid triggers in MultiInspiralUtils (cherry picked from commit 973b145628690247c431e0fac83b071be925746f)

--- pylal/pylal/MultiInspiralUtils.py
+++ pylal/pylal/MultiInspiralUtils.py
@@ -56,6 +56,62 @@ def ReadMultiInspiralFromFiles(fileList):
     except: multiInspiralTable = None
   return multis
 
+def ReadMultiInspiralTimeSlidesFromFiles(fileList):
+  """
+  Read time-slid multiInspiral tables from a list of files
+  @param fileList: list of input files
+  """
+  if not fileList:
+    return multiInspiralTable(), None
+
+  multis = None
+  timeSlides = []
+
+  for thisFile in fileList:
+    print thisFile
+
+    doc = utils.load_filename(thisFile,
+        gz=(thisFile or "stdin").endswith(".gz"))
+    # Extract the time slide table
+    timeSlideTable = table.get_table(doc,
+          lsctables.TimeSlideTable.tableName)
+    slideMapping = {}
+    currSlides = {}
+    for slide in timeSlideTable:
+      if slide.time_slide_id not in currSlides.keys():
+        currSlides[slide.time_slide_id] = {}
+       currSlides[slide.time_slide_id][slide.instrument] = slide.offset
+
+    print currSlides
+
+    for slideID,offsetDict in currSlides.items():
+      try:
+        # Is the slide already in the list and where?
+        offsetIndex = timeSlides.index(offsetDict)
+        slideMapping[slideID] = offsetIndex
+      except ValueError:
+        # If not then add it
+        timeSlides.append(offsetDict)
+        slideMapping[slideID] = len(timeSlide) - 1
+    
+    print slideMapping
+    print timeSlides
+         
+    # extract the multi inspiral table
+    try:
+      multiInspiralTable = table.get_table(doc,
+          lsctables.MultiInspiralTable.tableName)
+      # Remap the time slide IDs
+      for multi in multiInspiralTable:
+        newID = slideMapping[multi.time_slide_id]
+        multi.time_slide_id = ilwd.ilwdchar("multi_inspiral:time_slide_id:%d"\
+                                            newID)
+      if multis: multis.extend(multiInspiralTable)
+      else: multis = multiInspiralTable
+    except: multiInspiralTable = None
+  return multis,timeSlideTable
+
+
 #
 # =============================================================================
 #
