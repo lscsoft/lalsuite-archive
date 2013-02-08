Fixed scaling bug in 2D KDE confidence regions of bayespputils.py

pylal/pylal/bayespputils.py
@@ -3211,8 +3211,8 @@ def plot_two_param_kde_greedy_levels(posteriors_by_name,plot2DkdeParams,levels,c
   rangey=YMAX-YMIN
   Nx=50
   Ny=50
-  xax=np.linspace(min(xdat),max(xdat),Nx)
-  yax=np.linspace(min(ydat),max(ydat),Ny)
+  xax=np.linspace(XMIN,XMAX,Nx)
+  yax=np.linspace(YMIN,YMAX,Ny)
   x,y=np.meshgrid(xax,yax)
   
   CSlst=[]
@@ -3244,9 +3244,6 @@ def plot_two_param_kde_greedy_levels(posteriors_by_name,plot2DkdeParams,levels,c
       par2_injvalue=par2_injvalue-offset
       ax2_name=par2_name+' + %i'%(int(offset))
     else: ax2_name=par2_name
-    #Nx=rangex/xbin
-    #Ny=rangey/ybin
-
     
     samp=np.transpose(np.column_stack((xdat,ydat)))
     
@@ -3269,7 +3266,7 @@ def plot_two_param_kde_greedy_levels(posteriors_by_name,plot2DkdeParams,levels,c
     CSlst.append(CS)
     
     if par1_injvalue is not None and par2_injvalue is not None:
-      plt.plot([par1_injvalue],[par2_injvalue],'bo',scalex=False,scaley=False)
+      plt.plot([par1_injvalue],[par2_injvalue],'b*',scalex=False,scaley=False)
       
     if par_trigvalues1 is not None and par_trigvalues2 is not None:
 	par1IFOs = set([IFO for IFO in par_trigvalues1.keys()])