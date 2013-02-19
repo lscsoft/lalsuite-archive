42e2eea730aa1d3183096c4289c71decae6b9d62
Collin Capano <collin.capano@ligo.org>
2013-02-19 15:58:55 -0500

    added lalapps_cbc_plothzdist to pylal

--- pylal/setup.py
+++ pylal/setup.py
@@ -743,7 +743,8 @@ setup(
 		os.path.join("bin", "pylal_coh_PTF_sbv"),
 		os.path.join("bin", "pylal_coh_PTF_plot_results"),
 		os.path.join("bin", "pylal_write_coh_PTF_page"),
-		os.path.join("bin", "ligolw_dbinjfind")
+		os.path.join("bin", "ligolw_dbinjfind"),
+		os.path.join("bin", "lalapps_cbc_plothzdist")
 		],
 	data_files = [ ("etc", [
 		os.path.join("etc", "pylal-user-env.sh"),
