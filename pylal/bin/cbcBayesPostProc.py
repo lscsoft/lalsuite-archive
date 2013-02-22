496ac964c3e1a3d1d61a6d4e3f0519982e21cf22
Benjamin Farr <bfarr@u.northwestern.edu>
2013-02-22 15:30:00 -0600

    cbcBayesPostProc: removed unneeded print statements with new params

--- pylal/bin/cbcBayesPostProc.py
+++ pylal/bin/cbcBayesPostProc.py
@@ -469,9 +469,7 @@ def cbcBayesPostProc(
     old_spin_params = ['iota', 'theta1', 'phi1', 'theta2', 'phi2', 'beta']
     new_spin_params = ['theta_jn', 'phi_jl', 'tilt1', 'tilt2', 'phi12', 'a1', 'a2', 'm1', 'm2', 'f_ref']
     if set(new_spin_params).issubset(set(pos.names)) and not set(old_spin_params).issubset(set(pos.names)):
-      print pos['m1'].samples
       pos.append_mapping(old_spin_params, bppu.physical2radiationFrame, new_spin_params)
-      print pos['m1'].samples
 
     #Calculate spin magnitudes for aligned runs
     if 'spin1' in pos.names:
