e948c0ef74dbc48799f33dafeb288841d7f8abf4
Salvatore Vitale <salvatore.vitale@ligo.org>
2013-03-14 01:42:49 -0400

    Added cbcBayesBurstPostProc.py in the setup

--- pylal/setup.py
+++ pylal/setup.py
@@ -706,6 +706,7 @@ setup(
 		os.path.join("bin", "ring_post"),
 		os.path.join("bin", "ligolw_rinca_to_coinc"),
 		os.path.join("bin", "cbcBayesPostProc.py"),
+		os.path.join("bin", "cbcBayesBurstPostProc.py"),
 		os.path.join("bin", "cbcBayesCompPos.py"),
 		os.path.join("bin", "cbcBayesDIEvidence.py"),
 		os.path.join("bin", "cbcBayesInjProc.py"),
