3eede76d61d72c9457ad83af094d15c409f7dbc3
Stephen Privitera <stephen.privitera@ligo.org>
2013-03-28 17:21:48 -0500

    Revert "pipedown: make .sh file please"
    This reverts commit de63148751bffd5d0f38be1c7e2085e8ec6fc25f.  The
    addition of a mvsc sub-dag in pipedown broke this patch. Or maybe,
    this patch broke pipedown, since I failed to test from master.

--- glue/glue/pipeline.py
+++ glue/glue/pipeline.py
@@ -3600,7 +3600,7 @@ class LigolwSqliteJob(SqliteJob):
     Sets the --replace option. This will cause the job
     to overwrite existing databases rather than add to them.
     """
-    self.add_opt('replace', '')
+    self.add_var_opt('replace')
 
 
 class LigolwSqliteNode(SqliteNode):
