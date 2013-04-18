3b70e568b5579310f55e9e792a8b518133ee44d5
Marco Tompitak <marco.tompitak@ligo.org>
2013-04-18 15:23:41 +0200

    Put back qmParameters; removing them broke something.

--- lalmetaio/src/LIGOMetadataTables.h
+++ lalmetaio/src/LIGOMetadataTables.h
@@ -576,6 +576,8 @@ tagSimInspiralTable
   REAL4          eff_dist_g;
   REAL4          eff_dist_t;
   REAL4          eff_dist_v;
+  REAL4          qmParameter1;
+  REAL4          qmParameter2;
   INT4           eos;
   EventIDColumn  *event_id;
   INT4           numrel_mode_min;
