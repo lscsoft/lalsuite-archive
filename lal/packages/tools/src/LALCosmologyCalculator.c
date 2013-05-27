eea7d589db909edda1379e2b0ac2500b6b98f01b
Walter Del Pozzo <walter.delpozzo@ligo.org>
2013-05-27 14:18:37 +0200

    added comments to the new cosmology functions

--- lal/packages/tools/src/LALCosmologyCalculator.c
+++ lal/packages/tools/src/LALCosmologyCalculator.c
@@ -19,6 +19,17 @@
 #include <lal/LALMalloc.h>
 #include <lal/LALCosmologyCalculator.h>
 
+/** 
+* The set of functions in this module implements the standard cosmological distance measures 
+* defined a Friedmann-Robertson-Walker-Lemaitre metric.
+* For a detailed reference to the various formulae, see Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 ). 
+**/ 
+
+/** 
+* Computes the luminosity distance as the angular distance divided by (1+z)^2
+* Eq. 21 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 ) 
+**/
+
 double XLALLuminosityDistance(
             LALCosmologicalParameters *omega, 
             double z)
@@ -26,6 +37,11 @@ double XLALLuminosityDistance(
     double x=1.0+z;
     return XLALComovingTransverseDistance(omega, z)*x;
 }
+
+/** 
+* Computes the angular size distance by dividing the comoving transverse distance by 1+z
+* Eq. 18 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 )
+**/
 
 double XLALAngularDistance(
             LALCosmologicalParameters *omega, 
@@ -35,6 +51,12 @@ double XLALAngularDistance(
     return XLALComovingTransverseDistance(omega, z)/x;
 }
 
+ /** 
+* Computes the comoving line-of-sight distance (usually called the proper distance) by integrating the Hubble parameter.  
+* Eq. 15 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 ).
+**/
+
+
 double XLALComovingLOSDistance(
             LALCosmologicalParameters *omega, 
             double z)
@@ -42,7 +64,12 @@ double XLALComovingLOSDistance(
     double dH = XLALHubbleDistance(omega);
     return dH*XLALIntegrateHubbleParameter(omega,z);
 }
-            
+
+/**
+* Computes the comoving transverse distance from the comoving line-of-sight distance (proper distance) and
+* Eq. 16 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 )
+**/
+
 double XLALComovingTransverseDistance(
             LALCosmologicalParameters *omega, 
             double z)
@@ -72,6 +99,12 @@ double XLALComovingTransverseDistance(
 
 }
 
+/**
+* Computes the Hubble distance, independent of the redshift. This is the distance with sets the units for all others.
+* It returns the distance in Mpc. 
+* Eq. 4 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 )
+**/
+
 double XLALHubbleDistance(
             LALCosmologicalParameters *omega
             )
@@ -79,6 +112,11 @@ double XLALHubbleDistance(
     double x = GSL_CONST_MKSA_SPEED_OF_LIGHT/(100.0*omega->h);
     return 1e-3*x;
 }
+
+/**
+* Computes the inverse of the Hubble parameter at redshift z 
+* Eq. 14 in Hogg 1999 ( http://arxiv.org/abs/astro-ph/9905116 )
+**/
 
 double XLALHubbleParameter(double z,
             void *omega
@@ -97,6 +135,12 @@ double XLALHubbleParameter(double z,
     E = sqrt(om*x*x*x+ok*x*x+ol*pow(x,3.*(1.0+w0+w1+w2))*exp(-3.0*((w1+w2)*z/x + w2*z*z/(2.0*x*x))));
     return  1.0/E; 
 }
+
+/**
+* Computes the integral of inverse of the Hubble parameter at redshift z.
+* The integral is computed using the built-in function gsl_integration_qag of the gsl library. 
+* The integral is performed using a Gauss-Kronrod adaptive method (see http://www.gnu.org/software/gsl/manual/html_node/QAG-adaptive-integration.html ) 
+**/
 
 double XLALIntegrateHubbleParameter(LALCosmologicalParameters *omega, double z)
 {
@@ -122,6 +166,11 @@ double XLALIntegrateHubbleParameter(LALCosmologicalParameters *omega, double z)
     return result;
 }
 
+/** 
+* This function computes the value of a uniform probability distribution over the comoving volume.
+* Details of the derivation of these formulae can be found in Coward, Burman 2005 ( http://arxiv.org/abs/astro-ph/0505181 )
+**/
+
 double XLALUniformComovingVolumeDistribution(
             LALCosmologicalParameters *omega, 
             double z,
@@ -138,6 +187,10 @@ double XLALUniformComovingVolumeDistribution(
     return unnorm_density/norm;
 }
 
+/** 
+* This function computes the value of a uniform probability density distribution over the comoving volume at redshift z.
+**/
+
 double XLALUniformComovingVolumeDensity( 
             double z,
             void *omega)
@@ -151,6 +204,14 @@ double XLALUniformComovingVolumeDensity(
     double unnorm_density = 4.0*M_PI*dm*dm*E*XLALHubbleDistance(p)/x;
     return unnorm_density;
 }
+
+/**
+* Function that integrates the uniform in comoving volume density to compute the normalisation factor. 
+* Consistently with Coward, Burman 2005 ( http://arxiv.org/abs/astro-ph/0505181 ) the integral should be always performed up to infinity.
+* However, if the user specifies a value for zmax, the probability density will be normalised by integrating up to that value. 
+* To let the upper limit of integration go to infinity, specify zmax < 0. 
+* The integration is performed using gsl_integration_qagiu from the gsl library (see http://www.gnu.org/software/gsl/manual/html_node/QAGI-adaptive-integration-on-infinite-intervals.html )
+**/
 
 double XLALIntegrateComovingVolumeDensity(LALCosmologicalParameters *omega, double z)
 {
@@ -179,6 +240,14 @@ double XLALIntegrateComovingVolumeDensity(LALCosmologicalParameters *omega, doub
     return result;
 }
 
+/**
+* Creates a LALCosmologicalParameters structure from the values of the cosmological parameters.
+* Note that the boundary condition \Omega_m + \Omega_k + \Omega_\Lambda = 1 is NOT imposed here, 
+* but must be set prior to populating this structure and calling the required functions. 
+* Failure to impose the correct boundary condition will result in the Hubble parameter to be ill-defined 
+* and thus the code to crash.  
+**/
+
 LALCosmologicalParameters *XLALCreateCosmologicalParameters(double h, double om, double ok, double ol, double w0, double w1, double w2)
 {
     LALCosmologicalParameters *p = (LALCosmologicalParameters *)malloc(sizeof(LALCosmologicalParameters));
@@ -191,6 +260,10 @@ LALCosmologicalParameters *XLALCreateCosmologicalParameters(double h, double om,
     p->w2=w2;
     return p;
 }
+
+/**
+* Destroys a LALCosmologicalParameters structure.
+**/
 
 void XLALDestroyCosmologicalParameters(LALCosmologicalParameters *omega)
 {
