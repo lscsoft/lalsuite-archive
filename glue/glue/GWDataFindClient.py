0362bbfc93bbae572f5ebb39dbbfdff31527187d
Duncan Macleod <duncan.macleod@ligo.org>
2013-05-30 12:05:15 -0500

    glue.GWDataFindClient: formatting changes only

--- glue/glue/GWDataFindClient.py
+++ glue/glue/GWDataFindClient.py
@@ -14,99 +14,95 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+"""This module defines the GWDataFindHTTPConnection and
+GWDataFindHTTPSConnection objects for handling connections to
+the GWDataFindServer.
 """
-This module defines the GWDataFindHTTPConnection and GWDataFindHTTPSConnection objects for handling connections to the GWDataFindServer.
-"""
-
-# =============================================================================
-# Preamble
-# =============================================================================
 
 from __future__ import division
-import os, sys, time, calendar, httplib, M2Crypto, cjson, re, unittest
+import os
+import sys
+import time
+import calendar
+import httplib
+import M2Crypto
+import re
+import unittest
+
+from cjson import decode
 from glue import lal, git_version, segments
- 
-__author__ = "Duncan M. Macleod <duncan.macleod@ligo.org>"
+
+__author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __credits__ = "Scott Koranda <scott.koranda@ligo.org>"
 __version__ = git_version.id
 __date__    = git_version.date
 
 _server_env = "LIGO_DATAFIND_SERVER"
-_url_prefix = "/LDR/services/data/v1" 
+_url_prefix = "/LDR/services/data/v1"
 
-# =============================================================================
-# Class for datafind connection
-# =============================================================================
 
 class GWDataFindHTTPConnection(httplib.HTTPConnection):
+    """Connection to LIGO data replicator service using HTTP.
     """
-    Connection to LIGO data replicator service using HTTP.
-    """
-
     def __init__(self, host=None, **kwargs):
-        """
-        Connect to the LDR host using HTTPS. Default host is defined by the
-        %s environment variable.
+        """Connect to the LDR host using HTTPS. Default host is
+        defined by the %s environment variable.
         """
         if not host:
             host,port = find_server()
             kwargs.setdefault("port", port)
         httplib.HTTPConnection.__init__(self, host, **kwargs)
-    __init__.__doc__ %= _server_env 
-
-    decode = cjson.decode
+    __init__.__doc__ %= _server_env
 
     def _requestresponse(self, method, url, body=None, headers={}):
-        """
-        Internal method to perform request and verify reponse.
+        """Internal method to perform request and verify reponse.
         """
         try:
             self.request(method, url)
             response = self.getresponse()
         except Exception,e:
-            raise RuntimeError("Unable to query server %s: %s\n\n"\
-                               "Perhaps you need a valid proxy credential?\n"\
+            raise RuntimeError("Unable to query server %s: %s\n\n"
+                               "Perhaps you need a valid proxy credential?\n"
                                % (self.host, e))
         if response.status != 200:
-            raise RuntimeError("Server returned code %d: %s%s"\
-                               % (response.status, response.reason,\
+            raise RuntimeError("Server returned code %d: %s%s"
+                               % (response.status, response.reason,
                                   response.read()))
         return response
 
     def ping(self):
-        """
-        Ping the LDR host. Raises RuntimeError when ping fails, returns 0
-        otherwise.
+        """Ping the LDR host. Raises RuntimeError when ping fails,
+        returns 0 otherwise.
         """
         url = "%s/gwf/%s/%s/%s,%s" % (_url_prefix, 'H', 'R', '1', '2')
         self._requestresponse("HEAD", url)
         return 0
 
     def find_observatories(self, match=None):
-        """
-        Query the LDR host for observatories. Use match to restrict returned
-        observatories to those matching the regular expression.
+        """Query the LDR host for observatories. Use match to
+        restrict returned observatories to those matching the
+        regular expression.
 
         Example:
 
         >>> connection.find_observatories()
-        ['AGHLT', 'G', 'GHLTV', 'GHLV', 'GHT', 'H', 'HL', 'HLT', 'L', 'T', 'V', 'Z']
+        ['AGHLT', 'G', 'GHLTV', 'GHLV', 'GHT', 'H', 'HL', 'HLT',
+         'L', 'T', 'V', 'Z']
         >>> connection.find_observatories("H")
         ['H', 'HL', 'HLT']
         """
         url = "%s/gwf.json" % _url_prefix
         response = self._requestresponse("GET", url)
-        sitelist = sorted(set(self.decode(response.read())))
+        sitelist = sorted(set(decode(response.read())))
         if match:
             regmatch = re.compile(match)
             sitelist = [site for site in sitelist if regmatch.search(site)]
         return sitelist
-  
+
     def find_types(self, site=None, match=None):
-        """
-        Query the LDR host for frame types. Use site to restrict query to
-        given observatory prefix, and use match to restrict returned types to
-        those matching the regular expression.
+        """Query the LDR host for frame types. Use site to restrict
+        query to given observatory prefix, and use match to restrict
+        returned types to those matching the regular expression.
 
         Example:
 
@@ -128,77 +124,80 @@ class GWDataFindHTTPConnection(httplib.HTTPConnection):
         else:
             url = "%s/gwf/all.json" % _url_prefix
         response = self._requestresponse("GET", url)
-        typelist = sorted(set(self.decode(response.read())))
+        typelist = sorted(set(decode(response.read())))
         if match:
             regmatch = re.compile(match)
             typelist = [type for type in typelist if regmatch.search(type)]
         return typelist
-        
+
     def find_times(self, site, frametype, gpsstart=None, gpsend=None):
-        """
-        Query the LDR host for times for which frames are available for the
-        given site and frametype. Use gpsstart and gpsend to restrict the
-        returned times to this semiopen interval.
+        """Query the LDR host for times for which frames are
+        available for the given site and frametype.
+
+        Use gpsstart and gpsend to restrict the returned times to
+        this semiopen interval.
+
         Returns glue.segments.segmentlist.
         """
         if gpsstart and gpsend:
-            url = "%s/gwf/%s/%s/segments/%s,%s.json"\
-                  % (_url_prefix, site, frametype, gpsstart, gpsend)
+            url = ("%s/gwf/%s/%s/segments/%s,%s.json"
+                   % (_url_prefix, site, frametype, gpsstart, gpsend))
         else:
-            url = "%s/gwf/%s/%s/segments.json"\
-                  % (_url_prefix, site, frametype)
+            url = ("%s/gwf/%s/%s/segments.json"
+                   % (_url_prefix, site, frametype))
 
         response = self._requestresponse("GET", url)
-        segmentlist = self.decode(response.read())
+        segmentlist = decode(response.read())
         return segments.segmentlist(map(segments.segment, segmentlist))
 
     def find_frame(self, framefile, urltype=None, on_missing="warn"):
-        """
-        Query the LDR host for a single framefile. Use urltype to restrict the
-        returned frames to the given scheme (e.g. "file").
-        Returns glue.lal.Cache.
+        """Query the LDR host for a single framefile.
 
-        If on_missing="error" is given, raises RuntimeError if no frames are
-        found matching the given filepath, otherwise prints warning if "warn"
-        is given or nothing if "ignore" is given.
+        Use urltype to restrict the returned frames to the given
+        scheme (e.g. "file").
+
+
+        If on_missing="error" is given, raises RuntimeError if no
+        frames are found matching the given filepath, otherwise prints
+        warning if "warn" is given or nothing if "ignore" is given.
+
+        Returns glue.lal.Cache.
         """
         if on_missing not in ("warn", "error", "ignore"):
-            raise ValueError("on_missing must be \"warn\", "\
-                             "\"error\", or \"ignore\".")
+            raise ValueError("on_missing must be 'warn', 'error', or 'ignore'.")
         framefile = os.path.basename(framefile)
         # parse file name for site, frame type
         try:
             site,frametype,_,_ = framefile.split("-")
         except Exception, e:
             raise RuntimeError("Error parsing filename %s: %s" % (framefile, e))
-        url = "%s/gwf/%s/%s/%s.json"\
-              % (_url_prefix, site, frametype, framefile)
+        url = ("%s/gwf/%s/%s/%s.json"
+              % (_url_prefix, site, frametype, framefile))
         response = self._requestresponse("GET", url)
-        urllist  = self.decode(response.read())
+        urllist  = decode(response.read())
         if len(urllist) == 0:
             if on_missing == "warn":
                 sys.stderr.write("No files found!\n")
             elif on_missing == "error":
                 raise RuntimeError("No files found!")
         # verify urltype is what we want
-        cache = lal.Cache(e for e in\
-                          map(lal.CacheEntry.from_T050017, urllist)\
+        cache = lal.Cache(e for e in map(lal.CacheEntry.from_T050017, urllist)
                           if not urltype or e.scheme == urltype)
         return cache
 
     def find_latest(self, site, frametype, urltype=None, on_missing="warn"):
-        """
-        Query the LDR host for the most recently received framefile of the
-        given site and frametype. Use urltype to restrict the returned frames
-        to the given scheme (e.g. "file"). Returns glue.lal.Cache.
+        """Query the LDR host for the most recently received framefile
+        of the given site and frametype.
 
-        If on_missing="error" is given, raises RuntimeError if no frames are
-        found matching the given parameters, otherwise prints warning if "warn"
-        is given or nothing if "ignore" is given.
+        Use urltype to restrict the returned frames to the given scheme
+        (e.g. "file"). Returns glue.lal.Cache.
+
+        If on_missing="error" is given, raises RuntimeError if no
+        frames are found matching the given parameters, otherwise
+        prints warning if 'warn' is given or nothing if 'ignore' is given.
         """
-        if on_missing not in ("warn", "error", "ignore"):
-            raise ValueError("on_missing must be \"warn\", "\
-                             "\"error\", or \"ignore\".")
+        if on_missing not in ('warn', 'error', 'ignore'):
+            raise ValueError("on_missing must be 'warn', 'error', or 'ignore'.")
         url = "%s/gwf/%s/%s/latest" % (_url_prefix, site, frametype)
         # if a URL type is specified append it to the path
         if urltype:
@@ -206,7 +205,7 @@ class GWDataFindHTTPConnection(httplib.HTTPConnection):
         # request JSON output
         url += ".json"
         response = self._requestresponse("GET", url)
-        urllist  = self.decode(response.read())
+        urllist  = decode(response.read())
         if len(urllist) == 0:
             if on_missing == "warn":
                 sys.stderr.write("No files found!\n")
@@ -214,26 +213,29 @@ class GWDataFindHTTPConnection(httplib.HTTPConnection):
                 raise RuntimeError("No files found!")
         return lal.Cache(map(lal.CacheEntry.from_T050017, urllist))
 
-    def find_frame_urls(self, site, frametype, gpsstart, gpsend,\
+    def find_frame_urls(self, site, frametype, gpsstart, gpsend,
                         match=None, urltype=None, on_gaps="warn"):
-        """
-        Query the LDR host for the most all frame files of the given site and
-        frametype in the given [gpsstart, gpsend) interval. Use urltype to
-        restrict th returned frames to the given scheme (e.g. "file").
-        Returns glue.lal.Cache.
+        """Query the LDR host for the most all frame files of the given
+        site and frametype in the given [gpsstart, gpsend) interval.
 
-        If on_gaps="error" is given, raises RuntimeError if there are gaps in
-        the found frame list, otherwise prints warning if "warn" is given or
-        nothing if "ignore" is given. Use match to return only those frames
-        matching the given regular expression.
+        Use urltype to restrict th returned frames to the given
+        scheme (e.g. "file").
+
+        If on_gaps="error" is given, raises RuntimeError if there are
+        gaps in the found frame list, otherwise prints warning if
+        "warn" is given or nothing if "ignore" is given.
+
+        Use match to return only those frames matching the given
+        regular expression.
+
+        Returns glue.lal.Cache.
         """
         if on_gaps not in ("warn", "error", "ignore"):
-            raise ValueError("on_gaps must be \"warn\", "\
-                             "\"error\", or \"ignore\".")
-        url = "%s/gwf/%s/%s/%s,%s"\
-              % (_url_prefix, site, frametype, gpsstart, gpsend)
+            raise ValueError("on_gaps must be 'warn', 'error', or 'ignore'.")
+        url = ("%s/gwf/%s/%s/%s,%s"
+               % (_url_prefix, site, frametype, gpsstart, gpsend))
         # if a URL type is specified append it to the path
-        if urltype: 
+        if urltype:
             url += "/%s" % urltype
         # request JSON output
         url += ".json"
@@ -242,7 +244,7 @@ class GWDataFindHTTPConnection(httplib.HTTPConnection):
             url += "?match=%s" % match
         # make query
         response = self._requestresponse("GET", url)
-        urllist  = self.decode(response.read())
+        urllist  = decode(response.read())
 
         out = lal.Cache(map(lal.CacheEntry.from_T050017, urllist))
 
@@ -263,30 +265,24 @@ class GWDataFindHTTPConnection(httplib.HTTPConnection):
                     raise RuntimeError(msg)
 
 class GWDataFindHTTPSConnection(httplib.HTTPSConnection, GWDataFindHTTPConnection):
-    """
-    Secured connection to LIGO data replicator service using HTTPS.
+    """Secured connection to LIGO data replicator service using HTTPS.
     """
 
     def __init__(self, host=None, **kwargs):
-        """
-        Connect to the LDR host using HTTPS. Default host is defined by the
-        %s environment variable.
+        """Connect to the LDR host using HTTPS.
+
+        Default host is defined by the %s environment variable.
         """
         if not host:
             host, port = find_server()
             kwargs.setdefault("port", port)
         httplib.HTTPSConnection.__init__(self, host, **kwargs)
-    __init__.__doc__ %= _server_env 
+    __init__.__doc__ %= _server_env
 
-# =============================================================================
-# Helpers for validating datafind use
-# =============================================================================
 
 def validate_proxy(path):
-    """
-    Test that the proxy certificate is RFC 3820
-    compliant and that it is valid for at least
-    the next 15 minutes.
+    """Test that the proxy certificate is RFC 3820 compliant and
+    that it is valid for at least the next 15 minutes.
     """
     # load the proxy from path
     try:
@@ -299,8 +295,8 @@ def validate_proxy(path):
     try:
         proxy.get_ext("proxyCertInfo")
     except LookupError:
-      raise RuntimeError("Could not find a RFC 3820 compliant proxy "+\
-                         "credential. Please run 'grid-proxy-init -rfc' "+\
+      raise RuntimeError("Could not find a RFC 3820 compliant proxy "
+                         "credential. Please run 'grid-proxy-init -rfc' "
                          "and try again.")
 
     # attempt to make sure the proxy is still good for more than 15 minutes
@@ -316,20 +312,19 @@ def validate_proxy(path):
         secondsLeft = 3600
 
     if secondsLeft <= 0:
-        raise RuntimeError("Your proxy certificate is expired.\n"+\
-                           "Please generate a new proxy certificate and "+\
+        raise RuntimeError("Your proxy certificate is expired.\n"
+                           "Please generate a new proxy certificate and "
                            "try again. ")
     if secondsLeft < (60 * 15):
-        raise RuntimeError("Your proxy certificate expires in less than 15 "+\
-                           "minutes.\nPlease generate a new proxy "+\
+        raise RuntimeError("Your proxy certificate expires in less than 15 "
+                           "minutes.\nPlease generate a new proxy "
                            "certificate and try again.")
 
     # return True to indicate validated proxy
     return True
 
 def find_credential():
-    """
-    Follow the usual path that GSI libraries would
+    """Follow the usual path that GSI libraries would
     follow to find a valid proxy credential but
     also allow an end entity certificate to be used
     along with an unencrypted private key if they
@@ -339,8 +334,8 @@ def find_credential():
     kinit and then myproxy-login.
     """
 
-    rfc_proxy_msg = "Could not find a RFC 3820 compliant proxy credential."+\
-                    "Please run 'grid-proxy-init -rfc' and try again."
+    rfc_proxy_msg = ("Could not find a RFC 3820 compliant proxy credential."
+                     "Please run 'grid-proxy-init -rfc' and try again.")
 
     # use X509_USER_PROXY from environment if set
     if os.environ.has_key('X509_USER_PROXY'):
@@ -351,8 +346,8 @@ def find_credential():
             raise RuntimeError(rfc_proxy_msg)
 
     # use X509_USER_CERT and X509_USER_KEY if set
-    if os.environ.has_key('X509_USER_CERT')\
-    and os.environ.has_key('X509_USER_KEY'):
+    if (os.environ.has_key('X509_USER_CERT') and
+        os.environ.has_key('X509_USER_KEY')):
         certFile = os.environ['X509_USER_CERT']
         keyFile = os.environ['X509_USER_KEY']
         return certFile, keyFile
@@ -371,8 +366,7 @@ def find_credential():
     raise RuntimeError(rfc_proxy_msg)
 
 def find_server():
-    """
-    Find the LSC datafind server from the %s environment
+    """Find the LSC datafind server from the %s environment
     variable. If found returns the server,port tuple. If not found, raises
     a RuntimeError.
     """ % _server_env
@@ -388,13 +382,11 @@ def find_server():
     else:
         raise RuntimeError("Environment variable %s is not set" % _server_env)
 
-# =============================================================================
-# Unit test
-# =============================================================================
 
 class TestLDR(unittest.TestCase):
-    """
-    Small suite of test functions. Probably won't work if you're not on an LDAS
+    """Small suite of test functions.
+
+    robably won't work if you're not on an LDAS
     machine...
     """
 
@@ -425,7 +417,7 @@ class TestLDR(unittest.TestCase):
         h = GWDataFindHTTPConnection()
         h.find_times("L", "R")
         h.close()
- 
+
     def test_find_frame_urls(self):
         h = GWDataFindHTTPConnection()
         h.find_frame_urls("L", "R", 1000000000, 1000001000, on_gaps="ignore")
