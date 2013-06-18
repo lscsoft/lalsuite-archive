569be922c91d3076ba06a4b9f1b6e197a41cd7af
Will Meierjurgen Farr <will.farr@ligo.org>
2013-06-18 18:50:00 -0500

    Added '$' surrounding new tidal labels.

--- pylal/pylal/bayespputils.py
+++ pylal/pylal/bayespputils.py
@@ -235,12 +235,12 @@ def plot_label(param):
       'h1l1_delay':r'$\Delta t_\mathrm{HL}$',
       'h1v1_delay':r'$\Delta t_\mathrm{HV}$',
       'l1v1_delay':r'$\Delta t_\mathrm{LV}$',
-      'lambdat' : r'\tilde{\Lambda}', 
-      'dlambdat': r'\delta \tilde{\Lambda}',
-      'lambda1' : r'\lambda_1', 
-      'lambda2': r'\lambda_2',
-      'lam_tilde' : r'\tilde{\Lambda}', 
-      'dlam_tilde': r'\delta \tilde{\Lambda}'}
+      'lambdat' : r'$\tilde{\Lambda}$',
+      'dlambdat': r'$\delta \tilde{\Lambda}$',
+      'lambda1' : r'$\lambda_1$',
+      'lambda2': r'$\lambda_2$',
+      'lam_tilde' : r'$\tilde{\Lambda}$',
+      'dlam_tilde': r'$\delta \tilde{\Lambda}$'}
 
   # Handle cases where multiple names have been used
   if param in m1_names:
