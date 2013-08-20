9b489149f1b2729a287c0abf36aa711c0a1a0a0d
Kipp Cannon <kipp.cannon@ligo.org>
2013-08-20 12:53:17 -0400

    glue.ligolw.array.Stream:  check element count against array size

--- glue/glue/ligolw/array.py
+++ glue/glue/ligolw/array.py
@@ -188,6 +188,8 @@ class ArrayStream(ligolw.Stream):
 		# token, so add a final delimiter to induce the last token
 		# to get parsed.
 		self.appendData(self.getAttribute(u"Delimiter"))
+		if self._index != len(self._array_view):
+			raise ValueError("length of Stream (%d elements) does not match array size (%d elements)" % (self._index, len(self._array_view)))
 		del self._array_view
 		del self._index
 
