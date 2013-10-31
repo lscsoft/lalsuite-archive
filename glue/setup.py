8d24a443482637bb1dfcc020b01866061bcca81e
Collin Capano <collin.capano@ligo.org>
2013-10-31 15:49:34 -0400

    modifications to glue/setup.py for single stage ihope

--- glue/setup.py
+++ glue/setup.py
@@ -247,7 +247,7 @@ setup(
     os.path.join('bin','ligolw_dq_active'),
     os.path.join('bin','ligolw_dq_active_cats'),
     os.path.join('bin','ligolw_dq_grapher'),
-       os.path.join('bin','ldbdd'),
+    os.path.join('bin','ldbdd'),
     os.path.join('bin','ligolw_publish_dqxml'),
     os.path.join('bin','ligolw_diff'),
     os.path.join('bin','ligolw_geo_fr_to_dq'),
