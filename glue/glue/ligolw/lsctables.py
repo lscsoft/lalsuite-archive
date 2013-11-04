fa06420995a81ffedc6ba91a0e26926c7e1bd078
Salvatore Vitale <salvatore.vitale@ligo.org>
2013-11-04 17:41:22 -0500

    Use inject instead of antenna

--- glue/glue/ligolw/lsctables.py
+++ glue/glue/ligolw/lsctables.py
@@ -2740,13 +2740,13 @@ class SimBurst(TableRow):
 		if site is None:
 			return self.get_time_geocent()
 		else:
-			from pylal.xlal import tools
+			from pylal import xlal,inject
 			from pylal import date
 			from pylal.date import XLALTimeDelayFromEarthCenter
 			from pylal.xlal.datatypes.ligotimegps import LIGOTimeGPS
 			detMap = {'H': 'LHO_4k', 'L': 'LLO_4k',
                 'G': 'GEO_600', 'V': 'VIRGO', 'T': 'TAMA_300'}
-			location=tools.cached_detector[detMap[site]].location
+			location=inject.cached_detector[detMap[site]].location
 			ra,dec=self.get_ra_dec()
 			time=self.get_time_geocent()
 			time=time+LIGOTimeGPS(XLALTimeDelayFromEarthCenter(location,ra,dec,LIGOTimeGPS(time)))
