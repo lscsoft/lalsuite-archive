f1ca5e6f1f1aa55cb4b00d4585424c581da3092a
Florent Robinet <florent.robinet@ligo.org>
2013-11-07 09:33:32 +0100

    fix bug in the "print-snr" option flag

--- lalapps/src/string/StringSearch.c
+++ lalapps/src/string/StringSearch.c
@@ -465,9 +465,10 @@ int FindEvents(struct CommandLineArgsTag CLA, const StringTemplate *strtemplate,
   INT4 pmax, pend, pstart;
 
   /* print the snr to stdout */
-  if (CLA.printsnrflag)
+  if (CLA.printsnrflag){
     for ( p = vector->data->length/4 ; p < 3*vector->data->length/4; p++ )
-      fprintf(stdout,"%p %e\n", strtemplate, vector->data->data[p]);
+      fprintf(stdout,"%d %.5f %e\n", strtemplate->findex, vector->epoch.gpsSeconds+10e-9*vector->epoch.gpsNanoSeconds+p*vector->deltaT, vector->data->data[p]);
+  }
 
   /* Now find event in the inner half */
   for ( p = vector->data->length/4 ; p < 3*vector->data->length/4; p++ ){
@@ -1385,8 +1386,8 @@ int ReadCommandLine(int argc,char *argv[],struct CommandLineArgsTag *CLA, const 
       fprintf(stdout,"\t--cluster-events (-l)\tREAL4\t Cluster events with input timescale.\n");
       fprintf(stdout,"\t--print-spectrum (-a)\tFLAG\t Prints the spectrum to Spectrum.txt.\n");
       fprintf(stdout,"\t--print-fd-filter (-b)\tFLAG\t Prints the frequency domain filter to Filter-<template no>.txt.\n");
-      fprintf(stdout,"\t--print-td-filter (-r)\tFLAG\t Prints the time domain filter to FIRFilter-<template no>.txt.\n");
-      fprintf(stdout,"\t--print-snr (-x)\tFLAG\t Prints the snr to stdout.\n");
+      fprintf(stdout,"\t--print-td-filter (-x)\tFLAG\t Prints the time domain filter to FIRFilter-<template no>.txt.\n");
+      fprintf(stdout,"\t--print-snr (-r)\tFLAG\t Prints the snr to stdout.\n");
       fprintf(stdout,"\t--print-data (-y)\tFLAG\t Prints the post-processed (HP filtered, downsampled, padding removed, with injections) data to data.txt.\n");
       fprintf(stdout,"\t--print-injection (-z)\tFLAG\t Prints the injeciton data to injection.txt.\n");
       fprintf(stdout,"\t--help (-h)\t\t\tFLAG\t Print this message.\n");
