b379ef2ce73f406674847040868ff774eefeaee7
Duncan Macleod <duncan.macleod@ligo.org>
2013-11-13 15:42:30 -0600

    Revert "glue.ligolw.table.Table: new classmethod"
    This reverts commit 6a68116230fcc46fa8cd3f68ca306ade285db002.
    
    This should not have been pushed, redmine issue still open

--- glue/glue/ligolw/table.py
+++ glue/glue/ligolw/table.py
@@ -836,14 +836,6 @@ class Table(ligolw.Table, list):
 					except KeyError:
 						pass
 
-	@classmethod
-	def from_document(cls, xmldoc):
-		"""
-		Extract an instance of this `Table` from the given
-		LIGO_LW Document ``xmldoc``.
-		"""
-		return get_table(xmldoc, cls.tableName)
-
 
 #
 # =============================================================================
