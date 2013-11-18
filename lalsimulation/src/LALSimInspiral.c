0ace4cbdcb64b4cc5835e3e90bde6582148ab9d6
Yi Pan <ypan@umd.edu>
2013-11-18 18:25:25 -0500

    remove redefinition of SpinAlignedEOBversion

--- lalsimulation/src/LALSimInspiral.c
+++ lalsimulation/src/LALSimInspiral.c
@@ -2188,7 +2188,7 @@ int XLALSimInspiralChooseTDWaveform(
             if( f_ref != 0.)
                 XLALPrintWarning("XLAL Warning - %s: This approximant does use f_ref. The reference phase will be defined at coalescence.\n", __func__);
             /* Call the waveform driver routine */
-            UINT4 SpinAlignedEOBversion = 2;
+            SpinAlignedEOBversion = 2;
             ret = XLALSimIMRSpinAlignedEOBWaveform(hplus, hcross, phiRef, 
                     deltaT, m1, m2, f_min, r, i, S1z, S2z, SpinAlignedEOBversion);
             break;
