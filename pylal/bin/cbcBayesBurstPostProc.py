60f3364dbbfe600472bdc9c9728d451c2876e34a
Salvatore Vitale <salvatore.vitale@ligo.org>
2014-02-20 14:34:39 -0500

    added phase and psi to postproc

--- pylal/bin/cbcBayesBurstPostProc.py
+++ pylal/bin/cbcBayesBurstPostProc.py
@@ -1212,14 +1212,14 @@ if __name__=='__main__':
     polParams=['psi','polarisation','polarization']
     skyParams=['ra','rightascension','declination','dec']
     timeParams=['time']
-   
+    ellParams=['alpha']
     burstParams=['frequency','loghrss','q','hrss']
-    phaseParams=['phase']
+    phaseParams=['phase','phi_orb']
     #endTimeParams=['l1_end_time','h1_end_time','v1_end_time']
     endTimeParams=[]
     #statsParams=['logprior','logl','deltalogl','deltaloglh1','deltalogll1','deltaloglv1','deltaloglh2','deltaloglg1']
     statsParams=['logl']
-    oneDMenu=polParams + skyParams + timeParams + statsParams+burstParams
+    oneDMenu=polParams + skyParams + timeParams + statsParams+burstParams+ellParams+phaseParams
    
     ifos_menu=['h1','l1','v1']
     from itertools import combinations
@@ -1236,10 +1236,11 @@ if __name__=='__main__':
         #for bu in burstParams:
         #    for ti in timeParams:
         #        twoDGreedyMenu.append([bu,ti])
-  
+        twoDGreedyMenu.append(['alpha','phi_orb'])
+ 
     #twoDGreedyMenu=[['mc','eta'],['mchirp','eta'],['m1','m2'],['mtotal','eta'],['distance','iota'],['dist','iota'],['dist','m1'],['ra','dec']]
     #Bin size/resolution for binning. Need to match (converted) column names.
-    greedyBinSizes={'time':1e-4,'ra':0.05,'dec':0.05,'polarisation':0.04,'rightascension':0.05,'declination':0.05, 'loghrss':0.01,'frequency':1.,'q':0.1}
+    greedyBinSizes={'time':1e-4,'ra':0.05,'dec':0.05,'psi':0.04,'alpha':0.01,'polarisation':0.04,'phi_orb':0.04,'rightascension':0.05,'declination':0.05, 'loghrss':0.01,'frequency':1.,'q':0.1}
     #for derived_time in ['h1_end_time','l1_end_time','v1_end_time','h1l1_delay','l1v1_delay','h1v1_delay']:
     #    greedyBinSizes[derived_time]=greedyBinSizes['time']
     #if not opts.no2D:
