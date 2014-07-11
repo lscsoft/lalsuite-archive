5ade5586ff117b3f31bd4b0d63b671808d6ebe4d
Salvatore Vitale <salvatore.vitale@ligo.org>
2014-07-11 15:36:41 -0400

    Cleaned a bit and fixed a few problems

--- lalapps/src/inspiral/siminspiral_to_frame.c
+++ lalapps/src/inspiral/siminspiral_to_frame.c
@@ -80,9 +80,7 @@ static struct options options_defaults(void)
 }
 
 void ParseCharacterOptionString(char *input, char **strings[], UINT4 *n);
-//static void write_log(SimBurst **injs, TimeSlide * time_slide_table_head,struct options *options,char* fname);
 static void write_log(SimInspiralTable **injs, struct options *options,char* fname);
-
 
 static void print_usage(void)
 {
@@ -94,8 +92,8 @@ static void print_usage(void)
 " signals contained in the inspiral table. There will be a channel for each IFO. \n"\
 "Writes down a log file style\n"\
 "\n"\
-"By default the frame will start 10 seconds before the first injection of the\n"\
-" xml table and end 10 seconds after the last injection. \n"\
+"By default the frame will start 60 seconds before the first injection of the\n"\
+" xml table and end 60 seconds after the last injection. \n"\
 "Each channel will be named as the corresponding IFO:Strain (e.g. H1:Strain) .\n"\
 "\n" \
 "Options:\n" \
@@ -103,7 +101,7 @@ static void print_usage(void)
 "[--gps-start seconds]: Override the default starting time of the frame file. Note that this may lead to a frame with *fewer* signals than the original xml.\n" \
 "[--duration seconds]: Override the default duration of frame file. Note that this may lead to a frame file which contain *fewer* signals than the original xml \n" \
 "[--channels [A,B,..,N]: Override the default suffix of the channels to be IFO1:A, IFO2:B, etc. \n" \
-"[--pad seconds]: Override the default padding before the first and after the last signals\n"\
+"[--pad seconds]: Override the default padding before the first and after the last signals (this is neglected if gps-start is given)\n"\
 );
 }
 
@@ -177,7 +175,7 @@ static struct options parse_command_line(int *argc, char **argv[])
     if (options.mdc_gps_start==-1){
         fprintf(stdout,"mdc-gps-start time not provided. Will default to (min_t - pad) where min_t is the earliest trigtime contained in the siminpiral table. You can change this behavior with --mdc-gps-start\n\n");
     }
-    if (options.pad==-1){
+    if (options.pad==-1 && options.mdc_gps_start==-1){
         fprintf(stdout,"mdc padding length not provided. Will default to 10 seconds. You can change this behavior with --pad\n\n");
     }
     if (options.ifonames==NULL){
@@ -203,7 +201,7 @@ int main(int argc, char **argv)
 
 	struct options options;
 	SimInspiralTable *injs=NULL;
-	SimInspiralTable *inj,*cutinjs = NULL;
+	SimInspiralTable *inj=NULL,*cutinjs=NULL;
 
 	/*
 	 * Command line and process params table.
@@ -211,172 +209,189 @@ int main(int argc, char **argv)
      
 	options = parse_command_line(&argc, &argv);
   int Ninj;
-	  Ninj=SimInspiralTableFromLIGOLw(&injs,options.siminspiral_file,0,0);
-    cutinjs=injs;
-    (void) Ninj;
-    double xml_gps_start;
-    double xml_gps_end;
-    double dt=0;
-    UINT4 lost_before=0;
-        
+
+  // two copies of the injection file. Will *modify* cutinjs to remove those injections outside the desired time lapse
+  /* Edit, actually the last two options of SimInspiralTableFromLIGOLw can be used to only select events in a time range. Thus some of the code below is reinventing the wheel. Will need to update */
+  Ninj=SimInspiralTableFromLIGOLw(&injs,options.siminspiral_file,0,0);
+  Ninj=SimInspiralTableFromLIGOLw(&cutinjs,options.siminspiral_file,0,0);
+
+  // we won't need Ninj. Set it void to avoid complaints by the complier
+  (void) Ninj;
+
+  double xml_gps_start;
+  double xml_gps_end;
+  UINT4 lost_before=0, lost_after=0;
+
 	double mdc_gps_start=0;
 	int mdc_duration=0;
-    double pad=options.pad;
-    double mdc_max_time=0.; // that is the time of the last trigger we can write into the MDC , which is at least pad seconds far from the end of the frame
-    double mdc_min_time=0.; // that is the time of the first trigger we can write into the MDC , which is at least pad seconds far from the beginning of the frame
+  double pad=options.pad;
+  double mdc_max_time=0.; // that is the time of the last trigger we can write into the MDC , which is at least pad seconds far from the end of the frame
+  double mdc_min_time=0.; // that is the time of the first trigger we can write into the MDC , which is at least pad seconds far from the beginning of the frame
 
-    LIGOTimeGPS epoch;
-    char channame[256];
+  LIGOTimeGPS epoch;
+  char channame[256];
+
+  INT4 detectorFlags;
+  LALFrameH *frame=NULL;
+  CHAR fname[256];
+  REAL8 trigtime=0.0;
+  REAL8 srate=16384.;
+  REAL8 deltaT=1./srate;
+  REAL8 seglen,tmp;
+  UINT4 events=0;
+  UINT4 idx;
+  inj=injs;
+
+  /* if user did not provide pad, default to 60seconds */
+  if (pad==-1)
+      pad=60.;
     
-    INT4 detectorFlags;
-	LALFrameH *frame=NULL;
-	CHAR fname[256];
-    REAL8 trigtime=0.0;
-	REAL8 srate=16384.;
-	REAL8 deltaT=1./srate;
-	REAL8 seglen;
-    UINT4 events=0;
-    UINT4 i;
-    inj=injs;
-    
-    /* if user did not provide pad, default to 10seconds */
-    if (pad==-1)
-        pad=10.;
-    
-    xml_gps_start=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
-    while (inj){
-        xml_gps_end=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
-        events++;
-        inj=inj->next;
-    }
-    dt=(xml_gps_end-xml_gps_start)/(events-1.0);
-    
-    if (options.mdc_gps_start==-1)
-        mdc_gps_start=floor(xml_gps_start-pad);
-    else
-        mdc_gps_start=options.mdc_gps_start;
-        
-    XLALGPSSet(&epoch,mdc_gps_start,0); 
-    
-    if (options.mdc_duration!=-1)
-        mdc_duration=options.mdc_duration;
-    else
-        // Accomodate all the events in the xml table. Allow 1 pad per side
-        mdc_duration=(int) (2.*pad + (xml_gps_end-xml_gps_start));
-    
-    /* Check mdc_duration is larger than 2*pad */
-    if (2.*pad >= (double) mdc_duration){
-        fprintf(stderr,"ERROR: The duration of the frame (%d secs) is smaller than twice the pad (%.2f secs). Consider increasing mdc-duration or reducing pad\n",mdc_duration,2.*pad);
-        exit(1);
-    }    
-    
-    mdc_max_time=xml_gps_start+mdc_duration-2.0*pad;
-    mdc_min_time=mdc_gps_start+1.0*pad;
-    
-    inj=injs;
-    i=0;
-    
-    /* Now loop over the table, and only keep times smaller than mdc_max_time */
+  xml_gps_start=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
+  xml_gps_end=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
+
+  /* get the min and max gpstime in the xml file */
+  while (inj){
+      tmp=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
+      if (tmp<=xml_gps_start)
+        xml_gps_start=tmp;
+      if (tmp>=xml_gps_end)
+        xml_gps_end=tmp;
+      events++;
+      inj=inj->next;
+  }
+
+  /* If the user didn't specify a start time for the frame, use (xml_gps_start - pad), otherwise user choice w/o pad */
+  if (options.mdc_gps_start==-1)
+      mdc_gps_start=floor(xml_gps_start-pad);
+  else{
+      mdc_gps_start=options.mdc_gps_start;
+      pad=0.0;
+  }
+
+  /* If use specify duration, use that. Otherwise the time elapsed between last and first signal in the xml + 2 pad */
+  if (options.mdc_duration!=-1)
+    mdc_duration=options.mdc_duration;
+  else
+    // Accomodate all the events in the xml table.
+    mdc_duration=(int) ((xml_gps_end-xml_gps_start)+2.0*pad);
+
+  /* Check mdc_duration is larger than 2*pad */
+  if (2.*pad >= (double) mdc_duration){
+      fprintf(stderr,"ERROR: The duration of the frame (%d secs) is smaller than twice the pad (%.2f secs). Consider increasing mdc-duration or reducing pad\n",mdc_duration,2.*pad);
+      exit(1);
+  }
+
+  mdc_max_time=mdc_gps_start+mdc_duration;
+  mdc_min_time=mdc_gps_start;
+  XLALGPSSet(&epoch,mdc_gps_start,0);
+
+  inj=injs;
+  idx=0;
+
+    /* Now loop over the table, and only keep times mdc_min_time <t< mdc_max_time.
+     * Also count signals lost before or after the desired time range.  */
     while(inj){
+
         trigtime=inj->geocent_end_time.gpsSeconds+1.0e-9 * inj->geocent_end_time.gpsNanoSeconds;
-        
+
         if (trigtime < mdc_min_time){
-            /* This event is happening before the start of the frame, or is too close to the beginning of the frame. Skip it moving the start of injs by 1 */
-            cutinjs=inj->next;
+            /* This event is happening before the start of the frame (including pad). Skip it. */
             inj=inj->next;
             lost_before++;
             continue;
         }
         if (trigtime > mdc_max_time){
-            /* This event is happening after the end of the frame, or is too close to the end of the frame. That can happen only if this time is the first time of the xml (otherwise we would not have arrived here, because of the last if of this block) */
-            i=0;
-            break;
-        }
-
-        i++;
-
-        if (trigtime+dt<=mdc_max_time){
-            /* Next event is still going to be in range, so continue*/
+            /* This event is happening after the end of the frame (including pad).  Skip it.  */
+            lost_after++;
             inj=inj->next;
             continue;
         }
-        
-        /* Next event is going to be out of range. Set next to NULL*/
-        inj->next=NULL;
-        break;
+
+        //fprintf(stdout, "Keeping time %lf\n",trigtime);
+        memcpy(&cutinjs[idx],inj,sizeof(SimInspiralTable));
+        if (idx>0)
+          (cutinjs[idx-1]).next=&cutinjs[idx];
+        idx++;
+
+        inj=inj->next;
     }
-    
-    if (i==0){
+
+    fprintf(stdout, "Keeping %d events\n",idx);
+
+    /* Set the next element of the linked list to NULL so that LALSim will know where to stop */
+    (cutinjs[idx-1]).next=NULL;
+
+    if (idx==0){
         fprintf(stderr,"ERROR: The frame will not contain any injections. Check the gps-start, duration, and pad\n");
         exit(1);
     }
-    if (i<events){
-        if (i<events-lost_before)
-            fprintf(stdout,"WARNING: The frame will miss the last %d injections of the XML file, to follow duration and padding request!!\n\n",events-lost_before-i);
-        if (lost_before>0)
-            fprintf(stdout,"WARNING: The frame will miss the first %d injections of the XML file, to follow duration and padding request!!\n\n",lost_before);
-    }
-    
-    
-    /* point injs to cutinjs, i.e. to the first row with time >mdc_min_time */
-    injs=cutinjs;
-    inj=injs;
-    
+    if (lost_before>0)
+      fprintf(stdout,"WARNING: The frame will miss the first %d injections of the XML file, to comply with requested gsp-start, duration and padding!\n\n",lost_before);
+    if (lost_after>0)
+      fprintf(stdout,"WARNING: The frame will miss the last %d injections of the XML file, to comply with requested gsp-start, duration and padding!\n\n",lost_after);
+
+    /* Re-point to the beginning of the injections to store */
+    inj=&cutinjs[0];
+
     CHAR frameType[256];
     sprintf(frameType,inj->waveform);
-    
+
     /* Create a string with the first char of the IFO name to use as prefix for the frame name */
     char IFOs[10]="";
-    i=0;
+    UINT4 i=0;
     while(i<options.nIFO){
          sprintf(IFOs,"%s%c",IFOs,options.ifonames[i][0]);
          i++;
     }
-    
+
     snprintf( fname, FILENAME_MAX, "%s-%s-%d-%d.gwf", IFOs,frameType, (int) mdc_gps_start, mdc_duration );
-    
+
     /* set detector flags */
 	detectorFlags = LAL_GEO_600_DETECTOR_BIT | LAL_LHO_4K_DETECTOR_BIT |
 			LAL_LHO_2K_DETECTOR_BIT | LAL_LLO_4K_DETECTOR_BIT |
 			LAL_TAMA_300_DETECTOR_BIT | LAL_VIRGO_DETECTOR_BIT;
-		
+
 	seglen = mdc_duration*srate;
-	frame = XLALFrameNew( &epoch, mdc_duration, "LIGO", 0, 1,detectorFlags );
-	
+	/* Create the frame. */
+  frame = XLALFrameNew( &epoch, mdc_duration, "LIGO", 0, 1,detectorFlags );
+
+  /* For each IFO create a REAL8TimeSeries (soft) which will contain *all* injections in the time range for this IFO.
+   * This is done calling XLALInspiralInjectSignals.
+   * The time series is saved in the frame calling XLALFrameAddREAL8TimeSeriesSimData */
 	for (i=0;i<options.nIFO;i++){
-		
+
         if (options.channames)
             sprintf(channame,"%s:%s",options.ifonames[i],options.channames[i]);
         else
             sprintf(channame,"%s:Science",options.ifonames[i]);
-        
+
 		REAL8TimeSeries *soft=NULL;
+
 		soft = XLALCreateREAL8TimeSeries(channame,&epoch,0.0,deltaT,&lalStrainUnit,	seglen);
 		memset(soft->data->data,0.0,soft->data->length*sizeof(REAL8));
 		XLALInspiralInjectSignals(soft,inj , NULL);
-		/*char foutname[50]="";
-        sprintf(foutname,"MDC_create_time_%s",IFOname);
-		FILE * fout = fopen(foutname,"w");
-        UINT4 j=0;
-		for (j=0;j<soft->data->length;j++)
-		fprintf(fout,"%lf %10.10e\n", epoch.gpsSeconds+j*deltaT, soft->data->data[j]);
-		fclose(fout);*/
 		XLALFrameAddREAL8TimeSeriesSimData( frame, soft );
 		XLALDestroyREAL8TimeSeries(soft);
 	}
-    
+
 	XLALFrameWrite( frame, fname);
 	XLALFrameFree(frame);
-    
+
   write_log(&injs, &options, fname);
-    
+
     if(options.ifonames)
         XLALFree(options.ifonames);
+    /* We may want to free these, not sure.
+    if(injs)
+        XLALFree(injs);
+    if(cutinjs)
+        XLALFree(cutinjs);
+    if(first_inj)*/
 	return 0;
 }
 
 void ParseCharacterOptionString(char *input, char **strings[], UINT4 *n)
-/* "Stolen" from LALInference.c */
+/* Borrowed from LALInference.c */
 /* parses a character string (passed as one of the options) and decomposes   */
 /* it into individual parameter character strings. Input is of the form      */
 /*   input   :  "[one,two,three]"                                            */
@@ -426,7 +441,7 @@ void ParseCharacterOptionString(char *input, char **strings[], UINT4 *n)
 }
 
 static void write_log(SimInspiralTable **injs, struct options *options,char* fname){
-        
+
     SimInspiralTable *inj=&(*injs[0]);
     char log_filename[256];
     sprintf(log_filename,"%s.log",fname);
@@ -444,52 +459,49 @@ static void write_log(SimInspiralTable **injs, struct options *options,char* fna
     char IFOnames[5][4]={"GEO","H1","H2","L1","V1"};    
     int nifos=5;
     int i=0;
-    
+
     currentEqu.system = COORDINATESYSTEM_EQUATORIAL;
     currentGeo.system = COORDINATESYSTEM_GEOGRAPHIC;
 
-
     while(inj){
-        
+      mass1=inj->mass1;
+      mass2=inj->mass2;
+      distance=inj->distance;
+      ra=inj->longitude;
+      dec=inj->latitude;
+      psi=inj->polarization;
+      geoc_time=inj->geocent_end_time.gpsSeconds + 1.e-9* inj->geocent_end_time.gpsNanoSeconds;
+      XLALGPSSet(&injtime,inj->geocent_end_time.gpsSeconds ,inj->geocent_end_time.gpsNanoSeconds);
+
+      currentEqu.latitude = dec;
+      currentEqu.longitude = ra;
+      double mdc_gps_start=options->mdc_gps_start;
+
+      LALEquatorialToGeographic(&status, &currentGeo, &currentEqu, &injtime);
+
+      char wf[256];
+      sprintf(wf,"%s",inj->waveform);
     
-    mass1=inj->mass1;
-    mass2=inj->mass2;
-    distance=inj->distance;
-    ra=inj->longitude;
-    dec=inj->latitude;
-    psi=inj->polarization;
-    geoc_time=inj->geocent_end_time.gpsSeconds + 1.e-9* inj->geocent_end_time.gpsNanoSeconds;  
-    XLALGPSSet(&injtime,inj->geocent_end_time.gpsSeconds ,inj->geocent_end_time.gpsNanoSeconds);
-    
-    currentEqu.latitude = dec;
-    currentEqu.longitude = ra;
-    double mdc_gps_start=options->mdc_gps_start;
-    
-    LALEquatorialToGeographic(&status, &currentGeo, &currentEqu, &injtime);
-    
-    char wf[256];
-    sprintf(wf,"%s",inj->waveform);
-	
-    char WFname[512]; 
-    sprintf(WFname,"%s_m1_%.1fm2_%.1f",wf,mass1,mass2);
-    fprintf(log_file,"%s\
-    -100 \
-    -100 \
-    %.1f\
-    -100\
-    -100\
-    %.10e\
-    %.10e\
-    %.10e\
-    %d\
-    %10.10f\
-    %s\
-    -100\
-    -100\
-    -100 ",WFname,distance, cos(LAL_PI_2 - currentGeo.latitude),currentGeo.longitude,psi,(int) (mdc_gps_start), geoc_time,WFname);
-    
-    i=0;
-    while (i<nifos){
+      char WFname[512];
+      sprintf(WFname,"%s_m1_%.1fm2_%.1f",wf,mass1,mass2);
+      fprintf(log_file,"%s\
+      -100 \
+      -100 \
+      %.1f\
+      -100\
+      -100\
+      %.10e\
+      %.10e\
+      %.10e\
+      %d\
+      %10.10f\
+      %s\
+      -100\
+      -100\
+      -100 ",WFname,distance, cos(LAL_PI_2 - currentGeo.latitude),currentGeo.longitude,psi,(int) (mdc_gps_start), geoc_time,WFname);
+
+      i=0;
+      while (i<nifos){
         LALDetector* detector=XLALCalloc(1,sizeof(LALDetector));
         if (!strcmp(IFOnames[i],"H1") || !strcmp(IFOnames[i],"H2"))
             memcpy(detector,&lalCachedDetectors[LALDetectorIndexLHODIFF],sizeof(LALDetector));
@@ -507,12 +519,11 @@ static void write_log(SimInspiralTable **injs, struct options *options,char* fna
     
         i++;
         XLALFree(detector);
-    }    
-    
+      }
+
     fprintf(log_file, "\n");
     inj=inj->next;
     }
-    
-    fclose(log_file);    
 
+    fclose(log_file);
 }
