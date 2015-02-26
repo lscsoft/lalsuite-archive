123deb466b202cb8c4595855723b96b2fe898252
Kipp Cannon <kipp.cannon@ligo.org>
2015-02-26 15:00:04 -0500

    pylal:  remove xlal.noisemodels module

--- pylal/setup.py
+++ pylal/setup.py
@@ -388,15 +388,6 @@ setup(
 			extra_compile_args = lal_pkg_config.extra_cflags
 		),
 		Extension(
-			"pylal.xlal.noisemodels",
-			["src/xlal/noisemodels.c"],
-			include_dirs = lal_pkg_config.incdirs,
-			libraries = lal_pkg_config.libs,
-			library_dirs = lal_pkg_config.libdirs,
-			runtime_library_dirs = lal_pkg_config.libdirs,
-			extra_compile_args = lal_pkg_config.extra_cflags
-		),
-		Extension(
 			"pylal.xlal.tools",
 			["src/xlal/tools.c", "src/xlal/misc.c"],
 			include_dirs = lal_pkg_config.incdirs + lalmetaio_pkg_config.incdirs + lalinspiral_pkg_config.incdirs + [numpy_get_include(), "src/xlal"],
