c0a968c4c7da40c45178648781c41ea69a355c04
Thomas Dent <thomas.dent@ligo.org>
2015-03-25 22:45:50 +0100

    added GPL notice

--- pylal/pylal/trigger_fits.py
+++ pylal/pylal/trigger_fits.py
@@ -1,7 +1,16 @@
 from __future__ import division
 
-# Copyright T. Dent 2015
-# add Free Software stuff here
+# Copyright T. Dent 2015 (thomas.dent@aei.mpg.de)
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
 
 import numpy
 from scipy.stats import kstest
