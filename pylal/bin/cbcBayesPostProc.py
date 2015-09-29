78ad6e98fcba3aa1041f9a59bbe0b8c6a090ba4b
Ben Farr <farr@uchicago.edu>
2015-09-29 17:22:06 -0500

    Plot splines when no PSD files given

--- pylal/bin/cbcBayesPostProc.py
+++ pylal/bin/cbcBayesPostProc.py
@@ -605,14 +605,14 @@ def cbcBayesPostProc(
         print "Could not create PSD plot. The error was: %s\n"%str(e)
         wfsection.write("<b>No PSD file found!</b>")
 
-      # Add plots for calibration estimates
-      if np.any(['spcal_amp' in param for param in pos.names]) or np.any(['spcal_phase' in param for param in pos.names]):
-        wftd=html_wf.insert_td(row,'',label='Calibration',legend=legend)
-        wfsection=html.add_section_to_element('Calibration',wftd)
-        bppu.plot_calibration_pos(pos, outpath=outdir)
-        wfsection.write('<a href="calibration.png" target="_blank"><img src="calibration.png"/></a>')
-      else:
-        wfsection.write("<b> No calibration plots </b>")
+    # Add plots for calibration estimates
+    if np.any(['spcal_amp' in param for param in pos.names]) or np.any(['spcal_phase' in param for param in pos.names]):
+      wftd=html_wf.insert_td(row,'',label='Calibration',legend=legend)
+      wfsection=html.add_section_to_element('Calibration',wftd)
+      bppu.plot_calibration_pos(pos, outpath=outdir)
+      wfsection.write('<a href="calibration.png" target="_blank"><img src="calibration.png"/></a>')
+    else:
+      wfsection.write("<b> No calibration plots </b>")
 
 
     #==================================================================#
