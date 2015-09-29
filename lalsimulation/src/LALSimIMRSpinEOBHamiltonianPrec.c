77ea385610417f12716c259cbb03cd9fdced33a2
Roland Haas <rhaas@aei.mpg.de>
2015-09-29 20:33:26 +0200

    SEOBv3: remove divisions and if from XLALSimIMRCalculateSpinEOBHCoeffs
    * pre-compute and group some divisions and expensive ops
    * remove if statements in favor of arithmetic

--- lalsimulation/src/LALSimIMRSpinEOBHamiltonianPrec.c
+++ lalsimulation/src/LALSimIMRSpinEOBHamiltonianPrec.c
@@ -614,9 +614,14 @@ static int XLALSimIMRCalculateSpinPrecEOBHCoeffs(
   REAL8 KK, k0, k1, k2, k3, k4, k5, k5l, k1p2, k1p3;
   REAL8 m1PlusEtaKK;
 
+  if ( !coeffs )
+  {
+    XLAL_ERROR( XLAL_EINVAL );
+  }
+
   coeffs->SpinAlignedEOBversion = SpinAlignedEOBversion;
    
-  int debugPK = 0;
+  const int debugPK = 0;
   if( debugPK )
   {
     printf("In XLALSimIMRCalculateSpinPrecEOBHCoeffs: SpinAlignedEOBversion = %d,%d\n",
@@ -633,65 +638,54 @@ static int XLALSimIMRCalculateSpinPrecEOBHCoeffs(
   static const REAL8 c22  = -39.77229225266885;
   static const REAL8 c23  = 103.16588921239249;
 
-  if ( !coeffs )
-  {
-    XLAL_ERROR( XLAL_EINVAL );
-  }
+  static const REAL8 third = 1./3.;
+  static const REAL8 fifth = 1./5.;
+  static const REAL8 log2 = 0.6931471805599453094172321214581765680755; // log(2.)
 
+  // RH: this assumes that SpinAlignedEOBversion is either 1 or 2
+  // RH: the ifthenelse macros return their ifvalue if cond>=0 (specifically
+  // the positive sign is set) and elsevalue otherwise. So:
+  // RH: 1.5-SpinAlignedEOBversion is positive for SpinAlignedEOBversion==1 and
+  // RH: negative for SpinAlignedEOBversion==2
+  // RH: SpinAlignedEOBversion-1.5 is reversed
 
   coeffs->b3  = 0.;
   coeffs->bb3 = 0.;
-  coeffs->KK = KK  = c0 + c1*eta + c2*eta*eta;
-  if ( SpinAlignedEOBversion == 2)
-  {
-     coeffs->KK = KK = c20 + c21*eta + c22*eta*eta + c23*eta*eta*eta;
-  }
-
+#define ifthenelse(cond, ifvalue, elsevalue) ((elsevalue) + (0.5 + copysign(0.5, cond)) * ((ifvalue)-(elsevalue)))
+  coeffs->KK = KK = ifthenelse(1.5-SpinAlignedEOBversion,
+                               c0 + c1*eta + c2*eta*eta,
+                               c20 + c21*eta + c22*(eta*eta) + c23*(eta*eta)*eta);
   m1PlusEtaKK = -1. + eta*KK;
+  const REAL8 invm1PlusEtaKK = 1./m1PlusEtaKK;
   /* Eqs. 5.77 - 5.81 of BB1 */
   coeffs->k0 = k0 = KK*(m1PlusEtaKK - 1.);
   coeffs->k1 = k1 = - 2.*(k0 + KK)*m1PlusEtaKK;
   k1p2= k1*k1;
   k1p3= k1*k1p2;
-  coeffs->k2 = k2 = (k1 * (k1 - 4.*m1PlusEtaKK)) / 2. - a*a*k0*m1PlusEtaKK*m1PlusEtaKK;
-  coeffs->k3 = k3 = -k1*k1*k1/3. + k1*k2 + k1*k1*m1PlusEtaKK - 2.*(k2 - m1PlusEtaKK)*m1PlusEtaKK - a*a*k1*m1PlusEtaKK*m1PlusEtaKK;
-  coeffs->k4 = k4 = (24.*k1*k1*k1*k1 - 96.*k1*k1*k2 + 48.*k2*k2 - 64.*k1*k1*k1*m1PlusEtaKK
-      + 48.*a*a*(k1*k1 - 2.*k2)*m1PlusEtaKK*m1PlusEtaKK +
-      96.*k1*(k3 + 2.*k2*m1PlusEtaKK) - m1PlusEtaKK*(192.*k3 + m1PlusEtaKK*(-3008. + 123.*LAL_PI*LAL_PI)))/96.;
-  coeffs->k5 = k5 = 0.0;
-  coeffs->k5l= k5l= 0.0;
-  if ( SpinAlignedEOBversion == 2 )
-  {
-    coeffs->k5 = k5 = m1PlusEtaKK*m1PlusEtaKK
-	       * (-4237./60.+128./5.*LAL_GAMMA+2275.*LAL_PI*LAL_PI/512.
-	       - 1./3.*a*a*(k1p3-3.*k1*k2+3.*k3)
-	       - (k1p3*k1p2-5.*k1p3*k2+5.*k1*k2*k2+5.*k1p2*k3-5.*k2*k3-5.*k1*k4)/5./m1PlusEtaKK/m1PlusEtaKK
-	       + (k1p2*k1p2-4.*k1p2*k2+2.*k2*k2+4.*k1*k3-4.*k4)/2./m1PlusEtaKK+256./5.*log(2.));
-    coeffs->k5l = k5l = m1PlusEtaKK*m1PlusEtaKK * 64./5.;
-  }
+  coeffs->k2 = k2 = (k1 * (k1 - 4.*m1PlusEtaKK)) * 0.5 - a*a*k0*m1PlusEtaKK*m1PlusEtaKK;
+  coeffs->k3 = k3 = -(k1*k1)*k1 * third + k1*k2 + (k1*k1)*m1PlusEtaKK - 2.*(k2 - m1PlusEtaKK)*m1PlusEtaKK - a*a*k1*(m1PlusEtaKK*m1PlusEtaKK);
+  coeffs->k4 = k4 = ((24./96.)*(k1*k1)*(k1*k1) - (96./96.)*(k1*k1)*k2 + (48./96.)*k2*k2 - (64./96.)*(k1*k1)*k1*m1PlusEtaKK
+      + (48./96.)*(a*a)*(k1*k1 - 2.*k2)*(m1PlusEtaKK*m1PlusEtaKK) +
+      (96./96.)*k1*(k3 + 2.*k2*m1PlusEtaKK) - m1PlusEtaKK*((192./96.)*k3 + m1PlusEtaKK*(-(3008./96.) + (123./96.)*LAL_PI*LAL_PI)));
+#define ifthenelsezero(cond, ifvalue) ((0.5 + copysign(0.5, cond)) * (ifvalue))
+  coeffs->k5 = k5 = ifthenelsezero(SpinAlignedEOBversion-1.5,
+                     m1PlusEtaKK*m1PlusEtaKK
+                     * (-4237./60.+128./5.*LAL_GAMMA+2275.*LAL_PI*LAL_PI/512.
+                     - third*(a*a)*(k1p3-3.*(k1*k2)+3.*k3)
+                     - ((k1p3*k1p2)-5.*(k1p3*k2)+5.*k1*k2*k2+5.*k1p2*k3-5.*k2*k3-5.*k1*k4)*fifth*invm1PlusEtaKK*invm1PlusEtaKK
+                     + ((k1p2*k1p2)-4.*(k1p2*k2)+2.*k2*k2+4.*k1*k3-4.*k4)*0.5*invm1PlusEtaKK+(256./5.)*log2)
+                    );
+  coeffs->k5l= k5l= ifthenelsezero(SpinAlignedEOBversion-1.5, (m1PlusEtaKK*m1PlusEtaKK) * (64./5.));
 
   /*printf( "a = %.16e, k0 = %.16e, k1 = %.16e, k2 = %.16e, k3 = %.16e, k4 = %.16e, b3 = %.16e, bb3 = %.16e, KK = %.16e\n",
             a, coeffs->k0, coeffs->k1, coeffs->k2, coeffs->k3, coeffs->k4, coeffs->b3, coeffs->bb3, coeffs->KK );
   */
 
   /* Now calibrated parameters for spin models */
-  coeffs->d1 = coeffs->d1v2 = 0.0;
-  coeffs->dheffSS = coeffs->dheffSSv2 = 0.0;
-  switch ( SpinAlignedEOBversion )
-  {
-     case 1:
-       coeffs->d1 = -69.5;
-       coeffs->dheffSS = 2.75;
-       break;
-     case 2:
-       coeffs->d1v2 = -74.71 - 156.*eta + 627.5*eta*eta;
-       coeffs->dheffSSv2 = 8.127 - 154.2*eta + 830.8*eta*eta;
-       break;
-     default:
-       XLALPrintError( "XLAL Error - %s: wrong SpinAlignedEOBversion value, must be 1 or 2!\n", __func__ );
-       XLAL_ERROR( XLAL_EINVAL );
-       break;
-  }
+  coeffs->d1 = ifthenelsezero(1.5-SpinAlignedEOBversion, -69.5);
+  coeffs->d1v2 = ifthenelsezero(SpinAlignedEOBversion-1.5, -74.71 - 156.*eta + 627.5*eta*eta);
+  coeffs->dheffSS = ifthenelsezero(1.5-SpinAlignedEOBversion, 2.75);
+  coeffs->dheffSSv2 = ifthenelsezero(SpinAlignedEOBversion-1.5, 8.127 - 154.2*eta + 830.8*eta*eta);
 
   return XLAL_SUCCESS;
 }
