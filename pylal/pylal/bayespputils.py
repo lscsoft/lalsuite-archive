eb0a52415095bae5d7d02e4c32e52d82df8e8f3e
Salvatore Vitale <salvatore.vitale@ligo.org>
2015-10-01 19:31:58 -0400

    updated nonParams to include all calibParams

--- pylal/pylal/bayespputils.py
+++ pylal/pylal/bayespputils.py
@@ -5321,8 +5321,13 @@ class PEOutputParser(object):
                 ntots.append(ntot)
                 if nDownsample is None:
                     try:
+                        splineParams=["spcal_npts", "spcal_active","constantcal_active"]
+                        for i in np.arange(5):
+                          for k in ['h1','l1']:
+                            splineParams.append(k+'_spcal_freq'+str(i))
+                            splineParams.append(k+'_spcal_logfreq'+str(i))
                         nonParams = ["logpost", "cycle", "timestamp", "snrh1", "snrl1", "snrv1",
-                                     "time_mean", "time_maxl","sky_frame","constantcal_active","psdscaleflag","flow","f_ref","lal_amporder","lal_pnorder","lal_approximant","signalmodelflag", "spcal_npts", "spcal_active" ] + logParams + snrParams
+                                     "time_mean", "time_maxl","sky_frame","psdscaleflag","flow","f_ref","lal_amporder","lal_pnorder","lal_approximant","signalmodelflag"] + logParams + snrParams
                         nonParamsIdxs = [header.index(name) for name in nonParams if name in header]
                         paramIdxs = [i for i in range(len(header)) if i not in nonParamsIdxs]
                         samps = np.array(lines).astype(float)
