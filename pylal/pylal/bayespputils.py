a0bfcf6130cb888df6b834ae6646fef3c1206e03
Ben Farr <farr@uchicago.edu>
2015-10-06 13:27:54 -0400

    Add detector-frame parameters to post-proc ignore list

--- pylal/pylal/bayespputils.py
+++ pylal/pylal/bayespputils.py
@@ -5326,10 +5326,12 @@ class PEOutputParser(object):
                           for k in ['h1','l1']:
                             splineParams.append(k+'_spcal_freq'+str(i))
                             splineParams.append(k+'_spcal_logfreq'+str(i))
+
                         nonParams = ["logpost", "cycle", "timestamp", "snrh1", "snrl1", "snrv1",
                                      "margtime","margtimephi","margtime","time_max","time_min",
                                      "time_mean", "time_maxl","sky_frame","psdscaleflag","logdeltaf","flow","f_ref",
-                                     "lal_amporder","lal_pnorder","lal_approximant","signalmodelflag"] + logParams + snrParams + splineParams
+                                     "lal_amporder","lal_pnorder","lal_approximant","signalmodelflag",
+                                     "t0", "phase_maxl", "azimuth", "cosalpha"] + logParams + snrParams + splineParams
                         nonParamsIdxs = [header.index(name) for name in nonParams if name in header]
                         paramIdxs = [i for i in range(len(header)) if i not in nonParamsIdxs]
                         samps = np.array(lines).astype(float)
