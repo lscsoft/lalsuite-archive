05f3b1a0b1da3eb09f61d8ef712169bc54a7e2c2
Roland Haas <rhaas@aei.mpg.de>
2015-10-08 20:45:02 +0200

    SEOB: provide timing headers in a file

--- lalsimulation/test/run_test.sh
+++ lalsimulation/test/run_test.sh
@@ -6,7 +6,7 @@ HASH_ORIGINAL=4f360267440a71d315fd4625f2b5e7097a1e0112
 rm GenerateSimulation
 make GenerateSimulation
 
-/usr/bin/time -p --output=timing-$HASH.txt ./GenerateSimulation --approximant SEOBNRv3 --spin1x 0 --spin1y 0 --spin1z 0.8 --spin2x 0.5 --spin2y 0 --spin2z 0.0 --m1 40 --m2 30 --outname simV3-$HASH.out --f-min 10 --distance 1 --inclination 0 --sample-rate 2048 --domain TD
+/usr/bin/time -p --output=timing-$HASH.txt ./GenerateSimulation --approximant SEOBNRv3 --spin1x 0 --spin1y 0 --spin1z 0.8 --spin2x 0.5 --spin2y 0 --spin2z 0.0 --m1 40 --m2 30 --outname simV3-$HASH.out --f-min 10 --distance 1 --inclination 0 --sample-rate 2048 --domain TD >simV3-$HASH-times.txt
 $HOME/spec/specMaster/Support/bin/ComputeComplexPhase -A 2 <simV3-$HASH.out >simV3-$HASH-ampphase.out
 
 # compute maximum phase difference
