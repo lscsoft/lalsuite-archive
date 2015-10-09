9cd9bd198566e2baeb4d9941bd3faeed35c6cc4f
Chris Van Den Broeck <chris.vandenbroeck@ligo.org>
2015-10-09 06:56:43 -0500

    Adding the new testing parameters dsigma, dbeta, dalpha to postprocessing.

--- pylal/bin/cbcBayesPostProc.py
+++ pylal/bin/cbcBayesPostProc.py
@@ -1303,7 +1303,7 @@ if __name__=='__main__':
     phaseParams=['phase', 'phi0','phase_maxl']
     endTimeParams=['l1_end_time','h1_end_time','v1_end_time']
     ppEParams=['ppEalpha','ppElowera','ppEupperA','ppEbeta','ppElowerb','ppEupperB','alphaPPE','aPPE','betaPPE','bPPE']
-    tigerParams=['dchi%i'%(i) for i in range(7)] + ['dchi%il'%(i) for i in [5,6] ]+['dxi%d'%(i+1) for i in range(6)]
+    tigerParams=['dchi%i'%(i) for i in range(7)] + ['dchi%il'%(i) for i in [5,6] ] + ['dxi%d'%(i+1) for i in range(6)] + ['dalpha%i'%(i+1) for i in range(5)] + ['dbeta%i'%(i+1) for i in range(3)] + ['dsigma%i'%(i+1) for i in range(4)]
     bransDickeParams=['omegaBD','ScalarCharge1','ScalarCharge2']
     massiveGravitonParams=['lambdaG']
     tidalParams=['lambda1','lambda2','lam_tilde','dlam_tilde','lambdat','dlambdat']
