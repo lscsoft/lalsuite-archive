71c03fe5a43ea4e3f4e1b4ffda4f53eb4cb712e9
Jeongcho Kim <jeongcho.kim@ligo.org>
2016-03-12 05:54:35 +0900

    correct for eccentricity

--- lalsimulation/src/LALSimInspiralWaveformCache.c
+++ lalsimulation/src/LALSimInspiralWaveformCache.c
@@ -507,7 +507,8 @@ int XLALSimInspiralChooseFDWaveformFromCache(
         if (frequencies != NULL)
             return XLALSimInspiralChooseFDWaveformSequence(hptilde, hctilde, phiRef,
                                                            m1, m2, S1x, S1y, S1z, S2x, S2y, S2z, f_ref,
-                                                           r, i, lambda1, lambda2, waveFlags, nonGRparams, amplitudeO,
+                                                           r, i, lambda1, lambda2, 0, 0, 0, 
+                                                           waveFlags, nonGRparams, amplitudeO,
                                                            phaseO, approximant,frequencies);
         else
             return XLALSimInspiralChooseFDWaveform(hptilde, hctilde, phiRef, deltaF,
