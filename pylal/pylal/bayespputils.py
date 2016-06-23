bayespputils handle fixed parameters in ACL calculation

pylal/pylal/bayespputils.py
@@ -680,13 +680,13 @@ class PosteriorOneDPDF(object):
     def KL(self):
         """Returns the KL divergence between the prior and the posterior.
         It measures the relative information content in nats. The prior is evaluated
-        at run time. It defaults to None. If None is passed, it just returns the information content 
+        at run time. It defaults to None. If None is passed, it just returns the information content
         of the posterior."
         """
-        
+
         def uniform(x):
             return np.array([1./(np.max(x)-np.min(x)) for _ in x])
-        
+
         posterior, dx = np.histogram(self.samples,bins=36,normed=True)
         from scipy.stats import entropy
         # check the kind of prior and process the string accordingly
@@ -707,9 +707,9 @@ class PosteriorOneDPDF(object):
             prior_dist = eval(prior)
         except:
             raise ValueError
-        
+
         return entropy(posterior, qk=prior_dist)
-    
+
     def prob_interval(self,intervals):
         """
         Evaluate probability intervals.
@@ -980,7 +980,7 @@ class Posterior(object):
               pos.append_mapping(new_spin_params, spin_angles, old_params)
           except KeyError:
               print "Warning: Cannot find spin parameters.  Skipping spin angle calculations."
-                
+
       #Calculate effective precessing spin magnitude
       if ('a1' in pos.names and 'tilt1' in pos.names and 'm1' in pos.names ) and ('a2' in pos.names and 'tilt2' in pos.names and 'm2' in pos.names):
           pos.append_mapping('chi_p', chi_precessing, ['a1', 'tilt1', 'm1', 'a2', 'tilt2', 'm2'])
@@ -989,14 +989,14 @@ class Posterior(object):
       # Calculate redshift from luminosity distance measurements
       if('distance' in pos.names):
           pos.append_mapping('redshift', calculate_redshift, 'distance')
-     
+
       # Calculate source mass parameters
       if ('m1' in pos.names) and ('redshift' in pos.names):
           pos.append_mapping('m1_source', source_mass, ['m1', 'redshift'])
 
       if ('m2' in pos.names) and ('redshift' in pos.names):
           pos.append_mapping('m2_source', source_mass, ['m2', 'redshift'])
-        
+
       if ('mtotal' in pos.names) and ('redshift' in pos.names):
           pos.append_mapping('mtotal_source', source_mass, ['mtotal', 'redshift'])
 
@@ -1138,14 +1138,14 @@ class Posterior(object):
               pos.append_mapping('mf_source', source_mass, ['mf', 'redshift'])
           except Exception,e:
               print "Could not calculate final source frame mass. The error was: %s"%(str(e))
-    
+
       # Calculate radiated energy and peak luminosity
       if ('mtotal_source' in pos.names) and ('mf_source' in pos.names):
           try:
               pos.append_mapping('e_rad', lambda mtot_s, mf_s: mtot_s-mf_s, ['mtotal_source', 'mf_source'])
           except Exception,e:
               print "Could not calculate radiated energy. The error was: %s"%(str(e))
-      
+
       if ('q' in pos.names) and ('a1z' in pos.names) and ('a2z' in pos.names):
           try:
               pos.append_mapping('l_peak', bbh_aligned_Lpeak_6mode_SHXJDK, ['q', 'a1z', 'a2z'])
@@ -1221,7 +1221,7 @@ class Posterior(object):
         """
 
         return -2.0*(np.mean(self._logL) - np.var(self._logL))
-    
+
     @property
     def injection(self):
         """
@@ -1828,7 +1828,7 @@ class Posterior(object):
             hpmap = hp.reorder(hpmap, r2n=True)
 
         return hpmap
-        
+
     def __str__(self):
         """
         Define a string representation of the Posterior class ; returns
@@ -3650,7 +3650,7 @@ def skymap_confidence_areas(hpmap, cls):
 
     pixarea = hp.nside2pixarea(hp.npix2nside(hpmap.shape[0]))
     pixarea = pixarea*(180.0/np.pi)**2 # In square degrees
-    
+
     areas = []
     for cl in cls:
         npix = np.sum(cum_hpmap < cl) # How many pixels to sum before cl?
@@ -3844,7 +3844,7 @@ def orbital_momentum_mag(fref, m1,m2,eta):
     PNFirst = (((m1+m2)**2)*eta)/v0
     PNSecond = 1+ (v0**2) * (3.0/2.0 -eta/6.0)
     Lmag= PNFirst*PNSecond
-    return Lmag 
+    return Lmag
 
 def component_momentum(m, a, theta, phi):
     """
@@ -5721,8 +5721,10 @@ class PEOutputParser(object):
                                      "lal_amporder","lal_pnorder","lal_approximant","tideo","spino","signalmodelflag",
                                      "t0", "phase_maxl", "azimuth", "cosalpha"] + logParams + snrParams + splineParams
                         nonParamsIdxs = [header.index(name) for name in nonParams if name in header]
-                        paramIdxs = [i for i in range(len(header)) if i not in nonParamsIdxs]
                         samps = np.array(lines).astype(float)
+                        fixedIdxs = np.where(np.amin(samps,axis=0)-np.amax(samps,axis=0) == 0.0)[0]
+                        nonParamsIdxs.extend(fixedIdxs)
+                        paramIdxs = [i for i in range(len(header)) if i not in nonParamsIdxs]
                         stride=samps[1,iterindex] - samps[0,iterindex]
                         results = np.array([np.array(effectiveSampleSize(samps[:,i])[:2]) for i in paramIdxs])
                         nEffs = results[:,0]
@@ -6015,7 +6017,7 @@ class PEOutputParser(object):
             for key in header:
               posterior_data.append(active_group[key][...])
             posterior_data=np.array(posterior_data)
-        # join the fields into a (nfields, nsamples) array, transposed 
+        # join the fields into a (nfields, nsamples) array, transposed
         # to be (nsamples, nfields)
         flines = posterior_data.T
 
@@ -6037,7 +6039,7 @@ class PEOutputParser(object):
         print('Read columns %s' % str(header))
 
         return header, flines
-        
+
 
     def _common_to_pos(self,infile,info=[None,None]):
         """
@@ -6808,7 +6810,7 @@ def spline_angle_xform(delta_psi):
     rot = (2.0 + 1.0j*delta_psi)/(2.0 - 1.0j*delta_psi)
 
     return 180.0/np.pi*np.arctan2(np.imag(rot), np.real(rot))
-    
+
 def plot_spline_pos(logf, ys, nf=100, level=0.9, color='k', label=None, xform=None):
     """Plot calibration posterior estimates for a spline model in log space.
     Args:
@@ -6829,7 +6831,7 @@ def plot_spline_pos(logf, ys, nf=100, level=0.9, color='k', label=None, xform=No
         zs = ys
     else:
         zs = xform(ys)
-        
+
     mu = np.mean(zs, axis=0)
     lower_cl = mu - cred_interval(zs, level, lower=True)
     upper_cl = cred_interval(zs, level, lower=False) - mu
@@ -6908,7 +6910,7 @@ def plot_calibration_pos(pos, level=.9, outpath=None):
 
 
 def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1','V1']):
-  
+
   from lalinference.lalinference import SimBurstChooseFDWaveform,SimBurstChooseTDWaveform
   from lalinference.lalinference import SimBurstImplementedFDApproximants,SimBurstImplementedTDApproximants
   from lal.lal import StrainUnit
@@ -6934,7 +6936,7 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
     event=0
   colors_inj={'H1':'r','L1':'g','V1':'m','I1':'b','J1':'y'}
   colors_rec={'H1':'k','L1':'k','V1':'k','I1':'k','J1':'k'}
-  #import sim inspiral table content handler 
+  #import sim inspiral table content handler
   from glue.ligolw import ligolw
   from glue.ligolw import lsctables
   from glue.ligolw import table
@@ -6960,8 +6962,8 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
   lsctables.use_in(LIGOLWContentHandlerExtractSimBurstTable)
   #from pylal.SimBurstUtils import ExtractSimBurstTableLIGOLWContentHandler
   #lsctables.use_in(ExtractSimBurstTableLIGOLWContentHandler)
-  
-  # time and freq data handling variables 
+
+  # time and freq data handling variables
   srate=4096.0
   seglen=10.
   length=srate*seglen # lenght of 10 secs, hardcoded.
@@ -6980,7 +6982,7 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
   strainFrec= CreateCOMPLEX16FrequencySeries("strainF",segStart,0.0,deltaF,DimensionlessUnit,int(length/2. +1));
   strainTrec=CreateREAL8TimeSeries("strainT",segStart,0.0,1.0/srate,DimensionlessUnit,int(length));
   GlobREAL8time=None
-  f_min=25 # hardcoded default (may be changed below) 
+  f_min=25 # hardcoded default (may be changed below)
   f_ref=100 # hardcoded default (may be changed below)
   f_max=srate/2.0
 
@@ -6991,7 +6993,7 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
 
   inj_strains=dict((i,{"T":{'x':None,'strain':None},"F":{'x':None,'strain':None}}) for i in ifos)
   rec_strains=dict((i,{"T":{'x':None,'strain':None},"F":{'x':None,'strain':None}}) for i in ifos)
-  
+
   inj_domain=None
   rec_domain=None
   font_size=26
@@ -7019,11 +7021,11 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
       hrss=tbl.hrss
       polar_e_angle=tbl.pol_ellipse_angle
       polar_e_ecc=tbl.pol_ellipse_e
-  
+
       BurstExtraParams=None
       wf=str(tbl.waveform)
 
-      injapproximant=lalinf.GetBurstApproximantFromString(wf)  
+      injapproximant=lalinf.GetBurstApproximantFromString(wf)
       ra=tbl.ra
       dec=tbl.dec
       psi=tbl.psi
@@ -7037,14 +7039,14 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
       else:
         print "\nThe approximant %s doesn't seem to be recognized by lalinference!\n Skipping WF plots\n"%injapproximant
         return None
-      
+
       for ifo in ifos:
         (fp,fc,fa,qv)=ant.response(REAL8time,ra,dec,0.0,psi,'radians',ifo)
         if inj_domain=='T':
           # bin of ref time as seen in strainT
           tCinstrain=np.floor(REAL8time-float(strainTinj.epoch))/deltaT
           # bin of strainT where we need to start copying the WF over
-          #tSinstrain=floor(tCinstrain-float(plus.data.length)/2.)+1 
+          #tSinstrain=floor(tCinstrain-float(plus.data.length)/2.)+1
           tSinstrain=int(  (REAL8time-fabs(float(plus.epoch)) - fabs(float(strainTinj.epoch)))/deltaT)
           rem=(REAL8time-fabs(float(plus.epoch)) - fabs(float(strainTinj.epoch)))/deltaT-tSinstrain
           # strain is a temporary container for this IFO strain.
@@ -7117,10 +7119,10 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
 
 
   if pos is not None:
-    
+
     # Select the maxP sample
     _,which=pos._posMap()
-    
+
     if 'time' in pos.names:
       REAL8time=pos['time'].samples[which][0]
     elif 'time_maxl' in pos.names:
@@ -7133,9 +7135,9 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
       print "ERROR: could not find any time parameter in the posterior file. Not plotting the WF...\n"
       return None
 
-    # first check we have approx in posterior samples, otherwise skip 
+    # first check we have approx in posterior samples, otherwise skip
     skip=0
-    
+
     try:
       approximant=int(pos['LAL_APPROXIMANT'].samples[which][0])
     except:
@@ -7176,7 +7178,7 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
       BurstExtraParams=None
       #if alpha:
       #  BurstExtraParams=lalsim.SimBurstCreateExtraParam("alpha",alpha)
-      
+
       if SimBurstImplementedFDApproximants(approximant):
         rec_domain='F'
         [plus,cross]=SimBurstChooseFDWaveform(deltaF, deltaT, f0, q,dur, f_min, f_max,hrss,polar_e_angle ,polar_e_ecc,BurstExtraParams, approximant)
@@ -7269,10 +7271,10 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
             plot_tmax=REAL8time+6.*sigmaT
 
   myfig=plt.figure(1,figsize=(10,7))
-  
+
   rows=len(ifos)
   cols=2
-  
+
   #this variables decide which domain will be plotted on the left column of the plot.
   # only plot Time domain if both injections and recovery are TD
   global_domain="F"
@@ -7285,7 +7287,7 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
   elif inj_domain is not None:
     if inj_domain=="T":
       global_domain="T"
-  
+
   A,axes=plt.subplots(nrows=rows,ncols=cols,sharex=False,sharey=False)
   plt.setp(A,figwidth=10,figheight=7)
   for (r,i) in zip(np.arange(rows),ifos):
@@ -7352,26 +7354,26 @@ def plot_burst_waveform(pos=None,simburst=None,event=0,path=None,ifos=['H1','L1'
             ax.set_xlabel("frequency [Hz]",fontsize=font_size)
         else:
           ax.set_xlabel("frequency [Hz]",fontsize=font_size)
-      
+
       ax.legend(loc='best')
       ax.grid(True)
-      
+
       #ax.tight_layout()
   A.savefig(os.path.join(path,'WF_DetFrame.png'),bbox_inches='tight')
   return inj_strains, rec_strains
 
 def make_1d_table(html,legend,label,pos,pars,noacf,GreedyRes,onepdfdir,sampsdir,savepdfs,greedy,analyticLikelihood,nDownsample):
-    
+
     from numpy import unique, sort
     global confidenceLevels
     confidence_levels=confidenceLevels
-    
+
     out={}
     if pars==[]:
       return out
     if set(pos.names)-set(pars)==set(pos.names):
       return out
-    
+
     #2D plots list
     tabid='onedmargtable_'+label.lower()
     html_ompdf=html.add_collapse_section('1D marginal posterior PDFs (%s)'%label,legend=legend,innertable_id=tabid)
@@ -7394,7 +7396,7 @@ def make_1d_table(html,legend,label,pos,pars,noacf,GreedyRes,onepdfdir,sampsdir,
         cycles = sort(pos['cycle'].samples)
         Ncycles = cycles[-1]-cycles[0]
         Nskip = cycles[1]-cycles[0]
-    
+
     printed=0
     for par_name in pars:
         par_name=par_name.lower()
@@ -7548,9 +7550,8 @@ def make_1d_table(html,legend,label,pos,pars,noacf,GreedyRes,onepdfdir,sampsdir,
           html_ompdf_write+='<tr><td width="30%"><img width="100%" src="1Dpdf/'+figname+'"/></td><td width="30%"><img width="100%" src="1Dsamps/'+figname.replace('.png','_samps.png')+'"/></td>'+acfhtml+'</tr>'
         else:
             html_ompdf_write+='<tr><td width="30%"><img width="100%" src="1Dpdf/'+figname+'"/></td><td width="30%"><img width="100%" src="1Dsamps/'+figname.replace('.png','_samps.png')+'"/></td></tr>'
-    
+
     html_ompdf_write+='</table>'
     html_ompdf.write(html_ompdf_write)
-    
+
     return out
-    