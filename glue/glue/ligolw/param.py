57005ab44368b8f79d913a2149445a5e13f40ead
Kipp Cannon <kipp.cannon@ligo.org>
2016-08-17 20:01:00 +0900

    glue.ligolw.param:  update copyright

--- glue/glue/ligolw/param.py
+++ glue/glue/ligolw/param.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006--2009,2012--2015  Kipp Cannon
+# Copyright (C) 2006--2009,2012--2016  Kipp Cannon
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
