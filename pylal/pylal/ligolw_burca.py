lscsoft/lalsuite-archive

commit f0850d573fded4cf707edbd6db793915d2d69da7

snglcoinc:  delete make_eventlists()

pylal/pylal/ligolw_burca.py
@@ -369,7 +369,7 @@ def ligolw_burca(
 	# processes that can participate in a coincidence
 	#
 
-	eventlists = snglcoinc.make_eventlists(xmldoc, EventListType, lsctables.SnglBurstTable.tableName)
+	eventlists = snglcoinc.EventListDict(xmldoc, EventListType, lsctables.SnglBurstTable.get_table(xmldoc))
 
 	#
 	# construct offset vector assembly graph

pylal/pylal/ligolw_rinca.py
@@ -425,7 +425,7 @@ def ligolw_rinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.make_eventlists(xmldoc, EventListType, lsctables.SnglRingdownTable.tableName)
+	eventlists = snglcoinc.EventListDict(xmldoc, EventListType, lsctables.SnglRingdownTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.get_start() not in veto_segments[event.ifo]), eventlist)

pylal/pylal/ligolw_sstinca.py
@@ -580,7 +580,7 @@ def ligolw_thinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.make_eventlists(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.tableName)
+	eventlists = snglcoinc.EventListDict(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.get_end() not in veto_segments[event.ifo]), eventlist)

pylal/pylal/ligolw_thinca.py
@@ -408,7 +408,7 @@ def ligolw_thinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.make_eventlists(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.tableName)
+	eventlists = snglcoinc.EventListDict(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.end not in veto_segments[event.ifo]), eventlist)

pylal/pylal/snglcoinc.py
@@ -209,16 +209,6 @@ class EventListDict(dict):
 			eventlist.set_offset(0)
 
 
-def make_eventlists(xmldoc, EventListType, event_table_name, process_ids = None):
-	"""
-	Convenience wrapper for constructing a dictionary of event lists
-	from an XML document tree, the name of a table from which to get
-	the events, a maximum allowed time window, and the name of the
-	program that generated the events.
-	"""
-	return EventListDict(EventListType, ligolw_table.get_table(xmldoc, event_table_name), process_ids = process_ids)
-
-
 #
 # =============================================================================
 #

commit b86ea5cc248e7d3243dc7e03aea497cb569256de

pylal:  fix bug introduced in f0850d573fded4cf707edbd6db793915d2d69da7
- forgot to remove the first argument from EventListDict() calls

pylal/pylal/ligolw_burca.py
@@ -369,7 +369,7 @@ def ligolw_burca(
 	# processes that can participate in a coincidence
 	#
 
-	eventlists = snglcoinc.EventListDict(xmldoc, EventListType, lsctables.SnglBurstTable.get_table(xmldoc))
+	eventlists = snglcoinc.EventListDict(EventListType, lsctables.SnglBurstTable.get_table(xmldoc))
 
 	#
 	# construct offset vector assembly graph

pylal/pylal/ligolw_rinca.py
@@ -425,7 +425,7 @@ def ligolw_rinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.EventListDict(xmldoc, EventListType, lsctables.SnglRingdownTable.get_table(xmldoc))
+	eventlists = snglcoinc.EventListDict(EventListType, lsctables.SnglRingdownTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.get_start() not in veto_segments[event.ifo]), eventlist)

pylal/pylal/ligolw_sstinca.py
@@ -580,7 +580,7 @@ def ligolw_thinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.EventListDict(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
+	eventlists = snglcoinc.EventListDict(InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.get_end() not in veto_segments[event.ifo]), eventlist)

pylal/pylal/ligolw_thinca.py
@@ -408,7 +408,7 @@ def ligolw_thinca(
 	# removing events from the lists that fall in vetoed segments
 	#
 
-	eventlists = snglcoinc.EventListDict(xmldoc, InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
+	eventlists = snglcoinc.EventListDict(InspiralEventList, lsctables.SnglInspiralTable.get_table(xmldoc))
 	if veto_segments is not None:
 		for eventlist in eventlists.values():
 			iterutils.inplace_filter((lambda event: event.ifo not in veto_segments or event.end not in veto_segments[event.ifo]), eventlist)