cef8b5246fcf820d90f39cae78bfc33962f4c2ff
zetienne <zachetie@gmail.com>
2016-08-30 09:12:29 -0400

    Fix stray character in lalinspiral/src/LALRungeKutta4.c

--- lalinspiral/src/LALRungeKutta4.c
+++ lalinspiral/src/LALRungeKutta4.c
@@ -1,4 +1,4 @@
-\/*
+/*
 *  Copyright (C) 2007 Duncan Brown, Gareth Jones, Jolien Creighton, B.S. Sathyaprakash, Craig Robinson
 *
 *  This program is free software; you can redistribute it and/or modify
