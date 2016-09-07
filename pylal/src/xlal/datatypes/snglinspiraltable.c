604fb74b40a21eda3fa6c9229fe987ac367e11fb
Kipp Cannon <kipp.cannon@ligo.org>
2016-09-07 17:11:01 +0900

    pylal:  update snglinspiraltable.c copyright notice

--- pylal/src/xlal/datatypes/snglinspiraltable.c
+++ pylal/src/xlal/datatypes/snglinspiraltable.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010  Kipp Cannon
+ * Copyright (C) 2010-2013,2015,2016  Kipp Cannon
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
