98cd9c5b4d74abd520e2b1d1e9092a77396de955
Kipp Cannon <kipp.cannon@ligo.org>
2016-09-08 23:03:09 -0700

    pylal.xlal.tools: remove unused Coinc type

--- pylal/src/xlal/tools.c
+++ pylal/src/xlal/tools.c
@@ -42,89 +42,6 @@
 /*
  * ============================================================================
  *
- *                                 Coinc Type
- *
- * ============================================================================
- */
-
-
-/*
- * Cached ID types
- */
-
-
-static PyObject *process_id_type = NULL;
-static PyObject *coinc_event_id_type = NULL;
-static PyObject *coinc_def_id_type = NULL;
-static PyObject *time_slide_id_type = NULL;
-
-
-/*
- * Structure
- */
-
-
-typedef struct {
-	PyObject_HEAD
-	long process_id_i;
-	long coinc_def_id_i;
-	long coinc_event_id_i;
-	long time_slide_id_i;
-	PyObject *instruments;
-	int nevents;
-	double likelihood;
-} ligolw_Coinc;
-
-
-/*
- * Attributes
- */
-
-
-static struct PyMemberDef ligolw_Coinc_members[] = {
-	{"instruments", T_OBJECT, offsetof(ligolw_Coinc, instruments), 0, "instruments"},
-	{"nevents", T_INT, offsetof(ligolw_Coinc, nevents), 0, "nevents"},
-	{"likelihood", T_DOUBLE, offsetof(ligolw_Coinc, likelihood), 0, "likelihood"},
-	{NULL,}
-};
-
-
-static struct PyGetSetDef ligolw_Coinc_getset[] = {
-	{"process_id", pylal_ilwdchar_id_get, pylal_ilwdchar_id_set, "process_id", &(struct pylal_ilwdchar_id_description) {offsetof(ligolw_Coinc, process_id_i), &process_id_type}},
-	{"coinc_def_id", pylal_ilwdchar_id_get, pylal_ilwdchar_id_set, "coinc_def_id", &(struct pylal_ilwdchar_id_description) {offsetof(ligolw_Coinc, coinc_def_id_i), &coinc_def_id_type}},
-	{"coinc_event_id", pylal_ilwdchar_id_get, pylal_ilwdchar_id_set, "coinc_event_id", &(struct pylal_ilwdchar_id_description) {offsetof(ligolw_Coinc, coinc_event_id_i), &coinc_event_id_type}},
-	{"time_slide_id", pylal_ilwdchar_id_get, pylal_ilwdchar_id_set, "time_slide_id", &(struct pylal_ilwdchar_id_description) {offsetof(ligolw_Coinc, time_slide_id_i), &time_slide_id_type}},
-	{NULL,}
-};
-
-
-/*
- * Methods
- */
-
-
-/* FIXME:  add get_instruments(), set_instruments() */
-
-
-/*
- * Type
- */
-
-
-PyTypeObject ligolw_Coinc_Type = {
-	PyObject_HEAD_INIT(NULL)
-	.tp_basicsize = sizeof(ligolw_Coinc),
-	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_CHECKTYPES,
-	.tp_name = MODULE_NAME ".Coinc",
-	.tp_new = PyType_GenericNew,
-	.tp_members = ligolw_Coinc_members,
-	.tp_getset = ligolw_Coinc_getset,
-};
-
-
-/*
- * ============================================================================
- *
  *                                 Functions
  *
  * ============================================================================
@@ -199,29 +116,7 @@ PyMODINIT_FUNC inittools(void)
 
 	pylal_snglringdowntable_import();
 
-	/* Cached ID types */
-	process_id_type = pylal_get_ilwdchar_class("process", "process_id");
-	coinc_def_id_type = pylal_get_ilwdchar_class("coinc_definer", "coinc_def_id");
-	coinc_event_id_type = pylal_get_ilwdchar_class("coinc_event", "coinc_event_id");
-	time_slide_id_type = pylal_get_ilwdchar_class("time_slide", "time_slide_id");
-	if(!process_id_type || !coinc_def_id_type || !coinc_event_id_type || !time_slide_id_type)
-		goto noids;
-
-	/* Coinc */
-	if(PyType_Ready(&ligolw_Coinc_Type) < 0)
-		goto nocoinctype;
-	Py_INCREF(&ligolw_Coinc_Type);
-	PyModule_AddObject(module, "Coinc", (PyObject *) &ligolw_Coinc_Type);
-
 	return;
-
-nocoinctype:
-nocoincmaptype:
-noids:
-	Py_XDECREF(process_id_type);
-	Py_XDECREF(coinc_def_id_type);
-	Py_XDECREF(coinc_event_id_type);
-	Py_XDECREF(time_slide_id_type);
 
 nomodule:
 	return;
