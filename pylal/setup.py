c852ba860e1cf93aac5ef22335be87e5907eb23c
Kipp Cannon <kipp.cannon@ligo.org>
2017-02-03 17:49:13 +0900

    pylal: remove defunct lalapps_cbc_plot_likelihood_arrays

--- pylal/setup.py
+++ pylal/setup.py
@@ -437,7 +437,6 @@ setup(
 		os.path.join("bin", "lalapps_cbc_plotroc"),
 		os.path.join("bin", "lalapps_cbc_plotroc_ring"),
 		os.path.join("bin", "lalapps_cbc_plotsummary"),
-		os.path.join("bin", "lalapps_cbc_plot_likelihood_arrays"),
 		os.path.join("bin", "lalapps_farburst"),
 		os.path.join("bin", "lalapps_followup_pipe"),
 		os.path.join("bin", "lalapps_followup_page"),
