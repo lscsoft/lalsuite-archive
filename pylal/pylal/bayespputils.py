lscsoft/lalsuite-archive

commit f36e0434195a4d887f53ce1989459469e80a4719

Return integer for ACL sample number

pylal/pylal/bayespputils.py
@@ -5565,7 +5565,7 @@ def find_ndownsample(samples, nDownsample):
                 nskip = ceil(len(samples)/nEff)
         else:
             nskip = np.nan
-    return nskip
+    return int(nskip)
 
 class PEOutputParser(object):
     """

commit fd3c5ef182d655fee2405419d353a5454d38ee95

Fixed bug introduced with commit f36e0434195a4d887f53ce1989459469e80a4719

pylal/pylal/bayespputils.py
@@ -5557,6 +5557,7 @@ def find_ndownsample(samples, nDownsample):
     if nDownsample is not None:
         if len(samples) > nDownsample:
             nskip *= floor(len(samples)/nDownsample)
+            nskip = int(nskip)
 
     else:
         nEff = nEffective
@@ -5565,7 +5566,7 @@ def find_ndownsample(samples, nDownsample):
                 nskip = ceil(len(samples)/nEff)
         else:
             nskip = np.nan
-    return int(nskip)
+    return nskip
 
 class PEOutputParser(object):
     """