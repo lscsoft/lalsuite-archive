a969d463266c3cc9262bfa42740bb6a46421095a
Andrea taracchini <andrea.taracchini@aei.mpg.de>
2017-07-25 16:16:45 +0200

    SEOBNRv4HM Add array for H fo opt

--- lalsimulation/src/LALSimIMRSpinAlignedEOB.c
+++ lalsimulation/src/LALSimIMRSpinAlignedEOB.c
@@ -1504,7 +1504,7 @@ XLALSimIMRSpinAlignedEOBWaveformAll (REAL8TimeSeries ** hplus,
       if (use_optimized_v2_or_v4)
 	{
 	  /* OPTIMIZED: */
-	  ham =
+	  hamV->data[i] =
 	    XLALSimIMRSpinEOBHamiltonianOptimized (eta, &cartPosVec,
 						   &cartMomVec,
 						   &s1VecOverMtMt,
@@ -1746,6 +1746,12 @@ XLALSimIMRSpinAlignedEOBWaveformAll (REAL8TimeSeries ** hplus,
         nqcCoeffsInput->data[7] = nqcCoeffs.b2;
         nqcCoeffsInput->data[8] = nqcCoeffs.b3;
         nqcCoeffsInput->data[9] = nqcCoeffs.b4;
+#if debugOutput
+        printf
+        ("Tidal point-mass NQC should not be 0 here: %.16e %.16e %.16e %.16e %.16e %.16e %.16e %.16e %.16e %.16e\n",
+         nqcCoeffs.a1, nqcCoeffs.a2, nqcCoeffs.a3, nqcCoeffs.a3S, nqcCoeffs.a4,
+         nqcCoeffs.a5, nqcCoeffs.b1, nqcCoeffs.b2, nqcCoeffs.b3, nqcCoeffs.b4);
+#endif
         return XLAL_SUCCESS;
     }
     /* Here we store the NQC coefficients for the different modes in some arrays */
