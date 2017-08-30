9901e8fae5e273e9610d417203d347baeec54b19
Carl-Johan Haster <carl-johan.haster@ligo.org>
2017-08-30 00:54:47 -0400

    allow for alignedSpins with precessing approximants in LALInference

--- lalinference/src/LALInferenceTemplate.c
+++ lalinference/src/LALInferenceTemplate.c
@@ -312,8 +312,9 @@ void LALInferenceROQWrapperForXLALSimInspiralChooseFDWaveformSequence(LALInferen
 
   /* If we have tilt angles zero, then the spins are aligned and we just set the z component */
   /* However, if the waveform supports precession then we still need to get the right coordinate components */
-  SpinSupport spin_support=XLALSimInspiralGetSpinSupportFromApproximant(approximant);
-  if(tilt1==0.0 && tilt2==0.0 && (spin_support==LAL_SIM_INSPIRAL_SPINLESS || spin_support==LAL_SIM_INSPIRAL_ALIGNEDSPIN))
+  /*SpinSupport spin_support=XLALSimInspiralGetSpinSupportFromApproximant(approximant);*/
+  /*if(tilt1==0.0 && tilt2==0.0 && (spin_support==LAL_SIM_INSPIRAL_SPINLESS || spin_support==LAL_SIM_INSPIRAL_ALIGNEDSPIN))*/
+  if(tilt1==0.0 && tilt2==0.0)
   {
       spin1z=a_spin1;
       spin2z=a_spin2;
