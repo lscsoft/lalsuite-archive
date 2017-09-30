4f9994973ece0c4a2c71051bb5bfa1cd9f2c09e0
Salvatore Vitale <salvatore.vitale@ligo.org>
2017-09-30 17:01:25 -0400

    Make nskip an integer
    Signed-off-by: Vivien Raymond <vivien.raymond@ligo.org>
    (cherry picked from commit 8fcab243316ffdbd4f1dcc7e319dd030a5acc34d)

--- pylal/pylal/bayespputils.py
+++ pylal/pylal/bayespputils.py
@@ -5558,12 +5558,11 @@ def find_ndownsample(samples, nDownsample):
         if len(samples) > nDownsample:
             nskip *= floor(len(samples)/nDownsample)
             nskip = int(nskip)
-
     else:
         nEff = nEffective
         if nEff > 1:
             if len(samples) > nEff:
-                nskip = ceil(len(samples)/nEff)
+                nskip = int(ceil(len(samples)/nEff))
         else:
             nskip = np.nan
     return nskip
@@ -5824,15 +5823,14 @@ class PEOutputParser(object):
         ntot = sum(ntots)
         if nDownsample is not None:
             if ntot > nDownsample:
-                nskips *= floor(ntot/nDownsample)
-
+                nskips *= int(floor(ntot/nDownsample))
         else:
             for i in range(nfiles):
                 nEff = nEffectives[i]
                 ntot = ntots[i]
                 if nEff > 1:
                     if ntot > nEff:
-                        nskips[i] = ceil(ntot/nEff)
+                        nskips[i] = int(ceil(ntot/nEff))
                 else:
                     nskips[i] = None
         return nskips
