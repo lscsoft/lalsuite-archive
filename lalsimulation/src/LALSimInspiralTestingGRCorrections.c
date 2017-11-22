28a28cc9b7974b38d4a10582dba8ad2168c75d00
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-11-22 11:49:10 +0100

    removed unnecessary declarations

--- lalsimulation/src/LALSimInspiralTestingGRCorrections.c
+++ lalsimulation/src/LALSimInspiralTestingGRCorrections.c
@@ -339,12 +339,10 @@ int XLALSimInspiralPhaseCorrectionsPhasing(COMPLEX16FrequencySeries *htilde,    
 
 //  gsl_spline_init(splineTot, freqs->data, phaseTot->data, freqs->length);
 //  ref_phasing = gsl_spline_eval(splineTot, f_ref, acc);
-  const REAL8 m_sec = m * LAL_MTSUN_SI;  /* total mass in seconds */
-  const REAL8 eta = m1 * m2 / (m * m);
   //    const REAL8 piM = LAL_PI * m_sec;
   //    const REAL8 vISCO = 1. / sqrt(6.);
   //    const REAL8 fISCO = vISCO * vISCO * vISCO / piM;
-  const REAL8 fPeak = GetNRSpinPeakOmegaV4(2, 2, eta, 0.) / (2. * LAL_PI * m_sec);
+  const REAL8 fPeak = GetNRSpinPeakOmegaV4(2, 2, eta, 0.) / (2. * LAL_PI * mtot * LAL_MTSUN_SI);
     REAL8 PNPhaseRefDerivative = PNPhaseDerivative(fPeak, pfa);
   for ( i = iStart; i < freqs->length; i++ ) {
     REAL8 phasing = phaseTot->data[i] - PNPhaseRefDerivative*freqs->data[i];
